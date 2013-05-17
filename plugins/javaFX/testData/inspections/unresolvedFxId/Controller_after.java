858faa9fb277ec08446158aaa4d123b8376c0792
VISTALL <vistall.valeriy@gmail.com>
2013-05-17 11:54:21 +0300

    google-app-engine & javafx plugins removed

--- plugins/javaFX/testData/inspections/unresolvedFxId/Controller_after.java
+++
@@ -1,5 +0,0 @@
-import javafx.scene.control.TextField;
-
-public class Controller {
-    public TextField unknown;
-}
--- plugins/javaFX/testData/rename/CustomComponentTag.java
+++
@@ -1,5 +0,0 @@
-package p;
-
-import javafx.scene.layout.VBox;
-public class CustomVBox extends VBox {
-}
