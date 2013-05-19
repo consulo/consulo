bd30ebd90ea32e4fd9b25a2981b11e1c800457cc
VISTALL <vistall.valeriy@gmail.com>
2013-05-19 22:02:24 +0300

    uncomment

--- platform/lang-impl/src/com/intellij/ide/actions/NewProjectAction.java
+++ platform/lang-impl/src/com/intellij/ide/actions/NewProjectAction.java
@@ -15,12 +15,14 @@
  */
 package com.intellij.ide.actions;
 
+import com.intellij.ide.impl.NewProjectUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.DumbAware;
 
 public class NewProjectAction extends AnAction implements DumbAware {
   public void actionPerformed(AnActionEvent e) {
-   //TODO [VISTALL] java depend NewProjectUtil.createNewProject(PlatformDataKeys.PROJECT.getData(e.getDataContext()), null);
+   NewProjectUtil.createNewProject(PlatformDataKeys.PROJECT.getData(e.getDataContext()), null);
   }
 }
