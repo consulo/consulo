85332059d9a9c6e5fa91682fce31e73990cc4090
VISTALL <vistall.valeriy@gmail.com>
2013-06-25 18:45:48 +0300

    iae fix

--- plugins/java/src/com/intellij/refactoring/safeDelete/ImportSearcher.java
+++ plugins/java/src/com/intellij/refactoring/safeDelete/ImportSearcher.java
@@ -23,7 +23,7 @@ import org.jetbrains.annotations.Nullable;
  * @author Max Medvedev
  */
 public abstract class ImportSearcher {
-  private static final ExtensionPointName<ImportSearcher> EP_NAME = ExtensionPointName.create("com.intellij.safeDelete.importSearcher");
+  private static final ExtensionPointName<ImportSearcher> EP_NAME = ExtensionPointName.create("org.consulo.java.safeDelete.importSearcher");
 
   /**
    * @return found import or null
