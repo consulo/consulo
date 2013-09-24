ffa8b63940c6b43fddb7efb305f8859da6b670b7
VISTALL <vistall.valeriy@gmail.com>
2013-09-24 10:11:41 +0300

    jvm-bytecode-viewer merged into java-platform plugin

--- plugins/java-platform/jvm-bytecode-viewer/src/com/intellij/byteCodeViewer/ByteCodeViewerComponent.java
+++ plugins/java-platform/jvm-bytecode-viewer/src/com/intellij/byteCodeViewer/ByteCodeViewerComponent.java
@@ -1,7 +1,6 @@
 package com.intellij.byteCodeViewer;
 
 import com.intellij.codeInsight.hint.EditorFragmentComponent;
-import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.ActionPlaces;
@@ -14,6 +13,7 @@ import com.intellij.openapi.editor.colors.EditorColors;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.editor.highlighter.EditorHighlighterFactory;
+import com.intellij.openapi.fileTypes.PlainTextFileType;
 import com.intellij.openapi.fileTypes.SyntaxHighlighter;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
 import com.intellij.openapi.project.Project;
@@ -39,7 +39,7 @@ public class ByteCodeViewerComponent extends JPanel implements Disposable {
     doc.setReadOnly(true);
     myEditor = factory.createEditor(doc, project);
     EditorHighlighterFactory editorHighlighterFactory = EditorHighlighterFactory.getInstance();
-    final SyntaxHighlighter syntaxHighlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(JavaFileType.INSTANCE, project, null);
+    final SyntaxHighlighter syntaxHighlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(PlainTextFileType.INSTANCE, project, null);
     ((EditorEx)myEditor).setHighlighter(editorHighlighterFactory.createEditorHighlighter(syntaxHighlighter, EditorColorsManager.getInstance().getGlobalScheme()));
     ((EditorEx)myEditor).setBackgroundColor(EditorFragmentComponent.getBackgroundColor(myEditor));
     myEditor.getColorsScheme().setColor(EditorColors.CARET_ROW_COLOR, LightColors.SLIGHTLY_GRAY);
