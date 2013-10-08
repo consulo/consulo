2e1a49e3f3f01b82afeff2549ddb1ac12dda3944
VISTALL <vistall.valeriy@gmail.com>
2013-10-08 21:41:40 +0300

    help class

---
+++ platform/lang-impl/src/com/intellij/execution/configuration/EmptyRunProfileState.java
@@ -0,0 +1,22 @@
+package com.intellij.execution.configuration;
+
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.ExecutionResult;
+import com.intellij.execution.Executor;
+import com.intellij.execution.configurations.RunProfileState;
+import com.intellij.execution.runners.ProgramRunner;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public final class EmptyRunProfileState implements RunProfileState {
+  public static RunProfileState INSTANCE = new EmptyRunProfileState();
+
+  private EmptyRunProfileState() {
+  }
+
+  @Nullable
+  @Override
+  public ExecutionResult execute(Executor executor, @NotNull ProgramRunner runner) throws ExecutionException {
+    return null;
+  }
+}
