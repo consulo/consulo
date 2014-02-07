d1eebf301b4d8dd2e48ee212141c04ac77f9ab42
VISTALL <vistall.valeriy@gmail.com>
2014-02-07 17:26:49 +0200

    override

--- platform/vcs-impl/src/com/intellij/openapi/vcs/impl/VcsFileTypeFactory.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/impl/VcsFileTypeFactory.java
@@ -25,6 +25,7 @@ import org.jetbrains.annotations.NotNull;
  * @author yole
  */
 public class VcsFileTypeFactory extends FileTypeFactory {
+  @Override
   public void createFileTypes(@NotNull FileTypeConsumer consumer) {
     consumer.consume(PatchFileType.INSTANCE, VcsConfiguration.PATCH + ";" + VcsConfiguration.DIFF);
   }
