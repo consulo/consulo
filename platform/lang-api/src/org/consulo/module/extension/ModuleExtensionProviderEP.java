e182288a6aedb71619268342534b5995fbb887ca
VISTALL <vistall.valeriy@gmail.com>
2014-02-19 18:06:19 +0200

    updated obsolete copyrights

--- platform/lang-api/src/org/consulo/module/extension/ModuleExtensionProviderEP.java
+++ platform/lang-api/src/org/consulo/module/extension/ModuleExtensionProviderEP.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Consulo.org
+ * Copyright 2013 must-be.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
