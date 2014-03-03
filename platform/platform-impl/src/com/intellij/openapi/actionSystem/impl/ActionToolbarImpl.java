ccb982e9f047ae31a30fded965b3e91f5e9a2a45
VISTALL <vistall.valeriy@gmail.com>
2014-03-03 22:01:55 +0200

    fix separators in nav bar

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionToolbarImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionToolbarImpl.java
@@ -266,9 +266,9 @@ public class ActionToolbarImpl extends JPanel implements ActionToolbar {
     }
     for (int i = 0; i < actions.size(); i++) {
       final AnAction action = actions.get(i);
-      if (action instanceof AnSeparator && isNavBar()) {
-        continue;
-      }
+//      if (action instanceof AnSeparator && isNavBar()) {
+//        continue;
+//      }
 
       //if (action instanceof ComboBoxAction) {
       //  ((ComboBoxAction)action).setSmallVariant(true);
