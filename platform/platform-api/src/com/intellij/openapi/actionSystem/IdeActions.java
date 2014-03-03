eaa93bc6ec582d94336947c97033cf97a6fd4a24
VISTALL <vistall.valeriy@gmail.com>
2014-03-03 23:41:10 +0200

    next merge

--- platform/platform-api/src/com/intellij/openapi/actionSystem/IdeActions.java
+++ platform/platform-api/src/com/intellij/openapi/actionSystem/IdeActions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -71,6 +71,7 @@ public interface IdeActions {
   @NonNls String ACTION_SMART_TYPE_COMPLETION = "SmartTypeCompletion";
   @Deprecated @NonNls String ACTION_CLASS_NAME_COMPLETION = "ClassNameCompletion";
   @NonNls String ACTION_HIPPIE_COMPLETION = "HippieCompletion";
+  @NonNls String ACTION_HIPPIE_BACKWARD_COMPLETION = "HippieBackwardCompletion";
   @NonNls String ACTION_CHOOSE_LOOKUP_ITEM = "EditorChooseLookupItem";
   @NonNls String ACTION_CHOOSE_LOOKUP_ITEM_ALWAYS = "EditorChooseLookupItemAlways";
   @NonNls String ACTION_CHOOSE_LOOKUP_ITEM_REPLACE = "EditorChooseLookupItemReplace";
@@ -113,6 +114,8 @@ public interface IdeActions {
   @NonNls String ACTION_FIND = "Find";
   @NonNls String ACTION_FIND_NEXT = "FindNext";
   @NonNls String ACTION_FIND_PREVIOUS = "FindPrevious";
+  @NonNls String ACTION_SELECT_NEXT_OCCURENCE = "SelectNextOccurrence";
+  @NonNls String ACTION_UNSELECT_LAST_OCCURENCE = "UnselectLastOccurrence";
   @NonNls String ACTION_COMPILE = "Compile";
   @NonNls String ACTION_COMPILE_PROJECT = "CompileProject";
   @NonNls String ACTION_MAKE_MODULE = "MakeModule";
--- platform/platform-api/src/com/intellij/openapi/editor/actionSystem/EditorActionHandler.java
+++ platform/platform-api/src/com/intellij/openapi/editor/actionSystem/EditorActionHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,14 +16,35 @@
 package com.intellij.openapi.editor.actionSystem;
 
 import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.CaretAction;
 import com.intellij.openapi.editor.Editor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Interface for actions activated by keystrokes in the editor.
+ * Implementations should override {@link #execute(com.intellij.openapi.editor.Editor, com.intellij.openapi.actionSystem.DataContext)} or
+ * {@link #execute(com.intellij.openapi.editor.Editor, com.intellij.openapi.editor.Caret, com.intellij.openapi.actionSystem.DataContext)}
+ * (preferrably).
+ * <p>
+ * Two types of handlers are supported: the ones which are executed once, and the ones which are executed for each caret. The latter can be
+ * created using {@link com.intellij.openapi.editor.actionSystem.EditorActionHandler#EditorActionHandler(boolean)} constructor.
  *
  * @see EditorActionManager#setActionHandler(String, EditorActionHandler)
  */
 public abstract class EditorActionHandler {
+  private final boolean myRunForEachCaret;
+  private boolean inExecution;
+
+  protected EditorActionHandler() {
+    this(false);
+  }
+
+  protected EditorActionHandler(boolean runForEachCaret) {
+    myRunForEachCaret = runForEachCaret;
+  }
+
   /**
    * Checks if the action handler is currently enabled.
    *
@@ -36,15 +57,76 @@ public abstract class EditorActionHandler {
   }
 
   /**
-   * Executes the action.
+   * Executes the action in the context of the current caret. This method exists for historical reasons, in most cases you should use
+   * {@link #execute(com.intellij.openapi.editor.Editor, com.intellij.openapi.editor.Caret, com.intellij.openapi.actionSystem.DataContext)}
+   * instead.
+   *
+   * @param editor      the editor in which the action is invoked.
+   * @param dataContext the data context for the action.
+   *
+   * @see {@link com.intellij.openapi.editor.CaretModel#getCurrentCaret()}
+   */
+  public void execute(Editor editor, DataContext dataContext) {
+    if (inExecution) {
+      return;
+    }
+    try {
+      inExecution = true;
+      execute(editor, editor.getCaretModel().getCurrentCaret(), dataContext);
+    }
+    finally {
+      inExecution = false;
+    }
+  }
+
+  /**
+   * Executes the action in the context of given caret
+   *
+   * @param editor      the editor in which the action is invoked.
+   * @param caret       the caret for which the action is performed at the moment or <code>null</code> if it's a 'one-off' action executed
+   *                    without current context
+   * @param dataContext the data context for the action.
+   */
+  public void execute(Editor editor, @Nullable Caret caret, DataContext dataContext) {
+    if (inExecution) {
+      return;
+    }
+    try {
+      inExecution = true;
+      execute(editor, dataContext);
+    }
+    finally {
+      inExecution = false;
+    }
+  }
+
+  public boolean executeInCommand(Editor editor, DataContext dataContext) {
+    return true;
+  }
+
+  public boolean runForAllCarets() {
+    return myRunForEachCaret;
+  }
+
+  /**
+   * Executes the action in the context of given caret. If the caret is <code>null</code>, and the handler is a 'per-caret' handler,
+   * it's executed for all carets.
    *
    * @param editor      the editor in which the action is invoked.
    * @param dataContext the data context for the action.
    */
-  public abstract void execute(Editor editor, DataContext dataContext);
-
-  public boolean executeInCommand(Editor editor, DataContext dataContext) {
-    return true;
+  public void executeInCaretContext(@NotNull final Editor editor, @Nullable Caret caret, final DataContext dataContext) {
+    if (caret == null && runForAllCarets()) {
+      editor.getCaretModel().runForEachCaret(new CaretAction() {
+        @Override
+        public void perform(Caret caret) {
+          execute(editor, caret, dataContext);
+        }
+      });
+    }
+    else {
+      execute(editor, caret, dataContext);
+    }
   }
 
   public DocCommandGroupId getCommandGroupId(Editor editor) {
