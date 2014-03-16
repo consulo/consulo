d2e9efd5b5549ed473a78757babc654c633c4d4b
VISTALL <vistall.valeriy@gmail.com>
2014-03-16 16:07:19 +0200

    to java

--- platform/lang-impl/src/com/intellij/codeInsight/template/impl/LiveTemplateCompletionContributor.java
+++ platform/lang-impl/src/com/intellij/codeInsight/template/impl/LiveTemplateCompletionContributor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,54 +20,73 @@ import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.template.CustomLiveTemplate;
 import com.intellij.codeInsight.template.CustomTemplateCallback;
 import com.intellij.codeInsight.template.TemplateContextType;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiPlainTextFile;
+import com.intellij.ui.EditorTextField;
 import com.intellij.util.Consumer;
 import com.intellij.util.ProcessingContext;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.containers.MultiMap;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
+import static com.intellij.codeInsight.template.impl.ListTemplatesHandler.listApplicableCustomTemplates;
 
 /**
  * @author peter
  */
 public class LiveTemplateCompletionContributor extends CompletionContributor {
+  public static boolean ourShowTemplatesInTests = false;
+
+  public static boolean shouldShowAllTemplates() {
+    if (ApplicationManager.getApplication().isUnitTestMode()) {
+      return ourShowTemplatesInTests;
+    }
+    return Registry.is("show.live.templates.in.completion");
+  }
+
   public LiveTemplateCompletionContributor() {
     extend(CompletionType.BASIC, PlatformPatterns.psiElement(), new CompletionProvider<CompletionParameters>() {
       @Override
-      protected void addCompletions(@NotNull CompletionParameters parameters,
+      protected void addCompletions(@NotNull final CompletionParameters parameters,
                                     ProcessingContext context,
                                     @NotNull CompletionResultSet result) {
         final PsiFile file = parameters.getPosition().getContainingFile();
+        if (file instanceof PsiPlainTextFile &&
+            parameters.getEditor().getComponent().getParent() instanceof EditorTextField) {
+          return;
+        }
+
         final int offset = parameters.getOffset();
         final List<TemplateImpl> templates = listApplicableTemplates(file, offset);
+        Editor editor = parameters.getEditor();
         if (showAllTemplates()) {
+          final MultiMap<String, CustomLiveTemplateLookupElement> customTemplates = listApplicableCustomTemplates(editor, file, offset);
           final Ref<Boolean> templatesShown = Ref.create(false);
-
           final CompletionResultSet finalResult = result;
           result.runRemainingContributors(parameters, new Consumer<CompletionResult>() {
             @Override
             public void consume(CompletionResult completionResult) {
               finalResult.passResult(completionResult);
-              ensureTemplatesShown(templatesShown, templates, finalResult);
+              ensureTemplatesShown(templatesShown, templates, customTemplates, finalResult);
             }
           });
 
-          ensureTemplatesShown(templatesShown, templates, result);
+          ensureTemplatesShown(templatesShown, templates, customTemplates, result);
           return;
         }
 
         if (parameters.getInvocationCount() > 0) return; //only in autopopups for now
 
-        String templatePrefix = findLiveTemplatePrefix(file, parameters.getEditor(), result.getPrefixMatcher().getPrefix());
+        String templatePrefix = findLiveTemplatePrefix(file, editor, result.getPrefixMatcher().getPrefix());
         final TemplateImpl template = findApplicableTemplate(file, offset, templatePrefix);
         if (template != null) {
           result = result.withPrefixMatcher(template.getKey());
@@ -76,21 +95,30 @@ public class LiveTemplateCompletionContributor extends CompletionContributor {
         for (final TemplateImpl possible : templates) {
           result.restartCompletionOnPrefixChange(possible.getKey());
         }
-
       }
     });
   }
 
   @SuppressWarnings("MethodMayBeStatic") //for Kotlin
   protected boolean showAllTemplates() {
-    return Registry.is("show.live.templates.in.completion");
+    return shouldShowAllTemplates();
   }
 
-  private static void ensureTemplatesShown(Ref<Boolean> templatesShown, List<TemplateImpl> templates, CompletionResultSet result) {
+  private static void ensureTemplatesShown(Ref<Boolean> templatesShown,
+                                           List<TemplateImpl> templates,
+                                           MultiMap<String, CustomLiveTemplateLookupElement> customTemplates,
+                                           CompletionResultSet result) {
     if (!templatesShown.get()) {
       templatesShown.set(true);
       for (final TemplateImpl possible : templates) {
         result.addElement(new LiveTemplateLookupElementImpl(possible, false));
+      }
+
+      for (Map.Entry<String, Collection<CustomLiveTemplateLookupElement>> entry : customTemplates.entrySet()) {
+        Collection<CustomLiveTemplateLookupElement> value = entry.getValue();
+        if (!value.isEmpty()) {
+          result.withPrefixMatcher(entry.getKey()).addAllElements(value);
+        }
       }
     }
   }
