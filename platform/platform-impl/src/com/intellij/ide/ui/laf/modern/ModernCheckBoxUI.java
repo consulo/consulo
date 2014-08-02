9a7637a9a1cc340c08ed9291fa5c1710ac362841
VISTALL <vistall.valeriy@gmail.com>
2014-08-03 02:13:33 +0400

    fix checkbox paint

--- platform/platform-impl/src/com/intellij/ide/ui/laf/modern/ModernCheckBoxUI.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/modern/ModernCheckBoxUI.java
@@ -24,18 +24,18 @@ import sun.swing.SwingUtilities2;
 import javax.swing.*;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.IconUIResource;
+import javax.swing.plaf.basic.BasicCheckBoxUI;
 import javax.swing.plaf.basic.BasicHTML;
-import javax.swing.plaf.basic.BasicRadioButtonUI;
 import javax.swing.text.View;
 import java.awt.*;
 
 /**
  * @author VISTALL
  * @since 02.08.14
- *
+ * <p/>
  * Based on {@link com.intellij.ide.ui.laf.darcula.ui.DarculaCheckBoxUI}
  */
-public class ModernCheckBoxUI extends BasicRadioButtonUI {
+public class ModernCheckBoxUI extends BasicCheckBoxUI {
   @SuppressWarnings("MethodOverridesStaticMethodOfSuperclass")
   public static ComponentUI createUI(JComponent c) {
     if (UIUtil.getParentOfType(CellRendererPane.class, c) != null) {
@@ -53,7 +53,7 @@ public class ModernCheckBoxUI extends BasicRadioButtonUI {
   @Override
   public synchronized void paint(Graphics g2d, JComponent c) {
     Graphics2D g = (Graphics2D)g2d;
-    JCheckBox b = (JCheckBox) c;
+    JCheckBox b = (JCheckBox)c;
     final ButtonModel model = b.getModel();
     final Dimension size = c.getSize();
     final Font font = c.getFont();
@@ -71,10 +71,9 @@ public class ModernCheckBoxUI extends BasicRadioButtonUI {
     viewRect.width -= (i.right + viewRect.x);
     viewRect.height -= (i.bottom + viewRect.y);
 
-    String text = SwingUtilities.layoutCompoundLabel(c, fm, b.getText(), getDefaultIcon(),
-                                                     b.getVerticalAlignment(), b.getHorizontalAlignment(),
-                                                     b.getVerticalTextPosition(), b.getHorizontalTextPosition(),
-                                                     viewRect, iconRect, textRect, b.getIconTextGap());
+    String text = SwingUtilities
+            .layoutCompoundLabel(c, fm, b.getText(), getDefaultIcon(), b.getVerticalAlignment(), b.getHorizontalAlignment(), b.getVerticalTextPosition(),
+                                 b.getHorizontalTextPosition(), viewRect, iconRect, textRect, b.getIconTextGap());
 
     //background
     if (c.isOpaque()) {
@@ -84,9 +83,11 @@ public class ModernCheckBoxUI extends BasicRadioButtonUI {
 
     if (b.isSelected() && b.getSelectedIcon() != null) {
       b.getSelectedIcon().paintIcon(b, g, iconRect.x + 4, iconRect.y + 2);
-    } else if (!b.isSelected() && b.getIcon() != null) {
+    }
+    else if (!b.isSelected() && b.getIcon() != null) {
       b.getIcon().paintIcon(b, g, iconRect.x + 4, iconRect.y + 2);
-    } else {
+    }
+    else {
       final int x = iconRect.x + 3;
       final int y = iconRect.y + 3;
       final int w = iconRect.width - 6;
@@ -120,7 +121,7 @@ public class ModernCheckBoxUI extends BasicRadioButtonUI {
 
       if (b.getModel().isSelected()) {
         g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
-        g.setStroke(new BasicStroke(2.0f, BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND));
+        g.setStroke(new BasicStroke(2.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
 
         g.setPaint(getCheckSignColor(b.isEnabled()));
         g.drawLine(4, 7, 7, 10);
@@ -131,15 +132,22 @@ public class ModernCheckBoxUI extends BasicRadioButtonUI {
     }
 
     //text
-    if(text != null) {
-      View view = (View) c.getClientProperty(BasicHTML.propertyKey);
+    if (text != null) {
+      View view = (View)c.getClientProperty(BasicHTML.propertyKey);
       if (view != null) {
         view.paint(g, textRect);
-      } else {
+      }
+      else {
         g.setColor(model.isEnabled() ? b.getForeground() : UIManager.getColor("CheckBox.disabledText"));
         SwingUtilities2.drawStringUnderlineCharAt(c, g, text, b.getDisplayedMnemonicIndex(), textRect.x, textRect.y + fm.getAscent());
       }
     }
+  }
+
+  @Override
+  protected void paintFocus(Graphics g, Rectangle t, Dimension d) {
+    g.setColor(UIManager.getColor("CheckBox.focus"));
+    g.drawRect(t.x - 1, t.y - 1, t.width + 1, t.height + 1);
   }
 
   protected Color getInactiveFillColor() {
@@ -147,8 +155,7 @@ public class ModernCheckBoxUI extends BasicRadioButtonUI {
   }
 
   protected Color getBorderColor1(boolean enabled) {
-    return enabled ? getColor("borderColor1", Gray._120.withAlpha(0x5a))
-                   : getColor("disabledBorderColor1", Gray._120.withAlpha(90));
+    return enabled ? getColor("borderColor1", Gray._120.withAlpha(0x5a)) : getColor("disabledBorderColor1", Gray._120.withAlpha(90));
   }
 
   protected Color getBackgroundColor1() {
@@ -156,18 +163,15 @@ public class ModernCheckBoxUI extends BasicRadioButtonUI {
   }
 
   protected Color getCheckSignColor(boolean enabled) {
-    return enabled ? getColor("checkSignColor", Gray._170)
-                   : getColor("checkSignColorDisabled", Gray._120);
+    return enabled ? getColor("checkSignColor", Gray._170) : getColor("checkSignColorDisabled", Gray._120);
   }
 
   protected Color getShadowColor(boolean enabled) {
-    return enabled ? getColor("shadowColor", Gray._30)
-                   : getColor("shadowColorDisabled", Gray._60);
+    return enabled ? getColor("shadowColor", Gray._30) : getColor("shadowColorDisabled", Gray._60);
   }
 
   protected Color getFocusedBackgroundColor1(boolean armed) {
-    return armed ? getColor("focusedArmed.backgroundColor1", Gray._100)
-                 : getColor("focused.backgroundColor1", Gray._120);
+    return armed ? getColor("focusedArmed.backgroundColor1", Gray._100) : getColor("focused.backgroundColor1", Gray._120);
   }
 
   protected static Color getColor(String shortPropertyName, Color defaultValue) {
