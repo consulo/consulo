4e2a132cf6819a4dfe1c2c45718f9872160663d5
VISTALL <vistall.valeriy@gmail.com>
2015-02-14 17:01:51 +0300

    fixed #CEP-430

--- platform/lang-impl/src/com/intellij/codeInsight/editorActions/moveUpDown/MoverWrapper.java
+++ platform/lang-impl/src/com/intellij/codeInsight/editorActions/moveUpDown/MoverWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,8 @@ class MoverWrapper {
     return myInfo;
   }
 
-  public final void move(Editor editor, final PsiFile file) {
+  public final void move(final Editor editor, final PsiFile file) {
+    assert myInfo.toMove2 != null;
     myMover.beforeMove(editor, myInfo, myIsDown);
     final Document document = editor.getDocument();
     final int start = StatementUpDownMover.getLineStartSafeOffset(document, myInfo.toMove.startLine);
@@ -88,22 +89,11 @@ class MoverWrapper {
     // and the pointers behave as following during move up/down:
     //     method1() {}
     //     method2() {}
-    // Pointer for the fold region from method1 points to 'method2()' now and vice versa (check range markers processing on 
-    // document change for further information). I.e. information about fold regions statuses holds the data swapped for 
+    // Pointer for the fold region from method1 points to 'method2()' now and vice versa (check range markers processing on
+    // document change for further information). I.e. information about fold regions statuses holds the data swapped for
     // 'method1' and 'method2'. Hence, we want to apply correct 'collapsed' status.
-    FoldRegion topRegion = null;
-    FoldRegion bottomRegion = null;
-    for (FoldRegion foldRegion : editor.getFoldingModel().getAllFoldRegions()) {
-      if (!foldRegion.isValid() || (!contains(myInfo.range1, foldRegion) && !contains(myInfo.range2, foldRegion))) {
-        continue;
-      }
-      if (contains(myInfo.range1, foldRegion) && !contains(topRegion, foldRegion)) {
-        topRegion = foldRegion;
-      }
-      else if (contains(myInfo.range2, foldRegion) && !contains(bottomRegion, foldRegion)) {
-        bottomRegion = foldRegion;
-      }
-    }
+    final FoldRegion topRegion = findTopLevelRegionInRange(editor, myInfo.range1);
+    final FoldRegion bottomRegion = findTopLevelRegionInRange(editor, myInfo.range2);
 
     document.insertString(myInfo.range1.getStartOffset(), textToInsert2);
     document.deleteString(myInfo.range1.getStartOffset()+textToInsert2.length(), myInfo.range1.getEndOffset());
@@ -120,14 +110,19 @@ class MoverWrapper {
 
     // Swap fold regions status if necessary.
     if (topRegion != null && bottomRegion != null) {
-      final FoldRegion finalTopRegion = topRegion;
-      final FoldRegion finalBottomRegion = bottomRegion;
+      CodeFoldingManager.getInstance(project).updateFoldRegions(editor);
       editor.getFoldingModel().runBatchFoldingOperation(new Runnable() {
         @Override
         public void run() {
-          boolean topExpanded = finalTopRegion.isExpanded();
-          finalTopRegion.setExpanded(finalBottomRegion.isExpanded());
-          finalBottomRegion.setExpanded(topExpanded);
+          FoldRegion newTopRegion = findTopLevelRegionInRange(editor, myInfo.range1);
+          if (newTopRegion != null) {
+            newTopRegion.setExpanded(bottomRegion.isExpanded());
+          }
+
+          FoldRegion newBottomRegion = findTopLevelRegionInRange(editor, myInfo.range2);
+          if (newBottomRegion != null) {
+            newBottomRegion.setExpanded(topRegion.isExpanded());
+          }
         }
       });
     }
@@ -148,9 +143,19 @@ class MoverWrapper {
     editor.getScrollingModel().scrollToCaret(ScrollType.RELATIVE);
   }
 
+  private static FoldRegion findTopLevelRegionInRange(Editor editor, RangeMarker range) {
+    FoldRegion result = null;
+    for (FoldRegion foldRegion : editor.getFoldingModel().getAllFoldRegions()) {
+      if (foldRegion.isValid() && contains(range, foldRegion) && !contains(result, foldRegion)) {
+        result = foldRegion;
+      }
+    }
+    return result;
+  }
+
   /**
    * Allows to check if text range defined by the given range marker completely contains text range of the given fold region.
-   * 
+   *
    * @param rangeMarker   range marker to check
    * @param foldRegion    fold region to check
    * @return              <code>true</code> if text range defined by the given range marker completely contains text range
@@ -162,7 +167,7 @@ class MoverWrapper {
 
   /**
    * Allows to check if given <code>'region2'</code> is nested to <code>'region1'</code>
-   * 
+   *
    * @param region1   'outer' region candidate
    * @param region2   'inner' region candidate
    * @return          <code>true</code> if 'region2' is nested to 'region1'; <code>false</code> otherwise
@@ -173,18 +178,18 @@ class MoverWrapper {
     }
     return region1.getStartOffset() <= region2.getStartOffset() && region1.getEndOffset() >= region2.getEndOffset();
   }
-  
+
   private static void indentLinesIn(final Editor editor, final PsiFile file, final Document document, final Project project, RangeMarker range) {
     final CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(project);
     int line1 = editor.offsetToLogicalPosition(range.getStartOffset()).line;
     int line2 = editor.offsetToLogicalPosition(range.getEndOffset()).line;
 
-    while (!lineContainsNonSpaces(document, line1) && line1 <= line2) line1++;
-    while (!lineContainsNonSpaces(document, line2) && line2 > line1) line2--;
+    while (!lineContainsNonSpaces(document, line1) && line1 < line2) line1++;
+    while (!lineContainsNonSpaces(document, line2) && line1 < line2) line2--;
 
-      final FileViewProvider provider = file.getViewProvider();
-      PsiFile rootToAdjustIndentIn = provider.getPsi(provider.getBaseLanguage());
-      codeStyleManager.adjustLineIndent(rootToAdjustIndentIn, new TextRange(document.getLineStartOffset(line1), document.getLineStartOffset(line2)));
+    final FileViewProvider provider = file.getViewProvider();
+    PsiFile rootToAdjustIndentIn = provider.getPsi(provider.getBaseLanguage());
+    codeStyleManager.adjustLineIndent(rootToAdjustIndentIn, new TextRange(document.getLineStartOffset(line1), document.getLineStartOffset(line2)));
   }
 
   private static boolean lineContainsNonSpaces(final Document document, final int line) {
