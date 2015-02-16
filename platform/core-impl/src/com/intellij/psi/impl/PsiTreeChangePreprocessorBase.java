b0a6e16a9d9558a6658a4dbd5ce1a9082911234b
VISTALL <vistall.valeriy@gmail.com>
2015-02-16 12:10:43 +0300

    require only project parameter

--- platform/core-impl/src/com/intellij/psi/impl/PsiTreeChangePreprocessorBase.java
+++ platform/core-impl/src/com/intellij/psi/impl/PsiTreeChangePreprocessorBase.java
@@ -16,8 +16,10 @@
 
 package com.intellij.psi.impl;
 
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiModificationTracker;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -26,8 +28,8 @@ import org.jetbrains.annotations.NotNull;
 public abstract class PsiTreeChangePreprocessorBase implements PsiTreeChangePreprocessor {
   private final PsiModificationTrackerImpl myModificationTracker;
 
-  public PsiTreeChangePreprocessorBase(PsiManagerImpl psiManager) {
-    myModificationTracker = (PsiModificationTrackerImpl) psiManager.getModificationTracker();
+  public PsiTreeChangePreprocessorBase(@NotNull Project project) {
+    myModificationTracker = (PsiModificationTrackerImpl)PsiModificationTracker.SERVICE.getInstance(project);
   }
 
   @Override
