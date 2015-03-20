7173ab43e8cb9bbcf5c461cf73341a3c08a41c87
VISTALL <vistall.valeriy@gmail.com>
2015-03-21 01:25:12 +0300

    merge 	- get rid of gradients in table editor

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorHeaderComponent.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorHeaderComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,9 +15,11 @@
  */
 package com.intellij.openapi.editor.impl;
 
-import com.intellij.ui.Gray;
+import com.intellij.ide.ui.UISettings;
+import com.intellij.openapi.util.SystemInfo;
 import com.intellij.ui.JBColor;
-import com.intellij.util.ui.UIUtil;
+import com.intellij.ui.border.CustomLineBorder;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
@@ -26,39 +28,18 @@ import java.awt.*;
  * @author gregsh
  */
 public class EditorHeaderComponent extends JPanel {
-
   public EditorHeaderComponent() {
     super(new BorderLayout(0, 0));
+    boolean topBorderRequired = !SystemInfo.isMac && UISettings.getInstance().EDITOR_TAB_PLACEMENT != SwingConstants.TOP &&
+                                !(UISettings.getInstance().SHOW_MAIN_TOOLBAR && UISettings.getInstance().SHOW_NAVIGATION_BAR);
+    setBorder(new CustomLineBorder(JBColor.border(), topBorderRequired ? 1 : 0, 0, 1, 0));
   }
 
   @Override
-  public void paint(Graphics g) {
-    final Graphics2D g2 = (Graphics2D)g;
+  public void paint(@NotNull Graphics g) {
+    Graphics2D g2 = (Graphics2D)g;
     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
     super.paint(g);
-  }
-
-  @Override
-  protected void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    paintGradient(g, this);
-  }
-
-  public static void paintGradient(Graphics g, JComponent c) {
-    Color GRADIENT_C1 = new JBColor(c.getBackground(), JBColor.background());
-    Color GRADIENT_C2 = new JBColor(new Color(Math.max(0, GRADIENT_C1.getRed() - 0x18), Math.max(0, GRADIENT_C1.getGreen() - 0x18),
-                                              Math.max(0, GRADIENT_C1.getBlue() - 0x18)), Gray._75);
-
-    final Graphics2D g2d = (Graphics2D)g;
-
-    if (!UIUtil.isUnderGTKLookAndFeel()) {
-      g2d.setPaint(UIUtil.getGradientPaint(0, 0, GRADIENT_C1, 0, c.getHeight(), GRADIENT_C2));
-      g2d.fillRect(1, 1, c.getWidth(), c.getHeight() - 1);
-      g2d.setPaint(null);
-    }
-
-    g.setColor(UIUtil.getBorderColor());
-    g.drawLine(0, c.getHeight() - 1, c.getWidth(), c.getHeight() - 1);
   }
 }
