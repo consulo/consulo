4c72dd2f17ef5b6a36b566ce9b6a54a19905f009
VISTALL <vistall.valeriy@gmail.com>
2015-04-06 10:35:06 +0300

    provide way to add + remove content files when module dir is not not set

---
+++ platform/lang-impl/src/org/mustbe/consulo/roots/ContentEntryFileListener.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2013-2015 must-be.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mustbe.consulo.roots;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.AbstractProjectComponent;
+import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.*;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileAdapter;
+import com.intellij.openapi.vfs.VirtualFileEvent;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import org.consulo.lombok.annotations.Logger;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.mustbe.consulo.RequiredReadAction;
+
+/**
+ * @author VISTALL
+ * @since 06.04.2015
+ */
+@Logger
+public class ContentEntryFileListener extends AbstractProjectComponent {
+  public interface PossibleModuleForFileResolver {
+    ExtensionPointName<PossibleModuleForFileResolver> EP_NAME = ExtensionPointName.create("com.intellij.possibleModuleForFileResolver");
+
+    @Nullable
+    @RequiredReadAction
+    Module resolve(@NotNull Project project, @NotNull VirtualFile virtualFile);
+  }
+
+  public class Listener extends VirtualFileAdapter {
+    @Override
+    @RequiredReadAction
+    public void fileCreated(@NotNull VirtualFileEvent event) {
+      Module resolvedModule = null;
+      for (PossibleModuleForFileResolver possibleModuleForFileResolver : PossibleModuleForFileResolver.EP_NAME.getExtensions()) {
+        Module temp = possibleModuleForFileResolver.resolve(myProject, event.getFile());
+        if (temp != null) {
+          resolvedModule = temp;
+          break;
+        }
+      }
+
+      if (resolvedModule == null) {
+        return;
+      }
+
+      LOGGER.assertTrue(resolvedModule.getModuleDirUrl() == null, "We cant add file as content entry is module created as dir based");
+
+      ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(resolvedModule);
+      final ModifiableRootModel modifiableModel = moduleRootManager.getModifiableModel();
+
+      for (ModuleRootLayer moduleRootLayer : modifiableModel.getLayers().values()) {
+        ModifiableModuleRootLayer modifiableModuleRootLayer = (ModifiableModuleRootLayer)moduleRootLayer;
+
+        modifiableModuleRootLayer.addContentEntry(event.getFile());
+      }
+      ApplicationManager.getApplication().runWriteAction(new Runnable() {
+        @Override
+        public void run() {
+          modifiableModel.commit();
+        }
+      });
+    }
+
+    @Override
+    public void beforeFileDeletion(@NotNull VirtualFileEvent event) {
+      VirtualFile fileToDelete = event.getFile();
+
+      Module moduleForFile = ModuleUtilCore.findModuleForFile(fileToDelete, myProject);
+      // if module have dir url - dont need check
+      if (moduleForFile == null || moduleForFile.getModuleDirUrl() != null) {
+        return;
+      }
+      ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(moduleForFile);
+      final ModifiableRootModel modifiableModel = moduleRootManager.getModifiableModel();
+
+      boolean processed = false;
+      for (ModuleRootLayer moduleRootLayer : modifiableModel.getLayers().values()) {
+        ModifiableModuleRootLayer modifiableModuleRootLayer = (ModifiableModuleRootLayer)moduleRootLayer;
+
+        ContentEntry toRemove = null;
+        for (ContentEntry contentEntry : modifiableModuleRootLayer.getContentEntries()) {
+          VirtualFile contentEntryFile = contentEntry.getFile();
+          if (fileToDelete.equals(contentEntryFile)) {
+            toRemove = contentEntry;
+            break;
+          }
+        }
+
+        if(toRemove != null) {
+          processed = true;
+          modifiableModuleRootLayer.removeContentEntry(toRemove);
+        }
+      }
+
+      if (processed) {
+        ApplicationManager.getApplication().runWriteAction(new Runnable() {
+          @Override
+          public void run() {
+            modifiableModel.commit();
+          }
+        });
+      }
+      else {
+        modifiableModel.dispose();
+      }
+    }
+  }
+
+  private Listener myListener = new Listener();
+
+  public ContentEntryFileListener(Project project) {
+    super(project);
+  }
+
+  @Override
+  public void initComponent() {
+    VirtualFileManager.getInstance().addVirtualFileListener(myListener);
+  }
+
+  @Override
+  public void disposeComponent() {
+    VirtualFileManager.getInstance().removeVirtualFileListener(myListener);
+  }
+}
