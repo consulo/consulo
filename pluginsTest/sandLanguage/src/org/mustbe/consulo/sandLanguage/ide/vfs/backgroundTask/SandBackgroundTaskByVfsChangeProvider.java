7e4000166efe75d49fd99c00b7af2a1f0a9d5b3d
VISTALL <vistall.valeriy@gmail.com>
2015-08-13 11:50:10 +0300

    drop deprecated code

--- pluginsTest/sandLanguage/src/org/mustbe/consulo/sandLanguage/ide/vfs/backgroundTask/SandBackgroundTaskByVfsChangeProvider.java
+++ pluginsTest/sandLanguage/src/org/mustbe/consulo/sandLanguage/ide/vfs/backgroundTask/SandBackgroundTaskByVfsChangeProvider.java
@@ -17,10 +17,10 @@ package org.mustbe.consulo.sandLanguage.ide.vfs.backgroundTask;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.consulo.vfs.backgroundTask.BackgroundTaskByVfsChangeProvider;
-import org.mustbe.consulo.vfs.backgroundTask.BackgroundTaskByVfsParameters;
 import org.jetbrains.annotations.NotNull;
 import org.mustbe.consulo.sandLanguage.lang.SandFileType;
+import org.mustbe.consulo.vfs.backgroundTask.BackgroundTaskByVfsChangeProvider;
+import org.mustbe.consulo.vfs.backgroundTask.BackgroundTaskByVfsParameters;
 
 /**
  * @author VISTALL
