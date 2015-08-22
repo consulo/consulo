fd2c337bdc927bc0aef696db32580ff08da462db
VISTALL <vistall.valeriy@gmail.com>
2015-08-22 20:51:08 +0300

    public

--- platform/lang-impl/src/com/intellij/codeInsight/editorActions/TypedHandler.java
+++ platform/lang-impl/src/com/intellij/codeInsight/editorActions/TypedHandler.java
@@ -560,7 +560,7 @@ public class TypedHandler extends TypedActionHandlerBase {
     indentBrace(project, editor, '}');
   }
 
-  static void indentOpenedBrace(@NotNull Project project, @NotNull Editor editor) {
+  public static void indentOpenedBrace(@NotNull Project project, @NotNull Editor editor) {
     indentBrace(project, editor, '{');
   }
 
