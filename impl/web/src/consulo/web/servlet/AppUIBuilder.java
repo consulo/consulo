d2ae58a48aafbfaab83d486374250b6a3c87bf22
VISTALL <vistall.valeriy@gmail.com>
2016-06-16 21:06:39 +0300

    hide modal after close

--- impl/web/src/consulo/web/servlet/AppUIBuilder.java
+++ impl/web/src/consulo/web/servlet/AppUIBuilder.java
@@ -3,10 +3,7 @@ package consulo.web.servlet;
 import com.intellij.openapi.application.ex.ApplicationEx;
 import com.intellij.openapi.application.ex.ApplicationManagerEx;
 import com.intellij.util.TimeoutUtil;
-import consulo.ui.Components;
-import consulo.ui.Layouts;
-import consulo.ui.ValueComponent;
-import consulo.ui.Window;
+import consulo.ui.*;
 import consulo.ui.internal.WGwtListBoxImpl;
 import consulo.ui.internal.WGwtModalWindowImpl;
 import consulo.ui.model.ImmutableListModel;
@@ -44,7 +41,7 @@ public class AppUIBuilder extends UIBuilder {
       }
     }
 
-    WGwtModalWindowImpl modalWindow = new WGwtModalWindowImpl();
+    final WGwtModalWindowImpl modalWindow = new WGwtModalWindowImpl();
     modalWindow.setSize(new Size(777, 460));
 
     List<String> c = new ArrayList<String>();
@@ -57,11 +54,43 @@ public class AppUIBuilder extends UIBuilder {
       @Override
       public void valueChanged(@NotNull ValueComponent.ValueEvent<String> event) {
         System.out.println(event.getValue() + " selected");
+
+        modalWindow.setVisible(false);
       }
     });
     modalWindow.setContent(Layouts.horizontalSplit().setFirstComponent(list).setSecondComponent(Components.label("Some labe")));
     modalWindow.setVisible(true);
 
-    window.setContent(Components.label("Loaded"));
+    final Menu file = MenuItems.menu("File");
+    file.add(MenuItems.menu("New").add(MenuItems.item("Class")));
+    file.separate();
+    file.add(MenuItems.item("Exit"));
+
+    window.setMenuBar(MenuItems.menuBar().add(file).add(MenuItems.item("Help")));
+
+    final SplitLayout splitLayout = Layouts.horizontalSplit();
+    final TabbedLayout tabbed = Layouts.tabbed();
+
+    final VerticalLayout vertical = Layouts.vertical();
+
+    BooleanValueGroup group = new BooleanValueGroup();
+
+    final RadioButton component = Components.radioButton("Test 1", true);
+    vertical.add(component);
+    final RadioButton component1 = Components.radioButton("Test 2");
+    vertical.add(component1);
+
+    group.add(component).add(component1);
+
+    tabbed.addTab("Hello", vertical);
+
+    final LabeledLayout labeled = Layouts.labeled("Some Panel Label");
+    tabbed.addTab("Hello2", labeled.set(Components.label("test 1")));
+
+    splitLayout.setFirstComponent(Components.label("tree"));
+    splitLayout.setSecondComponent(tabbed);
+    splitLayout.setProportion(20);
+
+    window.setContent(splitLayout);
   }
 }
