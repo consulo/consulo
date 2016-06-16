da25f7b9bd94e68d176906ed4e2a6f82389b509d
VISTALL <vistall.valeriy@gmail.com>
2016-06-16 16:29:43 +0300

    preparing for modal

--- impl/web/src/consulo/web/servlet/AppUIBuilder.java
+++ impl/web/src/consulo/web/servlet/AppUIBuilder.java
@@ -6,6 +6,7 @@ import com.intellij.util.TimeoutUtil;
 import consulo.ui.Components;
 import consulo.ui.Window;
 import consulo.ui.internal.WGwtModalWindowImpl;
+import consulo.ui.shared.Size;
 import consulo.web.AppInit;
 import consulo.web.servlet.ui.UIBuilder;
 import consulo.web.servlet.ui.UIServlet;
@@ -37,9 +38,11 @@ public class AppUIBuilder extends UIBuilder {
       }
     }
 
-    WGwtModalWindowImpl modalWindow = new WGwtModalWindowImpl();
-    modalWindow.setContent(Components.label("Hello World"));
-
     window.setContent(Components.label("Loaded"));
+
+    WGwtModalWindowImpl modalWindow = new WGwtModalWindowImpl();
+    modalWindow.setSize(new Size(777, 460));
+    modalWindow.setContent(Components.label("Hello World"));
+    modalWindow.setVisible(true);
   }
 }
