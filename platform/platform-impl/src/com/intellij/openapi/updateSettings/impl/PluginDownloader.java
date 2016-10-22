45abe97fc62d5f8c968e9c3e5fe5f33432a14948
VISTALL <vistall.valeriy@gmail.com>
2016-10-22 15:15:16 +0300

    simplified plugin downloader

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/PluginDownloader.java
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/PluginDownloader.java
@@ -23,33 +23,25 @@ import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.util.ProgressIndicatorBase;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.util.PathUtil;
 import com.intellij.util.io.HttpRequests;
 import com.intellij.util.io.ZipUtil;
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.net.URLConnection;
-import java.util.List;
 
 /**
  * @author anna
  * @since 10-Aug-2007
  */
 public class PluginDownloader {
-  private static final Logger LOG = Logger.getInstance("#" + PluginDownloader.class.getName());
+  private static final Logger LOG = Logger.getInstance(PluginDownloader.class);
 
-  @NonNls private static final String FILENAME = "filename=";
-
-  private final String myPluginId;
+  private final PluginId myPluginId;
   private String myPluginUrl;
   private String myPluginVersion;
 
@@ -59,17 +51,10 @@ public class PluginDownloader {
   private File myFile;
   private File myOldFile;
   private String myDescription;
-  private PluginId[] myDepends;
-  private PluginId[] myOptionalDepends;
+
   private IdeaPluginDescriptor myDescriptor;
 
-  public PluginDownloader(String pluginId, String pluginUrl, String pluginVersion) {
-    myPluginId = pluginId;
-    myPluginUrl = pluginUrl;
-    myPluginVersion = pluginVersion;
-  }
-
-  public PluginDownloader(String pluginId, String pluginUrl, String pluginVersion, String fileName, String pluginName) {
+  public PluginDownloader(@NotNull PluginId pluginId, String pluginUrl, String pluginVersion, String fileName, String pluginName) {
     myPluginId = pluginId;
     myPluginUrl = pluginUrl;
     myPluginVersion = pluginVersion;
@@ -77,15 +62,11 @@ public class PluginDownloader {
     myPluginName = pluginName;
   }
 
-  public boolean prepareToInstall() throws IOException {
-    return prepareToInstall(new ProgressIndicatorBase());
-  }
-
   public boolean prepareToInstall(ProgressIndicator pi) throws IOException {
     IdeaPluginDescriptor descriptor = null;
-    if (!Boolean.getBoolean(StartupActionScriptManager.STARTUP_WIZARD_MODE) && PluginManager.isPluginInstalled(PluginId.getId(myPluginId))) {
+    if (!Boolean.getBoolean(StartupActionScriptManager.STARTUP_WIZARD_MODE) && PluginManager.isPluginInstalled(myPluginId)) {
       //store old plugins file
-      descriptor = PluginManager.getPlugin(PluginId.getId(myPluginId));
+      descriptor = PluginManager.getPlugin(myPluginId);
       LOG.assertTrue(descriptor != null);
       if (myPluginVersion != null && StringUtil.compareVersionNumbers(descriptor.getVersion(), myPluginVersion) >= 0) {
         LOG.info("Plugin " + myPluginId + ": current version (max) " + myPluginVersion);
@@ -144,11 +125,7 @@ public class PluginDownloader {
       if (file.getName().endsWith(".zip")) {
         final File outputDir = FileUtil.createTempDirectory("plugin", "");
         try {
-          ZipUtil.extract(file, outputDir, new FilenameFilter() {
-            public boolean accept(final File dir, final String name) {
-              return true;
-            }
-          });
+          ZipUtil.extract(file, outputDir, null);
           final File[] files = outputDir.listFiles();
           if (files != null && files.length == 1) {
             descriptor = PluginManagerCore.loadDescriptor(files[0], PluginManagerCore.PLUGIN_XML);
@@ -177,26 +154,18 @@ public class PluginDownloader {
   }
 
   public static void install(final File fromFile, final String pluginName, boolean deleteFromFile) throws IOException {
-    //noinspection HardCodedStringLiteral
-    if (fromFile.getName().endsWith(".jar")) {
-      // add command to copy file to the IDEA/plugins path
-      StartupActionScriptManager.ActionCommand copyPlugin =
-        new StartupActionScriptManager.CopyCommand(fromFile, new File(PathManager.getPluginsPath() + File.separator + fromFile.getName()));
-      StartupActionScriptManager.addActionCommand(copyPlugin);
+    // add command to unzip file to the IDEA/plugins path
+    String unzipPath;
+    if (ZipUtil.isZipContainsFolder(fromFile)) {
+      unzipPath = PathManager.getPluginsPath();
     }
     else {
-      // add command to unzip file to the IDEA/plugins path
-      String unzipPath;
-      if (ZipUtil.isZipContainsFolder(fromFile)) {
-        unzipPath = PathManager.getPluginsPath();
-      }
-      else {
-        unzipPath = PathManager.getPluginsPath() + File.separator + pluginName;
-      }
-
-      StartupActionScriptManager.ActionCommand unzip = new StartupActionScriptManager.UnzipCommand(fromFile, new File(unzipPath));
-      StartupActionScriptManager.addActionCommand(unzip);
+      unzipPath = PathManager.getPluginsPath() + File.separator + pluginName;
     }
+
+    StartupActionScriptManager.ActionCommand unzip = new StartupActionScriptManager.UnzipCommand(fromFile, new File(unzipPath));
+
+    StartupActionScriptManager.addActionCommand(unzip);
 
     // add command to remove temp plugin file
     if (deleteFromFile) {
@@ -215,60 +184,19 @@ public class PluginDownloader {
     indicator.checkCanceled();
     indicator.setText2(IdeBundle.message("progress.downloading.plugin", getPluginName()));
 
-    return HttpRequests.request(myPluginUrl).gzip(false).connect(new HttpRequests.RequestProcessor<File>() {
-      @Override
-      public File process(@NotNull HttpRequests.Request request) throws IOException {
-        request.saveToFile(file, indicator);
+    return HttpRequests.request(myPluginUrl).gzip(false).connect(request -> {
+      request.saveToFile(file, indicator);
 
-        String fileName = guessFileName(request.getConnection(), file);
-        File newFile = new File(file.getParentFile(), fileName);
-        FileUtil.rename(file, newFile);
-        return newFile;
-      }
+      String fileName = getFileName();
+      File newFile = new File(file.getParentFile(), fileName);
+      FileUtil.rename(file, newFile);
+      return newFile;
     });
-  }
-
-  @NotNull
-  private String guessFileName(final URLConnection connection, final File file) throws IOException {
-    String fileName = null;
-
-    final String contentDisposition = connection.getHeaderField("Content-Disposition");
-    LOG.debug("header: " + contentDisposition);
-
-    if (contentDisposition != null && contentDisposition.contains(FILENAME)) {
-      final int startIdx = contentDisposition.indexOf(FILENAME);
-      final int endIdx = contentDisposition.indexOf(';', startIdx);
-      fileName = contentDisposition.substring(startIdx + FILENAME.length(), endIdx > 0 ? endIdx : contentDisposition.length());
-
-      if (StringUtil.startsWithChar(fileName, '\"') && StringUtil.endsWithChar(fileName, '\"')) {
-        fileName = fileName.substring(1, fileName.length() - 1);
-      }
-    }
-
-    if (fileName == null) {
-      // try to find a filename in an URL
-      final String usedURL = connection.getURL().toString();
-      fileName = usedURL.substring(usedURL.lastIndexOf("/") + 1);
-      if (fileName.length() == 0 || fileName.contains("?")) {
-        fileName = myPluginUrl.substring(myPluginUrl.lastIndexOf("/") + 1);
-      }
-    }
-
-    if (!PathUtil.isValidFileName(fileName)) {
-      FileUtil.delete(file);
-      throw new IOException("Invalid filename returned by a server");
-    }
-
-    return fileName;
-  }
-
-  public String getPluginId() {
-    return myPluginId;
   }
 
   public String getFileName() {
     if (myFileName == null) {
-      myFileName = myPluginUrl.substring(myPluginUrl.lastIndexOf("/") + 1);
+      myFileName = myPluginId + ".zip";
     }
     return myFileName;
   }
@@ -292,25 +220,11 @@ public class PluginDownloader {
     return myDescription;
   }
 
-  public void setDepends(List<PluginId> depends, List<PluginId> optionalDependsPlugins) {
-    myDepends = depends.isEmpty() ? PluginId.EMPTY_ARRAY : depends.toArray(new PluginId[depends.size()]);
-    myDepends = optionalDependsPlugins.isEmpty() ? PluginId.EMPTY_ARRAY : optionalDependsPlugins.toArray(new PluginId[optionalDependsPlugins.size()]);
-  }
-
-  @NotNull
-  public PluginId[] getDepends() {
-    return myDepends == null ? PluginId.EMPTY_ARRAY : myDepends;
-  }
-
-  @NotNull
-  public PluginId[] getOptionalDepends() {
-    return myOptionalDepends == null ? PluginId.EMPTY_ARRAY : myOptionalDepends;
-  }
-
   public static PluginDownloader createDownloader(IdeaPluginDescriptor descriptor) {
-    String url = RepositoryHelper.buildUrlForDownload(consulo.ide.updateSettings.UpdateSettings.getInstance().getChannel(), descriptor.getPluginId().toString());
+    String url =
+            RepositoryHelper.buildUrlForDownload(consulo.ide.updateSettings.UpdateSettings.getInstance().getChannel(), descriptor.getPluginId().toString());
 
-    PluginDownloader downloader = new PluginDownloader(descriptor.getPluginId().getIdString(), url, null, null, descriptor.getName());
+    PluginDownloader downloader = new PluginDownloader(descriptor.getPluginId(), url, null, null, descriptor.getName());
     downloader.setDescriptor(descriptor);
     return downloader;
   }
