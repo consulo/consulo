c1ada9c98ec93b615210d6e108f090916505d975
VISTALL <vistall.valeriy@gmail.com>
2017-06-20 07:14:35 +0300

    merge from idea

--- modules/analysis-api/src/com/intellij/codeInsight/daemon/ImplicitUsageProvider.java
+++ modules/analysis-api/src/com/intellij/codeInsight/daemon/ImplicitUsageProvider.java
@@ -18,18 +18,42 @@ package com.intellij.codeInsight.daemon;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Allows to disable highlighting of certain elements as unused when such elements are not referenced
- * from the code but are referenced in some other way (for example, from generated code).
+ * from the code but are referenced in some other way. For example,
+ * <ul>
+ * <li>from generated code</li>
+ * <li>from outside containers: {@code @javax.servlet.annotation.WebServlet public class MyServlet {}}</li>
+ * <li>from some frameworks: {@code @javax.ejb.EJB private DataStore myInjectedDataStore;}</li> etc
+ * </ul>
  *
  * @author yole
  * @since 6.0
  */
 public interface ImplicitUsageProvider {
-  ExtensionPointName<ImplicitUsageProvider> EP_NAME = new ExtensionPointName<ImplicitUsageProvider>("com.intellij.implicitUsageProvider");
+  ExtensionPointName<ImplicitUsageProvider> EP_NAME = new ExtensionPointName<>("com.intellij.implicitUsageProvider");
 
+  /**
+   * @return true if element should not be reported as unused
+   */
   boolean isImplicitUsage(PsiElement element);
+
+  /**
+   * @return true if element should not be reported as "assigned but not used"
+   */
   boolean isImplicitRead(PsiElement element);
+
+  /**
+   * @return true if element should not be reported as "referenced but never assigned"
+   */
   boolean isImplicitWrite(PsiElement element);
+
+  /**
+   * @return true if the given element is implicitly initialized to a non-null value
+   */
+  default boolean isImplicitlyNotNullInitialized(@NotNull PsiElement element) {
+    return false;
+  }
 }
