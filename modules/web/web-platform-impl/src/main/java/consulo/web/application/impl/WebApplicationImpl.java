322aa2790a573885ab5875904e7c6df153588898
VISTALL <vistall.valeriy@gmail.com>
2017-09-24 17:29:14 +0300

    testing

--- modules/web/web-platform-impl/src/main/java/consulo/web/application/impl/WebApplicationImpl.java
+++ modules/web/web-platform-impl/src/main/java/consulo/web/application/impl/WebApplicationImpl.java
@@ -1,11 +1,17 @@
 package consulo.web.application.impl;
 
 import com.intellij.openapi.application.impl.ApplicationImpl;
+import com.intellij.openapi.progress.EmptyProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.project.Project;
 import com.intellij.ui.Splash;
+import consulo.annotations.RequiredDispatchThread;
 import consulo.web.application.WebApplication;
 import consulo.web.application.WebSession;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
 
 /**
  * @author VISTALL
@@ -16,6 +22,18 @@ public class WebApplicationImpl extends ApplicationImpl implements WebApplicatio
 
   public WebApplicationImpl(boolean isInternal, boolean isUnitTestMode, boolean isHeadless, boolean isCommandLine, @NotNull String appName, @Nullable Splash splash) {
     super(isInternal, isUnitTestMode, isHeadless, isCommandLine, appName, splash);
+  }
+
+  @RequiredDispatchThread
+  @Override
+  public boolean runProcessWithProgressSynchronously(@NotNull Runnable process,
+                                                     @NotNull String progressTitle,
+                                                     boolean canBeCanceled,
+                                                     @Nullable Project project,
+                                                     JComponent parentComponent,
+                                                     String cancelText) {
+    ProgressManager.getInstance().runProcess(process, new EmptyProgressIndicator());
+    return true;
   }
 
   @Override
