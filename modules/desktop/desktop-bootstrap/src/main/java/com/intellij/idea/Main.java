19dd1d679e828ef034b9000b1224cc3aed54f6f4
VISTALL <vistall.valeriy@gmail.com>
2017-11-16 19:34:34 +0300

    do not use util methods

--- modules/desktop/desktop-bootstrap/src/main/java/com/intellij/idea/Main.java
+++ modules/desktop/desktop-bootstrap/src/main/java/com/intellij/idea/Main.java
@@ -17,8 +17,6 @@ package com.intellij.idea;
 
 import com.intellij.ide.Bootstrap;
 import com.intellij.openapi.util.SystemInfo;
-import com.intellij.util.ui.JBUI;
-import com.intellij.util.ui.UIUtil;
 import consulo.application.ApplicationProperties;
 
 import javax.swing.*;
@@ -134,14 +132,14 @@ public class Main {
         JTextPane textPane = new JTextPane();
         textPane.setEditable(false);
         textPane.setText(message.replaceAll("\t", "    "));
-        textPane.setBackground(UIUtil.getPanelBackground());
+        textPane.setBackground(UIManager.getColor("Panel.background"));
         textPane.setCaretPosition(0);
         JScrollPane scrollPane =
                 new JScrollPane(textPane, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setBorder(null);
 
-        int maxHeight = Math.min(JBUI.scale(600), Toolkit.getDefaultToolkit().getScreenSize().height - 150);
-        int maxWidth = Math.min(JBUI.scale(600), Toolkit.getDefaultToolkit().getScreenSize().width - 150);
+        int maxHeight = Toolkit.getDefaultToolkit().getScreenSize().height / 2;
+        int maxWidth = Toolkit.getDefaultToolkit().getScreenSize().width / 2;
         Dimension component = scrollPane.getPreferredSize();
         if (component.height > maxHeight || component.width > maxWidth) {
           scrollPane.setPreferredSize(new Dimension(Math.min(maxWidth, component.width), Math.min(maxHeight, component.height)));
