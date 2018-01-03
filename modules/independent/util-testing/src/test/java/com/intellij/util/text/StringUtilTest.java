17f899d8c661b32d03d6b33e4c6feb8e6f2304e7
Valeriy Semenchuk <vistall.valeriy@gmail.com>
2018-01-03 21:40:11 +0300

    natural order comparator

--- modules/independent/util-testing/src/test/java/com/intellij/util/text/StringUtilTest.java
+++ modules/independent/util-testing/src/test/java/com/intellij/util/text/StringUtilTest.java
@@ -15,17 +15,21 @@
  */
 package com.intellij.util.text;
 
+import com.intellij.openapi.util.text.NaturalComparator;
 import com.intellij.openapi.util.text.StringUtil;
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.nio.CharBuffer;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Eugene Zhuravlev
  *         Date: Dec 22, 2006
  */
-public class StringUtilTest extends TestCase {
+public class StringUtilTest extends Assert {
+  @Test
   public void testToUpperCase() {
     assertEquals('/', StringUtil.toUpperCase('/'));
     assertEquals(':', StringUtil.toUpperCase(':'));
@@ -37,6 +41,7 @@ public class StringUtilTest extends TestCase {
     assertEquals('\u2567', StringUtil.toUpperCase(Character.toLowerCase('\u2567')));
   }
 
+  @Test
   public void testToLowerCase() {
     assertEquals('/', StringUtil.toLowerCase('/'));
     assertEquals(':', StringUtil.toLowerCase(':'));
@@ -48,6 +53,7 @@ public class StringUtilTest extends TestCase {
     assertEquals('\u2567', StringUtil.toUpperCase(Character.toLowerCase('\u2567')));
   }
 
+  @Test
   public void testIsEmptyOrSpaces() throws Exception {
     assertTrue(StringUtil.isEmptyOrSpaces(null));
     assertTrue(StringUtil.isEmptyOrSpaces(""));
@@ -58,6 +64,7 @@ public class StringUtilTest extends TestCase {
     assertFalse(StringUtil.isEmptyOrSpaces("test"));
   }
 
+  @Test
   public void testSplitWithQuotes() {
     final List<String> strings = StringUtil.splitHonorQuotes("aaa bbb   ccc \"ddd\" \"e\\\"e\\\"e\"  ", ' ');
     assertEquals(5, strings.size());
@@ -68,11 +75,13 @@ public class StringUtilTest extends TestCase {
     assertEquals("\"e\\\"e\\\"e\"", strings.get(4));
   }
 
+  @Test
   public void testUnpluralize() {
     assertEquals("s", StringUtil.unpluralize("s"));
     assertEquals("z", StringUtil.unpluralize("zs"));
   }
 
+  @Test
   public void testStartsWithConcatenation() {
     assertTrue(StringUtil.startsWithConcatenation("something.withdot", "something", "."));
     assertTrue(StringUtil.startsWithConcatenation("something.withdot", "", "something."));
@@ -82,34 +91,59 @@ public class StringUtilTest extends TestCase {
     assertFalse(StringUtil.startsWithConcatenation("some", "something", ""));
   }
 
-  public void testNaturalCompare() {
-    final List<String> strings = new ArrayList<String>(Arrays.asList("Test99", "tes0", "test0", "testing", "test", "test99", "test011", "test1",
-                                                             "test 3", "test2", "test10a", "test10", "1.2.10.5", "1.2.9.1"));
-    final Comparator<String> c = new Comparator<String>() {
-      @Override
-      public int compare(String o1, String o2) {
-        return StringUtil.naturalCompare(o1, o2);
-      }
-    };
-    Collections.sort(strings, c);
-    assertEquals(Arrays.asList("1.2.9.1", "1.2.10.5", "tes0", "test", "test0", "test1", "test2", "test 3", "test10", "test10a",
-                               "test011", "Test99", "test99", "testing"), strings);
-    final List<String> strings2 = new ArrayList<String>(Arrays.asList("t1", "t001", "T2", "T002", "T1", "t2"));
-    Collections.sort(strings2, c);
-    assertEquals(Arrays.asList("T1", "t1", "t001", "T2", "t2", "T002"), strings2);
-    assertEquals(1 ,StringUtil.naturalCompare("7403515080361171695", "07403515080361171694"));
-    assertEquals(-14, StringUtil.naturalCompare("_firstField", "myField1"));
-    //idea-80853
-    final List<String> strings3 = new ArrayList<String>(
-      Arrays.asList("C148A_InsomniaCure", "C148B_Escape", "C148C_TersePrincess", "C148D_BagOfMice", "C148E_Porcelain"));
-    Collections.sort(strings3, c);
-    assertEquals(Arrays.asList("C148A_InsomniaCure", "C148B_Escape", "C148C_TersePrincess", "C148D_BagOfMice", "C148E_Porcelain"), strings3);
+  @Test
+  public void testNaturalCompareTransitivity() {
+    String s1 = "#";
+    String s2 = "0b";
+    String s3 = " 0b";
+    assertTrue(StringUtil.naturalCompare(s1, s2) < 0);
+    assertTrue(StringUtil.naturalCompare(s2, s3) < 0);
+    assertTrue("non-transitive", StringUtil.naturalCompare(s1, s3) < 0);
   }
 
+  @Test
+  public void testNaturalCompareStability() {
+    assertTrue(StringUtil.naturalCompare("01a1", "1a01") != StringUtil.naturalCompare("1a01", "01a1"));
+    assertTrue(StringUtil.naturalCompare("#01A", "# 1A") != StringUtil.naturalCompare("# 1A", "#01A"));
+    assertTrue(StringUtil.naturalCompare("aA", "aa") != StringUtil.naturalCompare("aa", "aA"));
+  }
+
+  @Test
+  public void testNaturalCompare() {
+
+    final List<String> numbers = Arrays.asList("1a000001", "000001a1", "001a0001", "0001A001", "00001a01", "01a00001");
+    numbers.sort(NaturalComparator.INSTANCE);
+    assertEquals(Arrays.asList("1a000001", "01a00001", "001a0001", "0001A001", "00001a01", "000001a1"), numbers);
+
+    final List<String> test = Arrays.asList("test011", "test10", "test10a", "test010");
+    test.sort(NaturalComparator.INSTANCE);
+    assertEquals(Arrays.asList("test10", "test10a", "test010", "test011"), test);
+
+    final List<String> strings = Arrays.asList("Test99", "tes0", "test0", "testing", "test", "test99", "test011", "test1", "test 3", "test2", "test10a", "test10", "1.2.10.5", "1.2.9.1");
+    strings.sort(NaturalComparator.INSTANCE);
+    assertEquals(Arrays.asList("1.2.9.1", "1.2.10.5", "tes0", "test", "test0", "test1", "test2", "test 3", "test10", "test10a", "test011", "Test99", "test99", "testing"), strings);
+
+    final List<String> strings2 = Arrays.asList("t1", "t001", "T2", "T002", "T1", "t2");
+    strings2.sort(NaturalComparator.INSTANCE);
+    assertEquals(Arrays.asList("T1", "t1", "t001", "T2", "t2", "T002"), strings2);
+    assertEquals(1, StringUtil.naturalCompare("7403515080361171695", "07403515080361171694"));
+    assertEquals(-14, StringUtil.naturalCompare("_firstField", "myField1"));
+    //idea-80853
+    final List<String> strings3 = Arrays.asList("C148A_InsomniaCure", "C148B_Escape", "C148C_TersePrincess", "C148D_BagOfMice", "C148E_Porcelain");
+    strings3.sort(NaturalComparator.INSTANCE);
+    assertEquals(Arrays.asList("C148A_InsomniaCure", "C148B_Escape", "C148C_TersePrincess", "C148D_BagOfMice", "C148E_Porcelain"), strings3);
+
+    final List<String> l = Arrays.asList("a0002", "a0 2", "a001");
+    l.sort(NaturalComparator.INSTANCE);
+    assertEquals(Arrays.asList("a0 2", "a001", "a0002"), l);
+  }
+
+  @Test
   public void testFormatLinks() {
     assertEquals("<a href=\"http://a-b+c\">http://a-b+c</a>", StringUtil.formatLinks("http://a-b+c"));
   }
-  
+
+  @Test
   public void testCopyHeapCharBuffer() {
     String s = "abcde";
     CharBuffer buffer = CharBuffer.allocate(s.length());
@@ -122,17 +156,20 @@ public class StringUtilTest extends TestCase {
     assertNull(CharArrayUtil.fromSequenceWithoutCopying(buffer.subSequence(1, 5)));
     assertNull(CharArrayUtil.fromSequenceWithoutCopying(buffer.subSequence(1, 2)));
   }
-  
+
+  @Test
   public void testTitleCase() {
     assertEquals("Couldn't Connect to Debugger", StringUtil.wordsToBeginFromUpperCase("Couldn't connect to debugger"));
   }
 
+  @Test
   public void testEscapeStringCharacters() {
     assertEquals("\\\"\\n", StringUtil.escapeStringCharacters(3, "\\\"\n", "\"", false, new StringBuilder()).toString());
     assertEquals("\\\"\\n", StringUtil.escapeStringCharacters(2, "\"\n", "\"", false, new StringBuilder()).toString());
     assertEquals("\\\\\\\"\\n", StringUtil.escapeStringCharacters(3, "\\\"\n", "\"", true, new StringBuilder()).toString());
   }
 
+  @Test
   public void testEscapeSlashes() {
     assertEquals("\\/", StringUtil.escapeSlashes("/"));
     assertEquals("foo\\/bar\\foo\\/", StringUtil.escapeSlashes("foo/bar\\foo/"));
@@ -140,6 +177,7 @@ public class StringUtilTest extends TestCase {
     assertEquals("\\\\\\\\server\\\\share\\\\extension.crx", StringUtil.escapeBackSlashes("\\\\server\\share\\extension.crx"));
   }
 
+  @Test
   public void testEscapeQuotes() {
     assertEquals("\\\"", StringUtil.escapeQuotes("\""));
     assertEquals("foo\\\"bar'\\\"", StringUtil.escapeQuotes("foo\"bar'\""));
