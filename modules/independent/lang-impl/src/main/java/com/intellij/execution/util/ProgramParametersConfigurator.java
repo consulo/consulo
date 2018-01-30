fe65bca77b1e55dd049f6257fb7c538c266eec6e
Valeriy Semenchuk <vistall.valeriy@gmail.com>
2018-01-30 18:44:27 +0300

    some merge from idea

--- modules/independent/lang-impl/src/main/java/com/intellij/execution/util/ProgramParametersConfigurator.java
+++ modules/independent/lang-impl/src/main/java/com/intellij/execution/util/ProgramParametersConfigurator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,19 +20,29 @@ import com.intellij.execution.configurations.ModuleBasedConfiguration;
 import com.intellij.execution.configurations.RuntimeConfigurationWarning;
 import com.intellij.execution.configurations.SimpleProgramParameters;
 import com.intellij.openapi.components.PathMacroManager;
+import com.intellij.openapi.components.PathMacroUtil;
+import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.WorkingDirectoryProvider;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.EnvironmentUtil;
 import com.intellij.util.PathUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.SystemIndependent;
 
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProgramParametersConfigurator {
+  private static final ExtensionPointName<WorkingDirectoryProvider> WORKING_DIRECTORY_PROVIDER_EP_NAME = ExtensionPointName.create("com.intellij.module.workingDirectoryProvider");
+  public static final String MODULE_WORKING_DIR = "%MODULE_WORKING_DIR%";
+
   public void configureConfiguration(SimpleProgramParameters parameters, CommonProgramRunConfigurationParameters configuration) {
     Project project = configuration.getProject();
     Module module = getModule(configuration);
@@ -55,13 +65,22 @@ public class ProgramParametersConfigurator {
   public String getWorkingDir(CommonProgramRunConfigurationParameters configuration, Project project, Module module) {
     String workingDirectory = configuration.getWorkingDirectory();
     String defaultWorkingDir = getDefaultWorkingDir(project);
-
-    if (workingDirectory == null || workingDirectory.trim().length() == 0) {
+    if (StringUtil.isEmptyOrSpaces(workingDirectory)) {
       workingDirectory = defaultWorkingDir;
+      if (workingDirectory == null) {
+        return null;
+      }
     }
-    if (workingDirectory == null) return null;
     workingDirectory = expandPath(workingDirectory, module, project);
     if (!FileUtil.isAbsolute(workingDirectory) && defaultWorkingDir != null) {
+      if (("$" + PathMacroUtil.MODULE_DIR_MACRO_NAME + "$").equals(workingDirectory)) {
+        return defaultWorkingDir;
+      }
+
+      if (module != null && MODULE_WORKING_DIR.equals(workingDirectory)) {
+        String workingDir = getDefaultWorkingDir(module);
+        if (workingDir != null) return workingDir;
+      }
       workingDirectory = defaultWorkingDir + "/" + workingDirectory;
     }
     return workingDirectory;
@@ -72,8 +91,20 @@ public class ProgramParametersConfigurator {
     return PathUtil.getLocalPath(project.getBaseDir());
   }
 
-  public void checkWorkingDirectoryExist(CommonProgramRunConfigurationParameters configuration, Project project, Module module)
-          throws RuntimeConfigurationWarning {
+  @Nullable
+  protected String getDefaultWorkingDir(@NotNull Module module) {
+    for (WorkingDirectoryProvider provider : WORKING_DIRECTORY_PROVIDER_EP_NAME.getExtensions()) {
+      @SystemIndependent String path = provider.getWorkingDirectoryPath(module);
+      if (path != null) return path;
+    }
+    VirtualFile[] roots = ModuleRootManager.getInstance(module).getContentRoots();
+    if (roots.length > 0) {
+      return PathUtil.getLocalPath(roots[0]);
+    }
+    return null;
+  }
+
+  public void checkWorkingDirectoryExist(CommonProgramRunConfigurationParameters configuration, Project project, Module module) throws RuntimeConfigurationWarning {
     final String workingDir = getWorkingDir(configuration, project, module);
     if (workingDir == null) {
       throw new RuntimeConfigurationWarning("Working directory is null for " +
@@ -82,18 +113,15 @@ public class ProgramParametersConfigurator {
                                             "' (" +
                                             project.getBasePath() +
                                             ")" +
-                                            ", module '" +
-                                            module.getName() +
-                                            "' (" +
-                                            module.getModuleDirPath() +
-                                            ")");
+                                            ", module " +
+                                            (module == null ? "null" : "'" + module.getName() + "' (" + module.getModuleDirPath() + ")"));
     }
     if (!new File(workingDir).exists()) {
       throw new RuntimeConfigurationWarning("Working directory '" + workingDir + "' doesn't exist");
     }
   }
 
-  protected String expandPath(String path, Module module, Project project) {
+  protected String expandPath(@Nullable String path, Module module, Project project) {
     path = PathMacroManager.getInstance(project).expandPath(path);
     if (module != null) {
       path = PathMacroManager.getInstance(module).expandPath(path);
