bed80b4ea6ba6f51edd82836eddb8b29d0d83768
Valery Semenchuk <vistall.valeriy@gmail.com>
2018-09-01 10:16:42 +0300

    make public

--- modules/independent/vcs-impl/src/main/java/com/intellij/openapi/vcs/changes/ChangeListManagerImpl.java
+++ modules/independent/vcs-impl/src/main/java/com/intellij/openapi/vcs/changes/ChangeListManagerImpl.java
@@ -1475,7 +1475,7 @@ public class ChangeListManagerImpl extends ChangeListManagerEx implements Projec
     return ContainerUtil.exists(IgnoredFileProvider.IGNORE_FILE.getExtensions(), it -> it.isIgnoredFile(myProject, filePath));
   }
 
-  private static class DefaultIgnoredFileProvider implements IgnoredFileProvider {
+  public static class DefaultIgnoredFileProvider implements IgnoredFileProvider {
     @Override
     public boolean isIgnoredFile(@Nonnull Project project, @Nonnull FilePath filePath) {
       return ((ChangeListManagerImpl)ChangeListManager.getInstance(project)).myIgnoredIdeaLevel.isIgnoredFile(filePath);
