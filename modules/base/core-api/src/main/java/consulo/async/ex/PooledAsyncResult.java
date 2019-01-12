1d804fba534871bb6348f78a16270ed237cc274b
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-01-13 01:37:01 +0300

    intro PooledAsyncResult

---
+++ modules/base/core-api/src/main/java/consulo/async/ex/PooledAsyncResult.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013-2019 consulo.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package consulo.async.ex;
+
+import com.intellij.openapi.util.AsyncResult;
+import com.intellij.util.Consumer;
+import com.intellij.util.concurrency.AppExecutorUtil;
+
+import javax.annotation.Nonnull;
+import java.util.concurrent.Callable;
+
+/**
+ * @author VISTALL
+ * @since 2019-01-12
+ */
+public class PooledAsyncResult {
+  @Nonnull
+  public static <V> AsyncResult<V> create(@Nonnull Callable<V> callable) {
+    AsyncResult<V> result = new AsyncResult<>();
+    AppExecutorUtil.getAppExecutorService().execute(() -> {
+      try {
+        result.setDone(callable.call());
+      }
+      catch (Throwable e) {
+        result.rejectWithThrowable(e);
+      }
+    });
+    return result;
+  }
+
+  @Nonnull
+  public static <V> AsyncResult<V> create(@Nonnull Consumer<AsyncResult<V>> consumer) {
+    AsyncResult<V> result = new AsyncResult<>();
+    AppExecutorUtil.getAppExecutorService().execute(() -> consumer.consume(result));
+    return result;
+  }
+}
