71ce7cd940503946f86841ff0f6543e451f54c12
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-02-03 16:30:26 +0300

    remove wrapper2 due package duplication from platform code

--- modules/desktop/desktop-ui-impl/src/main/java/consulo/ui/desktop/internal/DesktopWrappedLayoutImpl.java
+++ modules/desktop/desktop-ui-impl/src/main/java/consulo/ui/desktop/internal/DesktopWrappedLayoutImpl.java
@@ -15,25 +15,106 @@
  */
 package consulo.ui.desktop.internal;
 
-import com.intellij.ui.components.panels.Wrapper2;
+import com.intellij.openapi.wm.IdeFocusManager;
 import consulo.awt.TargetAWT;
 import consulo.ui.Component;
 import consulo.ui.RequiredUIAccess;
 import consulo.ui.WrappedLayout;
-import javax.annotation.Nonnull;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author VISTALL
  * @since 25-Oct-17
  */
-public class DesktopWrappedLayoutImpl extends Wrapper2 implements WrappedLayout, SwingWrapper {
+public class DesktopWrappedLayoutImpl extends JPanel implements WrappedLayout, SwingWrapper {
+  public DesktopWrappedLayoutImpl() {
+    setLayout(new BorderLayout());
+    setOpaque(false);
+  }
+
+  public DesktopWrappedLayoutImpl(JComponent wrapped) {
+    setLayout(new BorderLayout());
+    add(wrapped, BorderLayout.CENTER);
+    setOpaque(false);
+  }
+
+  public DesktopWrappedLayoutImpl(LayoutManager layout, JComponent wrapped) {
+    super(layout);
+    add(wrapped);
+    setOpaque(false);
+  }
+
+  public DesktopWrappedLayoutImpl(boolean isDoubleBuffered) {
+    super(isDoubleBuffered);
+    setOpaque(false);
+  }
+
+  public DesktopWrappedLayoutImpl(LayoutManager layout) {
+    super(layout);
+    setOpaque(false);
+  }
+
+  public DesktopWrappedLayoutImpl(LayoutManager layout, boolean isDoubleBuffered) {
+    super(layout, isDoubleBuffered);
+    setOpaque(false);
+  }
+
   @RequiredUIAccess
   @Nonnull
   @Override
-  public WrappedLayout set(@javax.annotation.Nullable Component component) {
+  public WrappedLayout set(@Nullable Component component) {
     setContent(component == null ? null : (JComponent)TargetAWT.to(component));
     return this;
+  }
+
+  public void setContent(JComponent wrapped) {
+    if (wrapped == getTargetComponent()) {
+      return;
+    }
+
+    removeAll();
+    setLayout(new BorderLayout());
+    if (wrapped != null) {
+      add(wrapped, BorderLayout.CENTER);
+    }
+    validate();
+  }
+
+  @Override
+  public void requestFocus() {
+    if (getTargetComponent() == this) {
+      IdeFocusManager.getGlobalInstance().doWhenFocusSettlesDown(super::requestFocus);
+      return;
+    }
+    IdeFocusManager.getGlobalInstance().doForceFocusWhenFocusSettlesDown(getTargetComponent());
+  }
+
+  @Override
+  public boolean requestFocusInWindow() {
+    if (getTargetComponent() == this) {
+      return super.requestFocusInWindow();
+    }
+    return getTargetComponent().requestFocusInWindow();
+  }
+
+  @Override
+  public final boolean requestFocus(boolean temporary) {
+    if (getTargetComponent() == this) {
+      return super.requestFocus(temporary);
+    }
+    return getTargetComponent().requestFocus(temporary);
+  }
+
+  public JComponent getTargetComponent() {
+    if (getComponentCount() == 1) {
+      return (JComponent)getComponent(0);
+    }
+    else {
+      return this;
+    }
   }
 }
