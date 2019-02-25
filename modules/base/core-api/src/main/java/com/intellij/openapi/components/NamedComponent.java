f4f4690a278750633ae36deaf930aa842a4d3c1e
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-02-25 16:26:11 +0300

    deprecated

--- modules/base/core-api/src/main/java/com/intellij/openapi/components/NamedComponent.java
+++ modules/base/core-api/src/main/java/com/intellij/openapi/components/NamedComponent.java
@@ -16,8 +16,10 @@
 package com.intellij.openapi.components;
 
 import org.jetbrains.annotations.NonNls;
+
 import javax.annotation.Nonnull;
 
+@Deprecated
 public interface NamedComponent {
   /**
    * Unique name of this component. If there is another component with the same name or
