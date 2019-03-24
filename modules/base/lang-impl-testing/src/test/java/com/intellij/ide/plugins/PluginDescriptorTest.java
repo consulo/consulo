56b8b9b9f22716c14ead0ddc1df6f61ffdef1e16
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-03-24 15:33:38 +0300

    drop asp.jar since it's internal JB plugin (which is not opened)
    fix compilation

--- modules/base/lang-impl-testing/src/test/java/com/intellij/ide/plugins/PluginDescriptorTest.java
+++ modules/base/lang-impl-testing/src/test/java/com/intellij/ide/plugins/PluginDescriptorTest.java
@@ -3,6 +3,7 @@ package com.intellij.ide.plugins;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.net.URL;
 import java.nio.file.Paths;
 
@@ -13,10 +14,11 @@ import java.nio.file.Paths;
 public class PluginDescriptorTest extends Assert {
   @Test
   public void testDescriptorLoading() throws Exception {
-    URL url = PluginDescriptorTest.class.getResource("/ide/plugins/pluginDescriptor/asp.jar");
+    URL url = PluginDescriptorTest.class.getResource("/ide/plugins/pluginDescriptor/consulo.nunit-2-SNAPSHOT.jar");
     assertNotNull(url);
 
-    IdeaPluginDescriptorImpl descriptor = PluginManagerCore.loadDescriptorFromJar(Paths.get(url.toURI()).toFile(), true);
+    File jarFile = Paths.get(url.toURI()).toFile();
+    IdeaPluginDescriptorImpl descriptor = PluginManagerCore.loadDescriptorFromJar(jarFile, jarFile, PluginManagerCore.PLUGIN_XML, true, false);
     assertNotNull(descriptor);
   }
 }
