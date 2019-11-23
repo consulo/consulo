17a6706184742df09df222de854ff75f7ab98c7f
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-11-23 20:03:12 +0300

    module file

---
+++ modules/boot/util-rt/src/main/module-info.java
@@ -0,0 +1,15 @@
+module consulo.util.nodep {
+  requires java.base;
+  requires java.xml;
+
+  exports consulo.util.nodep;
+  exports consulo.util.nodep.classloader;
+  exports consulo.util.nodep.collection;
+  exports consulo.util.nodep.function;
+  exports consulo.util.nodep.io;
+  exports consulo.util.nodep.map;
+  exports consulo.util.nodep.reference;
+  exports consulo.util.nodep.text;
+  exports consulo.util.nodep.xml;
+  exports consulo.util.nodep.xml.node;
+}
