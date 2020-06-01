d8b679084165ebfe1baacd00796b587341f6332a
VISTALL <vistall.valeriy@gmail.com>
2020-06-01 22:31:15 +0300

    merge from idea

--- modules/base/platform-impl/src/main/java/com/intellij/openapi/progress/util/StatusBarProgress.java
+++ modules/base/platform-impl/src/main/java/com/intellij/openapi/progress/util/StatusBarProgress.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.progress.util;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -21,17 +7,18 @@ import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.WindowManager;
-import com.intellij.util.containers.ContainerUtil;
+import consulo.util.lang.ObjectUtil;
 
 import javax.swing.*;
+import java.util.HashMap;
 import java.util.Map;
 
 import static com.intellij.openapi.util.Pair.pair;
-import static com.intellij.util.ObjectUtils.notNull;
 
 public class StatusBarProgress extends ProgressIndicatorBase {
   // statusBar -> [textToRestore, MyPreviousText]
-  private final Map<StatusBar, Pair<String, String>> myStatusBar2SavedText = ContainerUtil.newHashMap();
+  private final Map<StatusBar, Pair<String, String>> myStatusBar2SavedText = new HashMap<>();
+  private boolean myScheduledStatusBarTextSave;
 
   public StatusBarProgress() {
     super(true);
@@ -39,47 +26,24 @@ public class StatusBarProgress extends ProgressIndicatorBase {
 
   @Override
   public void start() {
+    myScheduledStatusBarTextSave = false;
     super.start();
-    //noinspection SSBasedInspection
-    SwingUtilities.invokeLater(
-            new Runnable() {
-              @Override
-              public void run() {
-                if (ApplicationManager.getApplication().isDisposed()) return;
-                WindowManager windowManager = WindowManager.getInstance();
-                if (windowManager == null) return;
-
-                Project[] projects = ProjectManager.getInstance().getOpenProjects();
-                if (projects.length == 0) projects = new Project[]{null};
-
-                for (Project project : projects) {
-                  StatusBar statusBar = windowManager.getStatusBar(project);
-                  if (statusBar != null) {
-                    String info = notNull(statusBar.getInfo(), "");
-                    myStatusBar2SavedText.put(statusBar, pair(info, info));  // initial value
-                  }
-                }
-              }
-            }
-    );
   }
 
   @Override
   public void stop() {
     super.stop();
-    //noinspection SSBasedInspection
-    SwingUtilities.invokeLater(
-            new Runnable() {
-              @Override
-              public void run() {
-                for (StatusBar statusBar : myStatusBar2SavedText.keySet()) {
-                  String textToRestore = updateRestoreText(statusBar);
-                  statusBar.setInfo(textToRestore);
-                }
-                myStatusBar2SavedText.clear();
-              }
-            }
-    );
+
+    if (myScheduledStatusBarTextSave) {
+      //noinspection SSBasedInspection
+      SwingUtilities.invokeLater(() -> {
+        for (StatusBar statusBar : myStatusBar2SavedText.keySet()) {
+          String textToRestore = updateRestoreText(statusBar);
+          statusBar.setInfo(textToRestore);
+        }
+        myStatusBar2SavedText.clear();
+      });
+    }
   }
 
   @Override
@@ -107,17 +71,32 @@ public class StatusBarProgress extends ProgressIndicatorBase {
       }
     }
     final String _text = text;
+    if (!myScheduledStatusBarTextSave) {
+      myScheduledStatusBarTextSave = true;
+      //noinspection SSBasedInspection
+      SwingUtilities.invokeLater(() -> {
+        if (ApplicationManager.getApplication().isDisposed()) return;
+        WindowManager windowManager = WindowManager.getInstance();
+        if (windowManager == null) return;
+
+        Project[] projects = ProjectManager.getInstance().getOpenProjects();
+        if (projects.length == 0) projects = new Project[]{null};
+
+        for (Project project : projects) {
+          StatusBar statusBar = windowManager.getStatusBar(project);
+          if (statusBar != null) {
+            String info = ObjectUtil.notNull(statusBar.getInfo(), "");
+            myStatusBar2SavedText.put(statusBar, pair(info, info));  // initial value
+          }
+        }
+      });
+    }
     //noinspection SSBasedInspection
-    SwingUtilities.invokeLater(
-            new Runnable() {
-              @Override
-              public void run() {
-                for (StatusBar statusBarEx : myStatusBar2SavedText.keySet()) {
-                  setStatusBarText(statusBarEx, _text);
-                }
-              }
-            }
-    );
+    SwingUtilities.invokeLater(() -> {
+      for (StatusBar statusBarEx : myStatusBar2SavedText.keySet()) {
+        setStatusBarText(statusBarEx, _text);
+      }
+    });
   }
 
   private void setStatusBarText(StatusBar statusBar, String text) {
@@ -130,7 +109,7 @@ public class StatusBarProgress extends ProgressIndicatorBase {
   private String updateRestoreText(StatusBar statusBar) {
     Pair<String, String> textsPair = myStatusBar2SavedText.get(statusBar);
     // if current status bar info doesn't match the value, that we set, use this value as a restore value
-    String info = notNull(statusBar.getInfo(), "");
+    String info = ObjectUtil.notNull(statusBar.getInfo(), "");
     if (!textsPair.getSecond().equals(info)) {
       myStatusBar2SavedText.put(statusBar, pair(info, textsPair.second));
     }
