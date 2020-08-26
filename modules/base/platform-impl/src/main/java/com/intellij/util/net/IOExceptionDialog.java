604932d9ed64df15ee3aa96021e579fe9c882cbd
VISTALL <vistall.valeriy@gmail.com>
2020-08-26 14:27:52 +0300

    Revert "drop old io exception dialog"
    This reverts commit 48d33bf0fee6f5b304fa73ac783d942ecda932ec.

---
+++ modules/base/platform-impl/src/main/java/com/intellij/util/net/IOExceptionDialog.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.util.net;
+
+import com.intellij.CommonBundle;
+import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.util.Ref;
+import com.intellij.ui.GuiUtils;
+import com.intellij.util.ObjectUtil;
+import consulo.logging.Logger;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.lang.reflect.InvocationTargetException;
+
+public class IOExceptionDialog extends DialogWrapper {
+  private static final Logger LOG = Logger.getInstance(IOExceptionDialog.class);
+  private final JTextArea myErrorLabel;
+
+  public IOExceptionDialog(String title, String errorText)  {
+    super((Project)null, true);
+    setTitle(title);
+    setOKButtonText(CommonBundle.message("dialog.ioexception.tryagain"));
+
+    myErrorLabel = new JTextArea();
+    myErrorLabel.setText(errorText);
+    myErrorLabel.setFont(UIManager.getFont("Label.font"));
+    myErrorLabel.setBackground(UIManager.getColor("Label.background"));
+    myErrorLabel.setForeground(UIManager.getColor("Label.foreground"));
+
+    init();
+  }
+
+  @Nullable
+  @Override
+  protected JComponent createCenterPanel() {
+    return myErrorLabel;
+  }
+
+  @Nonnull
+  @Override
+  protected Action[] createLeftSideActions() {
+    return new Action[] {
+      new AbstractAction(CommonBundle.message("dialog.ioexception.proxy")) {
+        @Override
+        public void actionPerformed(@Nonnull ActionEvent e) {
+          ShowSettingsUtil.getInstance().editConfigurable(ObjectUtil.tryCast(e.getSource(), JComponent.class), new HttpProxyConfigurable());
+        }
+      }
+    };
+  }
+
+  /**
+   * Show the dialog
+   * @return <code>true</code> if "Try Again" button pressed and <code>false</code> if "Cancel" button pressed
+   */
+  public static boolean showErrorDialog(final String title, final String text) {
+    final Ref<Boolean> ok = Ref.create(false);
+    try {
+      GuiUtils.runOrInvokeAndWait(new Runnable() {
+        @Override
+        public void run() {
+          IOExceptionDialog dialog = new IOExceptionDialog(title, text);
+          dialog.show();
+          ok.set(dialog.isOK());
+        }
+      });
+    }
+    catch (InterruptedException e) {
+      LOG.info(e);
+    }
+    catch (InvocationTargetException e) {
+      LOG.info(e);
+    }
+
+    return ok.get();
+  }
+}
