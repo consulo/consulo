08db1579dd23ea71972312ae57f7cb470781e17a
VISTALL <vistall.valeriy@gmail.com>
2020-10-30 23:43:36 +0300

    remove wolf service

--- modules/base/lang-impl/src/main/java/com/intellij/codeInsight/daemon/impl/WholeFileLocalInspectionsPassFactory.java
+++ modules/base/lang-impl/src/main/java/com/intellij/codeInsight/daemon/impl/WholeFileLocalInspectionsPassFactory.java
@@ -23,26 +23,22 @@ import com.intellij.codeInsight.daemon.DaemonBundle;
 import com.intellij.codeInspection.InspectionManager;
 import com.intellij.codeInspection.ex.InspectionProfileWrapper;
 import com.intellij.codeInspection.ex.LocalInspectionToolWrapper;
-import com.intellij.openapi.application.Application;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.project.ProjectManagerListener;
-import consulo.disposer.Disposer;
 import com.intellij.openapi.util.ProperTextRange;
 import com.intellij.profile.Profile;
 import com.intellij.profile.ProfileChangeAdapter;
-import com.intellij.profile.codeInspection.InspectionProjectProfileManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.util.containers.ContainerUtil;
+import consulo.disposer.Disposer;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import jakarta.inject.Inject;
-import jakarta.inject.Singleton;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -54,38 +50,25 @@ import java.util.stream.Collectors;
 public class WholeFileLocalInspectionsPassFactory implements TextEditorHighlightingPassFactory {
   private final Map<PsiFile, Boolean> myFileToolsCache = ContainerUtil.createConcurrentWeakMap();
   private final Project myProject;
-  private final InspectionProjectProfileManager myProfileManager;
 
   private volatile long myPsiModificationCount;
 
   @Inject
-  public WholeFileLocalInspectionsPassFactory(Application application, Project project, InspectionProjectProfileManager profileManager) {
+  public WholeFileLocalInspectionsPassFactory(Project project) {
     myProject = project;
-    myProfileManager = profileManager;
 
-    application.getMessageBus().connect(project).subscribe(ProjectManager.TOPIC, new ProjectManagerListener() {
+    myProject.getMessageBus().connect().subscribe(ProfileChangeAdapter.TOPIC, new ProfileChangeAdapter() {
       @Override
-      public void projectOpened(Project project) {
-        if(myProject == project) {
-          WholeFileLocalInspectionsPassFactory.this.projectOpened();
-        }
+      public void profileChanged(@Nullable Profile profile) {
+        myFileToolsCache.clear();
+      }
+
+      @Override
+      public void profileActivated(@Nullable Profile oldProfile, @Nullable Profile profile) {
+        myFileToolsCache.clear();
       }
     });
-  }
 
-  private void projectOpened() {
-    final ProfileChangeAdapter myProfilesListener = new ProfileChangeAdapter() {
-      @Override
-      public void profileChanged(Profile profile) {
-        myFileToolsCache.clear();
-      }
-
-      @Override
-      public void profileActivated(Profile oldProfile, @Nullable Profile profile) {
-        myFileToolsCache.clear();
-      }
-    };
-    myProfileManager.addProfileChangeListener(myProfilesListener, myProject);
     Disposer.register(myProject, myFileToolsCache::clear);
   }
 
