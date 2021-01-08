f3e040b533309108a52f3eeaa8ba9f13fc14896b
VISTALL <vistall.valeriy@gmail.com>
2021-01-08 20:45:32 +0300

    make logger get from log4j2 lazy

--- modules/base/logging/logging-log4j2-impl/src/main/java/consulo/logging/impl/log4j2/Log4J2Logger.java
+++ modules/base/logging/logging-log4j2-impl/src/main/java/consulo/logging/impl/log4j2/Log4J2Logger.java
@@ -42,20 +42,33 @@ import java.util.function.Supplier;
  * @since 2018-08-15
  */
 public class Log4J2Logger implements Logger {
-  private static Supplier<String> ourApplicationInfoProvider = getIdeaInfoProvider();
+  private static Supplier<String> ourApplicationInfoProvider = getInfoProvider();
 
   private final LoggerContext myLoggerContext;
-  private final org.apache.logging.log4j.Logger myLogger;
+  private final String myName;
 
-  Log4J2Logger(LoggerContext loggerContext, org.apache.logging.log4j.Logger logger) {
+  @Nullable
+  private org.apache.logging.log4j.Logger myLogger;
+
+  Log4J2Logger(LoggerContext loggerContext, String name) {
     myLoggerContext = loggerContext;
-    myLogger = logger;
+    myName = name;
+  }
+
+  @Nonnull
+  private org.apache.logging.log4j.Logger logger() {
+    if(myLogger == null) {
+      org.apache.logging.log4j.core.Logger logger = myLoggerContext.getLogger(myName);
+      myLogger = logger;
+      return logger;
+    }
+    return myLogger;
   }
 
   @Override
   public void error(Object message) {
     if (message instanceof IdeaLoggingEvent) {
-      myLogger.error(message);
+      logger().error(message);
     }
     else {
       error(String.valueOf(message));
@@ -64,86 +77,90 @@ public class Log4J2Logger implements Logger {
 
   @Override
   public boolean isDebugEnabled() {
-    return myLogger.isDebugEnabled();
+    return logger().isDebugEnabled();
   }
 
   @Override
   public void debug(String message) {
-    myLogger.debug(message);
+    logger().debug(message);
   }
 
   @Override
   public void debug(Throwable t) {
-    myLogger.debug("", t);
+    logger().debug("", t);
   }
 
   @Override
   public void debug(String message, Throwable t) {
-    myLogger.debug(message, t);
+    logger().debug(message, t);
   }
 
   @Override
   public void error(String message, @Nullable Throwable t, String... details) {
+    org.apache.logging.log4j.Logger logger = logger();
+    
     if (t instanceof ControlFlowException) {
-      myLogger.error(new Throwable("Do not log ProcessCanceledException").initCause(t));
+      logger.error(new Throwable("Do not log ProcessCanceledException").initCause(t));
       throw (RuntimeException)t;
     }
 
     if (t != null && t.getClass().getName().contains("ReparsedSuccessfullyException")) {
-      myLogger.error(new Throwable("Do not log ReparsedSuccessfullyException").initCause(t));
+      logger.error(new Throwable("Do not log ReparsedSuccessfullyException").initCause(t));
       throw (RuntimeException)t;
     }
 
     String detailString = StringUtil.join(details, "\n");
 
-    myLogger.error(message + (!detailString.isEmpty() ? "\nDetails: " + detailString : ""), t);
+    logger.error(message + (!detailString.isEmpty() ? "\nDetails: " + detailString : ""), t);
     logErrorHeader();
     if (t != null && t.getCause() != null) {
-      myLogger.error("Original exception: ", t.getCause());
+      logger.error("Original exception: ", t.getCause());
     }
   }
 
   private void logErrorHeader() {
     final String info = ourApplicationInfoProvider.get();
 
+    org.apache.logging.log4j.Logger logger = logger();
+    
     if (info != null) {
-      myLogger.error(info);
+      logger.error(info);
     }
 
-    myLogger.error("JDK: " + System.getProperties().getProperty("java.version", "unknown"));
-    myLogger.error("VM: " + System.getProperties().getProperty("java.vm.name", "unknown"));
-    myLogger.error("Vendor: " + System.getProperties().getProperty("java.vendor", "unknown"));
-    myLogger.error("OS: " + System.getProperties().getProperty("os.name", "unknown"));
+    logger.error("JDK: " + System.getProperties().getProperty("java.version", "unknown"));
+    logger.error("VM: " + System.getProperties().getProperty("java.vm.name", "unknown"));
+    logger.error("Vendor: " + System.getProperties().getProperty("java.vendor", "unknown"));
+    logger.error("OS: " + System.getProperties().getProperty("os.name", "unknown"));
 
     Application application = ApplicationManager.getApplication();
     // only if app not disposed or not started disposing
     if (application != null && application.getDisposeState().get() == ThreeState.NO) {
       final String lastPreformedActionId = LastActionTracker.ourLastActionId;
       if (lastPreformedActionId != null) {
-        myLogger.error("Last Action: " + lastPreformedActionId);
+        logger.error("Last Action: " + lastPreformedActionId);
       }
 
       CommandProcessor commandProcessor = CommandProcessor.getInstance();
       final String currentCommandName = commandProcessor.getCurrentCommandName();
       if (currentCommandName != null) {
-        myLogger.error("Current Command: " + currentCommandName);
+        logger.error("Current Command: " + currentCommandName);
       }
     }
   }
 
   @Override
   public void info(String message) {
-    myLogger.info(message);
+    logger().info(message);
   }
 
   @Override
   public void info(String message, @Nullable Throwable t) {
-    myLogger.info(message, t);
+    logger().info(message, t);
   }
 
   @Override
   public void warn(String message, @Nullable Throwable t) {
-    myLogger.warn(message, t);
+    logger().warn(message, t);
   }
 
   @Override
@@ -158,7 +175,7 @@ public class Log4J2Logger implements Logger {
       throw new IllegalAccessException("Plugin is not platform: " + plugin);
     }
 
-    org.apache.logging.log4j.core.Logger logger = myLoggerContext.getLogger(myLogger.getName());
+    org.apache.logging.log4j.core.Logger logger = myLoggerContext.getLogger(myName);
 
     Level newLevel;
     switch (level) {
@@ -185,7 +202,7 @@ public class Log4J2Logger implements Logger {
     logger.setLevel(newLevel);
   }
 
-  private static Supplier<String> getIdeaInfoProvider() {
+  private static Supplier<String> getInfoProvider() {
     return () -> {
       final ApplicationInfo info = ApplicationInfo.getInstance();
       return info.getFullApplicationName() + "  " + "Build #" + info.getBuild().asString();
