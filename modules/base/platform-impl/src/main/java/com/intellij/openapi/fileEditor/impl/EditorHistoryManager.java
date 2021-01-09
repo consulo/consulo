81d9aaaec99d9086426f63aeeea7bc0c7fcd506e
VISTALL <vistall.valeriy@gmail.com>
2021-01-09 15:47:00 +0300

    fix npe - store provider anyway

--- modules/base/platform-impl/src/main/java/com/intellij/openapi/fileEditor/impl/EditorHistoryManager.java
+++ modules/base/platform-impl/src/main/java/com/intellij/openapi/fileEditor/impl/EditorHistoryManager.java
@@ -144,12 +144,13 @@ public final class EditorHistoryManager implements PersistentStateComponentWithU
       final FileEditorState[] states = new FileEditorState[editors.length];
       final FileEditorProvider[] providers = new FileEditorProvider[editors.length];
       for (int i = states.length - 1; i >= 0; i--) {
-        final FileEditorProvider provider = oldProviders[i];
+        FileEditorProvider provider = oldProviders[i];
         LOG.assertTrue(provider != null);
-        FileEditor editor = editors[i];
-        if (!editor.isValid()) continue;
         providers[i] = provider;
-        states[i] = editor.getState(FileEditorStateLevel.FULL);
+        FileEditor editor = editors[i];
+        if (editor.isValid()) {
+          states[i] = editor.getState(FileEditorStateLevel.FULL);
+        }
       }
       addEntry(HistoryEntry.createHeavy(myProject, file, providers, states, providers[selectedProviderIndex]));
       trimToSize();
