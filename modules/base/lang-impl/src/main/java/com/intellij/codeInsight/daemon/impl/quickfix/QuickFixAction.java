1d72a585274d83351d432da6fe85cbffd9882000
VISTALL <vistall.valeriy@gmail.com>
2021-02-19 16:21:44 +0300

    missed method from idea

--- modules/base/lang-impl/src/main/java/com/intellij/codeInsight/daemon/impl/quickfix/QuickFixAction.java
+++ modules/base/lang-impl/src/main/java/com/intellij/codeInsight/daemon/impl/quickfix/QuickFixAction.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.codeInsight.daemon.impl.quickfix;
 
@@ -20,6 +6,9 @@ import com.intellij.codeInsight.daemon.HighlightDisplayKey;
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.openapi.util.TextRange;
+import org.jetbrains.annotations.Nls;
+
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import java.util.List;
@@ -28,7 +17,8 @@ import java.util.List;
  * @author Alexey Kudravtsev
  */
 public final class QuickFixAction {
-  private QuickFixAction() { }
+  private QuickFixAction() {
+  }
 
   public static void registerQuickFixAction(@Nullable HighlightInfo info, @Nullable IntentionAction action, @Nullable HighlightDisplayKey key) {
     registerQuickFixAction(info, null, action, key);
@@ -38,21 +28,16 @@ public final class QuickFixAction {
     registerQuickFixAction(info, null, action);
   }
 
-  /** This is used by TeamCity plugin */
+  /**
+   * @deprecated This is used by TeamCity plugin
+   */
   @Deprecated
-  public static void registerQuickFixAction(@Nullable HighlightInfo info,
-                                            @Nullable IntentionAction action,
-                                            @Nullable List<IntentionAction> options,
-                                            @Nullable String displayName) {
+  public static void registerQuickFixAction(@Nullable HighlightInfo info, @Nullable IntentionAction action, @Nullable List<IntentionAction> options, @Nullable @Nls String displayName) {
     if (info == null) return;
     info.registerFix(action, options, displayName, null, null);
   }
 
-
-  public static void registerQuickFixAction(@Nullable HighlightInfo info,
-                                            @Nullable TextRange fixRange,
-                                            @Nullable IntentionAction action,
-                                            @Nullable final HighlightDisplayKey key) {
+  public static void registerQuickFixAction(@Nullable HighlightInfo info, @Nullable TextRange fixRange, @Nullable IntentionAction action, @Nullable HighlightDisplayKey key) {
     if (info == null) return;
     info.registerFix(action, null, HighlightDisplayKey.getDisplayNameByKey(key), fixRange, key);
   }
@@ -60,5 +45,11 @@ public final class QuickFixAction {
   public static void registerQuickFixAction(@Nullable HighlightInfo info, @Nullable TextRange fixRange, @Nullable IntentionAction action) {
     if (info == null) return;
     info.registerFix(action, null, null, fixRange, null);
+  }
+
+  public static void registerQuickFixActions(@Nullable HighlightInfo info, @Nullable TextRange fixRange, @Nonnull Iterable<? extends IntentionAction> actions) {
+    for (IntentionAction action : actions) {
+      registerQuickFixAction(info, fixRange, action);
+    }
   }
 }
