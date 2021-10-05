26a88f32a93652300600d567b44658c2a9117463
VISTALL <vistall.valeriy@gmail.com>
2021-10-05 23:15:16 +0300

    merge from idea

--- modules/desktop-awt/desktop-ui-laf-impl/src/main/java/consulo/desktop/ui/laf/idea/darcula/DarculaCheckBoxUI.java
+++ modules/desktop-awt/desktop-ui-laf-impl/src/main/java/consulo/desktop/ui/laf/idea/darcula/DarculaCheckBoxUI.java
@@ -39,7 +39,7 @@ public class DarculaCheckBoxUI extends MetalCheckBoxUI {
   @Override
   public void installUI(JComponent c) {
     super.installUI(c);
-    if (ComponentUtil.getParentOfType((Class<? extends CellRendererPane>)CellRendererPane.class, (Component)c) != null) {
+    if (ComponentUtil.getParentOfType(CellRendererPane.class, c) != null) {
       c.setBorder(null);
     }
   }
