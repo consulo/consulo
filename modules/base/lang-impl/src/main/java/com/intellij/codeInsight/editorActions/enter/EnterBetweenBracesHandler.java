843633f822a00d5c1d95588caa754a96d8a5107a
VISTALL <vistall.valeriy@gmail.com>
2021-11-06 16:34:04 +0300

    add api update from idea

--- modules/base/lang-impl/src/main/java/com/intellij/codeInsight/editorActions/enter/EnterBetweenBracesHandler.java
+++ modules/base/lang-impl/src/main/java/com/intellij/codeInsight/editorActions/enter/EnterBetweenBracesHandler.java
@@ -1,102 +1,28 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.codeInsight.editorActions.enter;
 
-import com.intellij.codeInsight.CodeInsightSettings;
-import com.intellij.codeInsight.editorActions.CodeDocumentationUtil;
-import com.intellij.openapi.actionSystem.DataContext;
-import consulo.logging.Logger;
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Ref;
-import com.intellij.psi.PsiComment;
-import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.codeStyle.CodeStyleManager;
-import com.intellij.psi.codeStyle.CodeStyleSettingsManager;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.text.CharArrayUtil;
 import javax.annotation.Nonnull;
 
-public class EnterBetweenBracesHandler extends EnterHandlerDelegateAdapter {
-  private static final Logger LOG = Logger.getInstance(EnterBetweenBracesHandler.class);
-
+/**
+ * @deprecated Please, use the {@code EnterBetweenBracesDelegate} language-specific implementation instead.
+ */
+@Deprecated
+public class EnterBetweenBracesHandler extends EnterBetweenBracesFinalHandler {
   @Override
-  public Result preprocessEnter(@Nonnull final PsiFile file, @Nonnull final Editor editor, @Nonnull final Ref<Integer> caretOffsetRef, @Nonnull final Ref<Integer> caretAdvance,
-                                @Nonnull final DataContext dataContext, final EditorActionHandler originalHandler) {
-    Document document = editor.getDocument();
-    CharSequence text = document.getCharsSequence();
-    int caretOffset = caretOffsetRef.get().intValue();
-    if (!CodeInsightSettings.getInstance().SMART_INDENT_ON_ENTER) {
-      return Result.Continue;
-    }
-
-    int prevCharOffset = CharArrayUtil.shiftBackward(text, caretOffset - 1, " \t");
-    int nextCharOffset = CharArrayUtil.shiftForward(text, caretOffset, " \t");
-
-    if (!isValidOffset(prevCharOffset, text) || !isValidOffset(nextCharOffset, text) ||
-        !isBracePair(text.charAt(prevCharOffset), text.charAt(nextCharOffset))) {
-      return Result.Continue;
-    }
-
-    PsiDocumentManager.getInstance(file.getProject()).commitDocument(editor.getDocument());
-    if (file.findElementAt(prevCharOffset) == file.findElementAt(nextCharOffset)) {
-      return Result.Continue;
-    }
-
-    final int line = document.getLineNumber(caretOffset);
-    final int start = document.getLineStartOffset(line);
-    final CodeDocumentationUtil.CommentContext commentContext =
-            CodeDocumentationUtil.tryParseCommentContext(file, text, caretOffset, start);
-
-    // special case: enter inside "()" or "{}"
-    String indentInsideJavadoc = isInComment(caretOffset, file) && commentContext.docAsterisk
-                                 ? CodeDocumentationUtil.getIndentInsideJavadoc(document, caretOffset)
-                                 : null;
-
-    originalHandler.execute(editor, editor.getCaretModel().getCurrentCaret(), dataContext);
-
-    Project project = editor.getProject();
-    if (indentInsideJavadoc != null && project != null && CodeStyleSettingsManager.getSettings(project).JD_LEADING_ASTERISKS_ARE_ENABLED) {
-      document.insertString(editor.getCaretModel().getOffset(), "*" + indentInsideJavadoc);
-    }
-
-    PsiDocumentManager.getInstance(file.getProject()).commitDocument(document);
-    try {
-      CodeStyleManager.getInstance(file.getProject()).adjustLineIndent(file, editor.getCaretModel().getOffset());
-    }
-    catch (IncorrectOperationException e) {
-      LOG.error(e);
-    }
-    return indentInsideJavadoc == null ? Result.Continue : Result.DefaultForceIndent;
+  protected boolean isApplicable(@Nonnull PsiFile file, @Nonnull Editor editor, CharSequence documentText, int caretOffset, EnterBetweenBracesDelegate helper) {
+    int prevCharOffset = CharArrayUtil.shiftBackward(documentText, caretOffset - 1, " \t");
+    int nextCharOffset = CharArrayUtil.shiftForward(documentText, caretOffset, " \t");
+    return isValidOffset(prevCharOffset, documentText) &&
+           isValidOffset(nextCharOffset, documentText) &&
+           isBracePair(documentText.charAt(prevCharOffset), documentText.charAt(nextCharOffset)) &&
+           !ourDefaultBetweenDelegate.bracesAreInTheSameElement(file, editor, prevCharOffset, nextCharOffset);
   }
 
-  private static boolean isInComment(int offset, PsiFile file) {
-    return PsiTreeUtil.getParentOfType(file.findElementAt(offset), PsiComment.class)!=null;
-  }
-
-  private static boolean isValidOffset(int offset, CharSequence text) {
-    return offset >= 0 && offset < text.length();
-  }
-
-  protected boolean isBracePair(char c1, char c2) {
-    return (c1 == '(' && c2 == ')') || (c1 == '{' && c2 == '}');
+  protected boolean isBracePair(char lBrace, char rBrace) {
+    return ourDefaultBetweenDelegate.isBracePair(lBrace, rBrace);
   }
 }
