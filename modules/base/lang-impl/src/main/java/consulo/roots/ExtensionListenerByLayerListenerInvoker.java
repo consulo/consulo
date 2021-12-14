4f52f14ebb59ff1261d1c1c70ae32e99d5a569bc
VISTALL <vistall.valeriy@gmail.com>
2021-12-15 00:13:58 +0300

    reducing not lazy project components

--- modules/base/lang-impl/src/main/java/consulo/roots/ExtensionListenerByLayerListenerInvoker.java
+++ modules/base/lang-impl/src/main/java/consulo/roots/ExtensionListenerByLayerListenerInvoker.java
@@ -15,22 +15,17 @@
  */
 package consulo.roots;
 
-import com.intellij.ProjectTopics;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Couple;
 import consulo.module.extension.ModuleExtension;
 import consulo.module.extension.ModuleExtensionChangeListener;
 import consulo.module.extension.ModuleExtensionProviderEP;
 import consulo.module.extension.MutableModuleExtension;
 import consulo.module.extension.impl.ModuleExtensionProviders;
+import consulo.util.lang.Couple;
 
 import javax.annotation.Nonnull;
-import jakarta.inject.Inject;
-import jakarta.inject.Singleton;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,43 +33,34 @@ import java.util.List;
  * @author VISTALL
  * @since 31.07.14
  */
-@Singleton
-public class ExtensionListenerByLayerListenerInvoker {
-  @Inject
-  public ExtensionListenerByLayerListenerInvoker(@Nonnull Project project) {
-    project.getMessageBus().connect().subscribe(ProjectTopics.MODULE_LAYERS, new ModuleRootLayerListener.Adapter() {
-      @Override
-      public void currentLayerChanged(@Nonnull final Module module,
-                                      @Nonnull final String oldName,
-                                      @Nonnull final ModuleRootLayer oldLayer,
-                                      @Nonnull final String newName,
-                                      @Nonnull final ModuleRootLayer newLayer) {
+public class ExtensionListenerByLayerListenerInvoker extends ModuleRootLayerListener.Adapter {
+  @Override
+  public void currentLayerChanged(@Nonnull final Module module,
+                                  @Nonnull final String oldName,
+                                  @Nonnull final ModuleRootLayer oldLayer,
+                                  @Nonnull final String newName,
+                                  @Nonnull final ModuleRootLayer newLayer) {
 
-        final List<Couple<ModuleExtension>> list = new ArrayList<Couple<ModuleExtension>>();
-        for (ModuleExtensionProviderEP providerEP : ModuleExtensionProviders.getProviders()) {
-          MutableModuleExtension oldExtension = oldLayer.getExtensionWithoutCheck(providerEP.getKey());
-          MutableModuleExtension newExtension = newLayer.getExtensionWithoutCheck(providerEP.getKey());
+    final List<Couple<ModuleExtension>> list = new ArrayList<>();
+    for (ModuleExtensionProviderEP providerEP : ModuleExtensionProviders.getProviders()) {
+      MutableModuleExtension oldExtension = oldLayer.getExtensionWithoutCheck(providerEP.getKey());
+      MutableModuleExtension newExtension = newLayer.getExtensionWithoutCheck(providerEP.getKey());
 
-          if(oldExtension == null || newExtension == null) {
-            continue;
-          }
-
-          if(oldExtension.isModified(newExtension)) {
-            list.add(Couple.<ModuleExtension>of(oldExtension, newExtension));
-          }
-        }
-
-        ApplicationManager.getApplication().invokeLater(new Runnable() {
-          @Override
-          public void run() {
-            ModuleExtensionChangeListener moduleExtensionChangeListener = project.getMessageBus().syncPublisher(ModuleExtension.CHANGE_TOPIC);
-
-            for (Couple<ModuleExtension> couple : list) {
-              moduleExtensionChangeListener.beforeExtensionChanged(couple.getFirst(), couple.getSecond());
-            }
-          }
-        }, ModalityState.NON_MODAL);
+      if (oldExtension == null || newExtension == null) {
+        continue;
       }
-    });
+
+      if (oldExtension.isModified(newExtension)) {
+        list.add(Couple.<ModuleExtension>of(oldExtension, newExtension));
+      }
+    }
+
+    ApplicationManager.getApplication().invokeLater(() -> {
+      ModuleExtensionChangeListener moduleExtensionChangeListener = module.getProject().getMessageBus().syncPublisher(ModuleExtension.CHANGE_TOPIC);
+
+      for (Couple<ModuleExtension> couple : list) {
+        moduleExtensionChangeListener.beforeExtensionChanged(couple.getFirst(), couple.getSecond());
+      }
+    }, ModalityState.NON_MODAL);
   }
 }
