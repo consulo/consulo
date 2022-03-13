38ace7250ffd43c2097a084b023e036f61253aa3
VISTALL <vistall.valeriy@gmail.com>
2022-03-13 16:39:18 +0300

    /MT fix for C++ binaries

--- modules/base/ui-ex-awt-api/src/main/java/consulo/ui/ex/awt/AbstractDelegatingToRootTraversalPolicy.java
+++ modules/base/ui-ex-awt-api/src/main/java/consulo/ui/ex/awt/AbstractDelegatingToRootTraversalPolicy.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intellij.openapi.wm.ex;
+package consulo.ui.ex.awt;
 
 import java.awt.*;
 
