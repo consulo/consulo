f3d996ee37f3a0726fcd5951caae34fb8f29a58a
VISTALL <vistall.valeriy@gmail.com>
2022-04-03 14:55:33 +0300

    move debug/coverage modules under execution package

---
+++ modules/base/execution-coverage-api/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/**
+ * @author VISTALL
+ * @since 02-Apr-22
+ */
+module consulo.coverage.api {
+  // TODO remove in future
+  requires java.desktop;
+
+  requires transitive consulo.project.api;
+  requires transitive consulo.project.ui.view.api;
+  requires transitive consulo.ui.ex.awt.api;
+  requires transitive consulo.execution.api;
+  requires transitive consulo.xcoverage.rt;
+
+  exports consulo.execution.coverage;
+  exports consulo.execution.coverage.view;
+}
