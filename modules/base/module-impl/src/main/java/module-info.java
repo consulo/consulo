aa884ce41aa733a12823e54f3f9df0943e69eab1
VISTALL <vistall.valeriy@gmail.com>
2022-06-13 14:33:51 +0300

    exports to xml.serializer for fix runtime errors

--- modules/base/module-impl/src/main/java/module-info.java
+++ modules/base/module-impl/src/main/java/module-info.java
@@ -11,7 +11,7 @@ module consulo.module.impl {
 
   exports consulo.module.impl.internal to consulo.ide.impl;
   exports consulo.module.impl.internal.extension to consulo.ide.impl;
-  exports consulo.module.impl.internal.layer to consulo.ide.impl;
+  exports consulo.module.impl.internal.layer to consulo.ide.impl, consulo.util.xml.serializer;
   exports consulo.module.impl.internal.layer.library to consulo.ide.impl;
   exports consulo.module.impl.internal.layer.orderEntry to consulo.ide.impl;
 
