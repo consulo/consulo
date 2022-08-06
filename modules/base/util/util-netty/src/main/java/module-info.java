478a104e0a9c3f353056b9e7e60801639ec050e5
VISTALL <vistall.valeriy@gmail.com>
2022-08-06 22:13:31 +0300

    if we require netty module export all netty modules

--- modules/base/util/util-netty/src/main/java/module-info.java
+++ modules/base/util/util-netty/src/main/java/module-info.java
@@ -7,13 +7,13 @@ module consulo.util.netty {
   requires transitive consulo.util.lang;
   requires transitive consulo.util.concurrent;
 
-  requires io.netty.buffer;
-  requires io.netty.codec;
-  requires io.netty.codec.http;
-  requires io.netty.common;
-  requires io.netty.handler;
-  requires io.netty.resolver;
-  requires io.netty.transport;
+  requires transitive io.netty.buffer;
+  requires transitive io.netty.codec;
+  requires transitive io.netty.codec.http;
+  requires transitive io.netty.common;
+  requires transitive io.netty.handler;
+  requires transitive io.netty.resolver;
+  requires transitive io.netty.transport;
 
   exports consulo.util.netty;
 }
