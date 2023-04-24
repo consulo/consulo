3f5d1e38ac9eeb540f0457216abbca5216d09151
Valery Semenchuk <vistall.valeriy@gmail.com>
2023-04-24 10:43:50 +0400

    drop package which not exists

--- modules/base/ui-api/src/main/java/module-info.java
+++ modules/base/ui-api/src/main/java/module-info.java
@@ -23,7 +23,6 @@ module consulo.ui.api {
   exports consulo.ui.image;
   exports consulo.ui.image.canvas;
   exports consulo.ui.layout;
-  exports consulo.ui.layout.event;
   exports consulo.ui.model;
   exports consulo.ui.style;
   exports consulo.ui.util;
