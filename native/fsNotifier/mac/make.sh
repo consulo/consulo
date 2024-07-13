2c51111879636ee034eb39593217679f5d1d5a08
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-07-13 09:52:07 -0700

    add CMakeLists for native projects, also add arm64 binaries

--- native/fsNotifier/mac/make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Clang can be downloaded from http://llvm.org/releases/download.html or found in XCode 4+
-clang -arch i386 -arch x86_64 -mmacosx-version-min=10.5 -framework CoreServices -o fsnotifier fsnotifier.c
