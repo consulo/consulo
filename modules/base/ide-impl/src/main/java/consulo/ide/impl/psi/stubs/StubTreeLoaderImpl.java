6bef2f72994329c5b7155c52d7576b4d32602e36
UNV <nikolay@yurchenko.su>
2024-07-28 21:53:35 +0300

    IntelliJ IDEA → Consulo.

--- modules/base/ide-impl/src/main/java/consulo/ide/impl/psi/stubs/StubTreeLoaderImpl.java
+++ modules/base/ide-impl/src/main/java/consulo/ide/impl/psi/stubs/StubTreeLoaderImpl.java
@@ -308,7 +308,7 @@ public class StubTreeLoaderImpl extends StubTreeLoader {
 
     boolean canBePrebuilt = isPrebuilt(psiFile.getVirtualFile());
 
-    String msg = "PSI and index do not match.\nPlease report the problem to JetBrains with the files attached\n";
+    String msg = "PSI and index do not match.\nPlease report the problem with the files attached\n";
 
     if (canBePrebuilt) {
       msg += "This stub can have pre-built origin\n";
