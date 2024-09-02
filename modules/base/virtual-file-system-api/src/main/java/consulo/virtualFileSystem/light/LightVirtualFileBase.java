1497e5a4b715c1bd2c4e56628b04373ccc8268de
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-09-02 20:02:33 +0400

    if file type not set return unknown file type

--- modules/base/virtual-file-system-api/src/main/java/consulo/virtualFileSystem/light/LightVirtualFileBase.java
+++ modules/base/virtual-file-system-api/src/main/java/consulo/virtualFileSystem/light/LightVirtualFileBase.java
@@ -21,6 +21,7 @@ import consulo.virtualFileSystem.NonPhysicalFileSystem;
 import consulo.virtualFileSystem.VirtualFile;
 import consulo.virtualFileSystem.VirtualFileSystem;
 import consulo.virtualFileSystem.fileType.FileType;
+import consulo.virtualFileSystem.fileType.UnknownFileType;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 
@@ -30,194 +31,197 @@ import java.io.IOException;
  * In-memory implementation of {@link VirtualFile}.
  */
 public abstract class LightVirtualFileBase extends VirtualFile {
-  private FileType myFileType;
-  private String myName = "";
-  private long myModStamp = LocalTimeCounter.currentTime();
-  private boolean myIsWritable = true;
-  private boolean myValid = true;
-  private VirtualFile myOriginalFile;
+    private FileType myFileType;
+    private String myName = "";
+    private long myModStamp = LocalTimeCounter.currentTime();
+    private boolean myIsWritable = true;
+    private boolean myValid = true;
+    private VirtualFile myOriginalFile;
 
-  public LightVirtualFileBase(final String name, final FileType fileType, final long modificationStamp) {
-    myName = name;
-    myFileType = fileType;
-    myModStamp = modificationStamp;
-  }
-
-  public void setFileType(final FileType fileType) {
-    myFileType = fileType;
-  }
-
-  @Nonnull
-  @Override
-  public FileType getFileType() {
-    return myFileType;
-  }
-
-  public VirtualFile getOriginalFile() {
-    return myOriginalFile;
-  }
-
-  public void setOriginalFile(VirtualFile originalFile) {
-    myOriginalFile = originalFile;
-  }
-
-  private static class MyVirtualFileSystem extends BaseVirtualFileSystem implements NonPhysicalFileSystem {
-    private static final String PROTOCOL = "mock";
-
-    private MyVirtualFileSystem() {
-      startEventPropagation();
+    public LightVirtualFileBase(final String name, final FileType fileType, final long modificationStamp) {
+        myName = name;
+        myFileType = fileType;
+        myModStamp = modificationStamp;
     }
 
-    @Override
-    public void deleteFile(Object requestor, @Nonnull VirtualFile vFile) throws IOException {
-
-    }
-
-    @Override
-    public void moveFile(Object requestor, @Nonnull VirtualFile vFile, @Nonnull VirtualFile newParent) throws IOException {
-
-    }
-
-    @Override
-    public void renameFile(Object requestor, @Nonnull VirtualFile vFile, @Nonnull String newName) throws IOException {
-
-    }
-
-    @Override
-    public VirtualFile createChildFile(Object requestor, @Nonnull VirtualFile vDir, @Nonnull String fileName) throws IOException {
-      return null;
+    public void setFileType(final FileType fileType) {
+        myFileType = fileType;
     }
 
     @Nonnull
     @Override
-    public VirtualFile createChildDirectory(Object requestor, @Nonnull VirtualFile vDir, @Nonnull String dirName) throws IOException {
-      return null;
+    public FileType getFileType() {
+        if (myFileType == null) {
+            return UnknownFileType.INSTANCE;
+        }
+        return myFileType;
     }
 
+    public VirtualFile getOriginalFile() {
+        return myOriginalFile;
+    }
+
+    public void setOriginalFile(VirtualFile originalFile) {
+        myOriginalFile = originalFile;
+    }
+
+    private static class MyVirtualFileSystem extends BaseVirtualFileSystem implements NonPhysicalFileSystem {
+        private static final String PROTOCOL = "mock";
+
+        private MyVirtualFileSystem() {
+            startEventPropagation();
+        }
+
+        @Override
+        public void deleteFile(Object requestor, @Nonnull VirtualFile vFile) throws IOException {
+
+        }
+
+        @Override
+        public void moveFile(Object requestor, @Nonnull VirtualFile vFile, @Nonnull VirtualFile newParent) throws IOException {
+
+        }
+
+        @Override
+        public void renameFile(Object requestor, @Nonnull VirtualFile vFile, @Nonnull String newName) throws IOException {
+
+        }
+
+        @Override
+        public VirtualFile createChildFile(Object requestor, @Nonnull VirtualFile vDir, @Nonnull String fileName) throws IOException {
+            return null;
+        }
+
+        @Nonnull
+        @Override
+        public VirtualFile createChildDirectory(Object requestor, @Nonnull VirtualFile vDir, @Nonnull String dirName) throws IOException {
+            return null;
+        }
+
+        @Override
+        public VirtualFile copyFile(Object requestor,
+                                    @Nonnull VirtualFile virtualFile,
+                                    @Nonnull VirtualFile newParent,
+                                    @Nonnull String copyName)
+            throws IOException {
+            return null;
+        }
+
+        @Override
+        @Nonnull
+        public String getProtocol() {
+            return PROTOCOL;
+        }
+
+        @Override
+        @Nullable
+        public VirtualFile findFileByPath(@Nonnull String path) {
+            return null;
+        }
+
+        @Override
+        public void refresh(boolean asynchronous) {
+        }
+
+        @Override
+        @Nullable
+        public VirtualFile refreshAndFindFileByPath(@Nonnull String path) {
+            return null;
+        }
+    }
+
+    private static final MyVirtualFileSystem ourFileSystem = new MyVirtualFileSystem();
+
     @Override
-    public VirtualFile copyFile(Object requestor,
-                                @Nonnull VirtualFile virtualFile,
-                                @Nonnull VirtualFile newParent,
-                                @Nonnull String copyName)
-      throws IOException {
-      return null;
+    @Nonnull
+    public VirtualFileSystem getFileSystem() {
+        return ourFileSystem;
+    }
+
+    @Nullable
+    public FileType getAssignedFileType() {
+        return myFileType;
+    }
+
+    @Nonnull
+    @Override
+    public String getPath() {
+        return "/" + getName();
     }
 
     @Override
     @Nonnull
-    public String getProtocol() {
-      return PROTOCOL;
+    public String getName() {
+        return myName;
     }
 
     @Override
-    @Nullable
-    public VirtualFile findFileByPath(@Nonnull String path) {
-      return null;
+    public boolean isWritable() {
+        return myIsWritable;
     }
 
     @Override
-    public void refresh(boolean asynchronous) {
+    public boolean isDirectory() {
+        return false;
     }
 
     @Override
-    @Nullable
-    public VirtualFile refreshAndFindFileByPath(@Nonnull String path) {
-      return null;
+    public boolean isValid() {
+        return myValid;
     }
-  }
 
-  private static final MyVirtualFileSystem ourFileSystem = new MyVirtualFileSystem();
-
-  @Override
-  @Nonnull
-  public VirtualFileSystem getFileSystem() {
-    return ourFileSystem;
-  }
-
-  @Nullable
-  public FileType getAssignedFileType() {
-    return myFileType;
-  }
-
-  @Nonnull
-  @Override
-  public String getPath() {
-    return "/" + getName();
-  }
-
-  @Override
-  @Nonnull
-  public String getName() {
-    return myName;
-  }
-
-  @Override
-  public boolean isWritable() {
-    return myIsWritable;
-  }
-
-  @Override
-  public boolean isDirectory() {
-    return false;
-  }
-
-  @Override
-  public boolean isValid() {
-    return myValid;
-  }
-
-  public void setValid(boolean valid) {
-    myValid = valid;
-  }
-
-  @Override
-  public VirtualFile getParent() {
-    return null;
-  }
-
-  @Override
-  public VirtualFile[] getChildren() {
-    return EMPTY_ARRAY;
-  }
-
-  @Override
-  public long getModificationStamp() {
-    return myModStamp;
-  }
-
-  protected void setModificationStamp(long stamp) {
-    myModStamp = stamp;
-  }
-
-  @Override
-  public long getTimeStamp() {
-    return 0; // todo[max] : Add UnsupportedOperationException at better times.
-  }
-
-  @Override
-  public long getLength() {
-    try {
-      return contentsToByteArray().length;
+    public void setValid(boolean valid) {
+        myValid = valid;
     }
-    catch (IOException e) {
-      //noinspection CallToPrintStackTrace
-      e.printStackTrace();
-      assert false;
-      return 0;
+
+    @Override
+    public VirtualFile getParent() {
+        return null;
     }
-  }
 
-  @Override
-  public void refresh(boolean asynchronous, boolean recursive, Runnable postRunnable) {
-  }
+    @Override
+    public VirtualFile[] getChildren() {
+        return EMPTY_ARRAY;
+    }
 
-  @Override
-  public void setWritable(boolean b) {
-    myIsWritable = b;
-  }
+    @Override
+    public long getModificationStamp() {
+        return myModStamp;
+    }
 
-  @Override
-  public void rename(Object requestor, @Nonnull String newName) throws IOException {
-    myName = newName;
-  }
+    protected void setModificationStamp(long stamp) {
+        myModStamp = stamp;
+    }
+
+    @Override
+    public long getTimeStamp() {
+        return 0; // todo[max] : Add UnsupportedOperationException at better times.
+    }
+
+    @Override
+    public long getLength() {
+        try {
+            return contentsToByteArray().length;
+        }
+        catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+            assert false;
+            return 0;
+        }
+    }
+
+    @Override
+    public void refresh(boolean asynchronous, boolean recursive, Runnable postRunnable) {
+    }
+
+    @Override
+    public void setWritable(boolean b) {
+        myIsWritable = b;
+    }
+
+    @Override
+    public void rename(Object requestor, @Nonnull String newName) throws IOException {
+        myName = newName;
+    }
 }
