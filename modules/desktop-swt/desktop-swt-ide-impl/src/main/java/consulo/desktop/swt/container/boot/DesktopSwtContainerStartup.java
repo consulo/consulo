1d2d5f52b40d3bd259b26fab79a20b66244117db
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-09-05 22:14:03 +0400

    do not use app executor

--- modules/desktop-swt/desktop-swt-ide-impl/src/main/java/consulo/desktop/swt/container/boot/DesktopSwtContainerStartup.java
+++ modules/desktop-swt/desktop-swt-ide-impl/src/main/java/consulo/desktop/swt/container/boot/DesktopSwtContainerStartup.java
@@ -18,7 +18,6 @@ package consulo.desktop.swt.container.boot;
 import consulo.application.impl.internal.start.ApplicationStarter;
 import consulo.application.impl.internal.start.StartupAbortedException;
 import consulo.application.impl.internal.start.StartupUtil;
-import consulo.application.util.concurrent.AppExecutorUtil;
 import consulo.bootstrap.concurrent.IdeaForkJoinWorkerThreadFactory;
 import consulo.container.boot.ContainerPathManager;
 import consulo.container.boot.ContainerStartup;
@@ -39,58 +38,58 @@ import java.util.Map;
  * @since 27/04/2021
  */
 public class DesktopSwtContainerStartup implements ContainerStartup {
-  @Nonnull
-  @Override
-  public ContainerPathManager createPathManager(@Nonnull Map<String, Object> args) {
-    return new DesktopContainerPathManager();
-  }
-
-  @Override
-  public void run(@Nonnull Map<String, Object> map) {
-    StatCollector stat = (StatCollector)map.get(ContainerStartup.STAT_COLLECTOR);
-    String[] args = (String[])map.get(ContainerStartup.ARGS);
-
-    IdeaForkJoinWorkerThreadFactory.setupForkJoinCommonPool();
-
-    StartupUtil.initializeLogger();
-
-    Runnable appInitializeMark = stat.mark(StatCollector.APP_INITIALIZE);
-
-    Runnable runnable = () -> {
-      try {
-        start(stat, appInitializeMark, args);
-      }
-      catch (Throwable t) {
-        t.printStackTrace();
-        throw new StartupAbortedException(t);
-      }
-    };
-
-    new Thread(runnable, "Consulo Main Thread").start();
-  }
-
-  @Override
-  public void destroy() {
-
-  }
-
-  private static void start(StatCollector stat, Runnable appInitalizeMark, String[] args) {
-    ApplicationStarter.installExceptionHandler(() -> Logger.getInstance(DesktopSwtContainerStartup.class));
-
-    try {
-      StartupActionScriptManager.executeActionScript();
-    }
-    catch (IOException e) {
-      Logger.getInstance(DesktopSwtContainerStartup.class).error(e);
-
-      ShowErrorCaller.showErrorDialog("Plugin Installation Error", e.getMessage(), e);
-      return;
+    @Nonnull
+    @Override
+    public ContainerPathManager createPathManager(@Nonnull Map<String, Object> args) {
+        return new DesktopContainerPathManager();
     }
 
-    StartupUtil.prepareAndStart(args, stat, DesktopImportantFolderLocker::new, (newConfigFolder, commandLineArgs) -> {
-      ApplicationStarter app = new DesktopSwtApplicationStarter(commandLineArgs, stat);
+    @Override
+    public void run(@Nonnull Map<String, Object> map) {
+        StatCollector stat = (StatCollector) map.get(ContainerStartup.STAT_COLLECTOR);
+        String[] args = (String[]) map.get(ContainerStartup.ARGS);
 
-      AppExecutorUtil.getAppExecutorService().execute(() -> app.run(stat, appInitalizeMark, newConfigFolder));
-    });
-  }
+        IdeaForkJoinWorkerThreadFactory.setupForkJoinCommonPool();
+
+        StartupUtil.initializeLogger();
+
+        Runnable appInitializeMark = stat.mark(StatCollector.APP_INITIALIZE);
+
+        Runnable runnable = () -> {
+            try {
+                start(stat, appInitializeMark, args);
+            }
+            catch (Throwable t) {
+                t.printStackTrace();
+                throw new StartupAbortedException(t);
+            }
+        };
+
+        new Thread(runnable, "Consulo Main Thread").start();
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    private static void start(StatCollector stat, Runnable appInitalizeMark, String[] args) {
+        ApplicationStarter.installExceptionHandler(() -> Logger.getInstance(DesktopSwtContainerStartup.class));
+
+        try {
+            StartupActionScriptManager.executeActionScript();
+        }
+        catch (IOException e) {
+            Logger.getInstance(DesktopSwtContainerStartup.class).error(e);
+
+            ShowErrorCaller.showErrorDialog("Plugin Installation Error", e.getMessage(), e);
+            return;
+        }
+
+        StartupUtil.prepareAndStart(args, stat, DesktopImportantFolderLocker::new, (newConfigFolder, commandLineArgs) -> {
+            ApplicationStarter app = new DesktopSwtApplicationStarter(commandLineArgs, stat);
+
+            app.run(stat, appInitalizeMark, newConfigFolder);
+        });
+    }
 }
