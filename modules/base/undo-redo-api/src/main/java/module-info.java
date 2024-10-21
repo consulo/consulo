44df7bf25c3a43e1e9a692c5d1bba591965a8425
UNV <nikolay@yurchenko.su>
2024-10-22 01:08:41 +0300

    Reformatting module-info.java.

--- modules/base/undo-redo-api/src/main/java/module-info.java
+++ modules/base/undo-redo-api/src/main/java/module-info.java
@@ -3,12 +3,12 @@
  * @since 16-Feb-22
  */
 module consulo.undo.redo.api {
-  requires transitive consulo.project.api;
-  requires transitive consulo.document.api;
-  requires transitive consulo.util.lang;
+    requires transitive consulo.project.api;
+    requires transitive consulo.document.api;
+    requires transitive consulo.util.lang;
 
-  exports consulo.undoRedo;
-  exports consulo.undoRedo.event;
-  exports consulo.undoRedo.util;
-  exports consulo.undoRedo.internal to consulo.ide.impl, consulo.language.editor.refactoring.api;
+    exports consulo.undoRedo;
+    exports consulo.undoRedo.event;
+    exports consulo.undoRedo.util;
+    exports consulo.undoRedo.internal to consulo.ide.impl, consulo.language.editor.refactoring.api;
 }
