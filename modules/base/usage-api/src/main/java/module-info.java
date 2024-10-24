e8c0547a537de924abcff696705185197fb5f9f5
UNV <nikolay@yurchenko.su>
2024-10-24 19:00:51 +0300

    Reformatting pom.xml and module-info.java in usage-api.

--- modules/base/usage-api/src/main/java/module-info.java
+++ modules/base/usage-api/src/main/java/module-info.java
@@ -3,17 +3,19 @@
  * @since 27-Mar-22
  */
 module consulo.usage.api {
-  // TODO remove this dependency in future
-  requires java.desktop;
+    // TODO remove this dependency in future
+    requires java.desktop;
 
-  requires transitive consulo.language.api;
-  requires transitive consulo.language.editor.api;
-  requires transitive consulo.file.editor.api;
-  requires transitive consulo.virtual.file.status.api;
-  requires transitive consulo.project.ui.api;
+    requires transitive consulo.language.api;
+    requires transitive consulo.language.editor.api;
+    requires transitive consulo.file.editor.api;
+    requires transitive consulo.virtual.file.status.api;
+    requires transitive consulo.project.ui.api;
 
-  exports consulo.usage;
-  exports consulo.usage.rule;
-  exports consulo.usage.util;
-  exports consulo.usage.internal to consulo.ide.impl, consulo.language.editor.refactoring.api;
+    exports consulo.usage;
+    exports consulo.usage.rule;
+    exports consulo.usage.util;
+    exports consulo.usage.internal to
+        consulo.ide.impl,
+        consulo.language.editor.refactoring.api;
 }
