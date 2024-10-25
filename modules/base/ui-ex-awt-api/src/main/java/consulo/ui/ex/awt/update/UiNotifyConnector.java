e0dcab39b49a5da9bdab8f2dee114cea97b5aec8
UNV <nikolay@yurchenko.su>
2024-10-25 22:36:54 +0300

    Reformatting UiNotifyConnector.

--- modules/base/ui-ex-awt-api/src/main/java/consulo/ui/ex/awt/update/UiNotifyConnector.java
+++ modules/base/ui-ex-awt-api/src/main/java/consulo/ui/ex/awt/update/UiNotifyConnector.java
@@ -22,6 +22,7 @@ import consulo.disposer.Disposer;
 import consulo.ui.ex.update.Activatable;
 
 import jakarta.annotation.Nonnull;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.HierarchyEvent;
@@ -29,137 +30,145 @@ import java.awt.event.HierarchyListener;
 import java.lang.ref.WeakReference;
 
 public class UiNotifyConnector implements Disposable, HierarchyListener {
+    @Nonnull
+    private final WeakReference<Component> myComponent;
+    private Activatable myTarget;
 
-  @Nonnull
-  private final WeakReference<Component> myComponent;
-  private Activatable myTarget;
-
-  public UiNotifyConnector(@Nonnull final Component component, @Nonnull final Activatable target) {
-    myComponent = new WeakReference<>(component);
-    myTarget = target;
-    if (component.isShowing()) {
-      showNotify();
-    }
-    else {
-      hideNotify();
-    }
-    if (isDisposed()) return;
-    component.addHierarchyListener(this);
-  }
-
-  @Override
-  public void hierarchyChanged(@Nonnull HierarchyEvent e) {
-    if (isDisposed()) return;
-
-    if ((e.getChangeFlags() & HierarchyEvent.SHOWING_CHANGED) > 0) {
-      final Runnable runnable = () -> {
-        final Component c = myComponent.get();
-        if (isDisposed() || c == null) return;
-
-        if (c.isShowing()) {
-          showNotify();
+    public UiNotifyConnector(@Nonnull final Component component, @Nonnull final Activatable target) {
+        myComponent = new WeakReference<>(component);
+        myTarget = target;
+        if (component.isShowing()) {
+            showNotify();
         }
         else {
-          hideNotify();
+            hideNotify();
         }
-      };
-      final Application app = ApplicationManager.getApplication();
-      if (app != null && app.isDispatchThread()) {
-        app.invokeLater(runnable, app.getCurrentModalityState());
-      }
-      else {
-        //noinspection SSBasedInspection
-        SwingUtilities.invokeLater(runnable);
-      }
-    }
-  }
 
-  protected void hideNotify() {
-    myTarget.hideNotify();
-  }
-
-  protected void showNotify() {
-    myTarget.showNotify();
-  }
-
-  protected void hideOnDispose() {
-    myTarget.hideNotify();
-  }
-
-  @Override
-  public void dispose() {
-    if (isDisposed()) return;
-
-    hideOnDispose();
-    final Component c = myComponent.get();
-    if (c != null) {
-      c.removeHierarchyListener(this);
-    }
-
-    myTarget = null;
-    myComponent.clear();
-  }
-
-  private boolean isDisposed() {
-    return myTarget == null;
-  }
-
-  public static class Once extends UiNotifyConnector {
-
-    private boolean myShown;
-    private boolean myHidden;
-
-    public Once(final Component component, final Activatable target) {
-      super(component, target);
+        if (isDisposed()) {
+            return;
+        }
+        component.addHierarchyListener(this);
     }
 
     @Override
-    protected final void hideNotify() {
-      super.hideNotify();
-      myHidden = true;
-      disposeIfNeeded();
+    public void hierarchyChanged(@Nonnull HierarchyEvent e) {
+        if (isDisposed()) {
+            return;
+        }
+
+        if ((e.getChangeFlags() & HierarchyEvent.SHOWING_CHANGED) > 0) {
+            final Runnable runnable = () -> {
+                final Component c = myComponent.get();
+                if (isDisposed() || c == null) {
+                    return;
+                }
+
+                if (c.isShowing()) {
+                    showNotify();
+                }
+                else {
+                    hideNotify();
+                }
+            };
+            final Application app = ApplicationManager.getApplication();
+            if (app != null && app.isDispatchThread()) {
+                app.invokeLater(runnable, app.getCurrentModalityState());
+            }
+            else {
+                //noinspection SSBasedInspection
+                SwingUtilities.invokeLater(runnable);
+            }
+        }
     }
 
-    @Override
-    protected final void showNotify() {
-      super.showNotify();
-      myShown = true;
-      disposeIfNeeded();
+    protected void hideNotify() {
+        myTarget.hideNotify();
     }
 
-    @Override
+    protected void showNotify() {
+        myTarget.showNotify();
+    }
+
     protected void hideOnDispose() {
+        myTarget.hideNotify();
     }
 
-    private void disposeIfNeeded() {
-      if (myShown && myHidden) {
-        Disposer.dispose(this);
-      }
+    @Override
+    public void dispose() {
+        if (isDisposed()) {
+            return;
+        }
+
+        hideOnDispose();
+        final Component c = myComponent.get();
+        if (c != null) {
+            c.removeHierarchyListener(this);
+        }
+
+        myTarget = null;
+        myComponent.clear();
     }
-  }
 
-  public static void doWhenFirstShown(@Nonnull JComponent c, @Nonnull final Runnable runnable) {
-    doWhenFirstShown((Component)c, runnable);
-  }
+    private boolean isDisposed() {
+        return myTarget == null;
+    }
 
-  public static void doWhenFirstShown(@Nonnull Component c, @Nonnull final Runnable runnable) {
-    Activatable activatable = new Activatable() {
-      @Override
-      public void showNotify() {
-        runnable.run();
-      }
+    public static class Once extends UiNotifyConnector {
 
-      @Override
-      public void hideNotify() {
-      }
-    };
+        private boolean myShown;
+        private boolean myHidden;
 
-    new UiNotifyConnector(c, activatable) {
-      @Override
-      protected void showNotify() {
-        super.showNotify();
-        Disposer.dispose(this);
-      }
-    };
-  }
+        public Once(final Component component, final Activatable target) {
+            super(component, target);
+        }
+
+        @Override
+        protected final void hideNotify() {
+            super.hideNotify();
+            myHidden = true;
+            disposeIfNeeded();
+        }
+
+        @Override
+        protected final void showNotify() {
+            super.showNotify();
+            myShown = true;
+            disposeIfNeeded();
+        }
+
+        @Override
+        protected void hideOnDispose() {
+        }
+
+        private void disposeIfNeeded() {
+            if (myShown && myHidden) {
+                Disposer.dispose(this);
+            }
+        }
+    }
+
+    public static void doWhenFirstShown(@Nonnull JComponent c, @Nonnull Runnable runnable) {
+        doWhenFirstShown((Component)c, runnable);
+    }
+
+    public static void doWhenFirstShown(@Nonnull Component c, @Nonnull final Runnable runnable) {
+        Activatable activatable = new Activatable() {
+            @Override
+            public void showNotify() {
+                runnable.run();
+            }
+
+            @Override
+            public void hideNotify() {
+            }
+        };
+
+        new UiNotifyConnector(c, activatable) {
+            @Override
+            protected void showNotify() {
+                super.showNotify();
+                Disposer.dispose(this);
+            }
+        };
+    }
 }
