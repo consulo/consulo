89a428654df990ee7013c3606971e4fb54817043
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-11-14 13:00:55 +0400

    handle windows server 2025 as windows 11

--- modules/base/platform-impl/src/main/java/consulo/platform/impl/WindowsOperatingSystemImpl.java
+++ modules/base/platform-impl/src/main/java/consulo/platform/impl/WindowsOperatingSystemImpl.java
@@ -18,8 +18,8 @@ package consulo.platform.impl;
 import com.sun.jna.platform.win32.Kernel32;
 import com.sun.jna.platform.win32.WinNT;
 import consulo.platform.os.WindowsOperatingSystem;
-
 import jakarta.annotation.Nonnull;
+
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.function.Function;
@@ -30,75 +30,88 @@ import java.util.function.Supplier;
  * @since 25/04/2023
  */
 public class WindowsOperatingSystemImpl extends PlatformOperatingSystemImpl implements WindowsOperatingSystem {
-  private final boolean isWin7OrNewer;
-  private final boolean isWin8OrNewer;
-  private final boolean isWin10OrNewer;
+    private final boolean isWin7OrNewer;
+    private final boolean isWin8OrNewer;
+    private final boolean isWin10OrNewer;
 
-  private Boolean myWindows11OrLater;
+    private Boolean myWindows11OrLater;
 
-  public WindowsOperatingSystemImpl(Map<String, String> jvmProperties,
-                                    Function<String, String> getEnvFunc,
-                                    Supplier<Map<String, String>> getEnvsSup) {
-    super(jvmProperties, getEnvFunc, getEnvsSup);
-    isWin7OrNewer = isOsVersionAtLeast("6.1");
-    isWin8OrNewer = isOsVersionAtLeast("6.2");
-    isWin10OrNewer = isOsVersionAtLeast("10.0");
-  }
-
-  @Override
-  public boolean isWindows7OrNewer() {
-    return isWin7OrNewer;
-  }
-
-  @Override
-  public boolean isWindows8OrNewer() {
-    return isWin8OrNewer;
-  }
-
-  @Override
-  public boolean isWindows10OrNewer() {
-    return isWin10OrNewer;
-  }
-
-  @Override
-  public boolean isWindows11OrNewer() {
-    if (myWindows11OrLater == null) {
-      myWindows11OrLater = isWindows11OrNewerImpl();
+    public WindowsOperatingSystemImpl(Map<String, String> jvmProperties,
+                                      Function<String, String> getEnvFunc,
+                                      Supplier<Map<String, String>> getEnvsSup) {
+        super(jvmProperties, getEnvFunc, getEnvsSup);
+        isWin7OrNewer = isOsVersionAtLeast("6.1");
+        isWin8OrNewer = isOsVersionAtLeast("6.2");
+        isWin10OrNewer = isOsVersionAtLeast("10.0");
     }
-    return myWindows11OrLater;
-  }
 
-  @Nonnull
-  @Override
-  public String getWindowsFileVersion(@Nonnull Path path, int parts) {
-    if (!isWindows()) {
-      throw new IllegalArgumentException("Windows OS required");
+    @Override
+    public boolean isWindows7OrNewer() {
+        return isWin7OrNewer;
     }
-    return WindowsVersionHelper.getVersion(path.toString(), parts);
-  }
 
-  private boolean isWindows11OrNewerImpl() {
-    // at jdk 17 windows 11 will return in os name, but at old versions of jdk that will be Windows 10
-    try {
-      boolean windows11OrLater = OS_NAME.contains("Windows 11");
-      if (windows11OrLater) {
-          return true;
-      }
+    @Override
+    public boolean isWindows8OrNewer() {
+        return isWin8OrNewer;
+    }
 
-      if (isWindows10OrNewer()) {
-        WinNT.OSVERSIONINFO osversioninfo = new WinNT.OSVERSIONINFO();
-        if (Kernel32.INSTANCE.GetVersionEx(osversioninfo)) {
-          int dwBuildNumber = osversioninfo.dwBuildNumber.intValue();
+    @Override
+    public boolean isWindows10OrNewer() {
+        return isWin10OrNewer;
+    }
 
-          if (dwBuildNumber >= 22_000) {
-            return true;
-          }
+    @Override
+    public boolean isWindows11OrNewer() {
+        if (myWindows11OrLater == null) {
+            myWindows11OrLater = isWindows11OrNewerImpl();
         }
-      }
-    }
-    catch (Throwable ignored) {
+        return myWindows11OrLater;
     }
 
-    return false;
-  }
+    @Nonnull
+    @Override
+    public String getWindowsFileVersion(@Nonnull Path path, int parts) {
+        if (!isWindows()) {
+            throw new IllegalArgumentException("Windows OS required");
+        }
+        return WindowsVersionHelper.getVersion(path.toString(), parts);
+    }
+
+    private boolean isWindows11OrNewerImpl() {
+        // at jdk 17 windows 11 will return in os name, but at old versions of jdk that will be Windows 10
+        try {
+            // windows 2025 has same core & ui as Windows 11
+            boolean windows11OrLater = OS_NAME.contains("Windows 11") || OS_NAME.contains("Windows Server 2025");
+            if (windows11OrLater) {
+                return true;
+            }
+
+            // windows server check
+            if (OS_NAME.contains("Windows Server 2022")) {
+                WinNT.OSVERSIONINFO osversioninfo = new WinNT.OSVERSIONINFO();
+                if (Kernel32.INSTANCE.GetVersionEx(osversioninfo)) {
+                    int dwBuildNumber = osversioninfo.dwBuildNumber.intValue();
+
+                    if (dwBuildNumber >= 26_039) {
+                        return true;
+                    }
+                }
+            }
+            // windows 10 check
+            else if (isWindows10OrNewer()) {
+                WinNT.OSVERSIONINFO osversioninfo = new WinNT.OSVERSIONINFO();
+                if (Kernel32.INSTANCE.GetVersionEx(osversioninfo)) {
+                    int dwBuildNumber = osversioninfo.dwBuildNumber.intValue();
+
+                    if (dwBuildNumber >= 22_000) {
+                        return true;
+                    }
+                }
+            }
+        }
+        catch (Throwable ignored) {
+        }
+
+        return false;
+    }
 }
