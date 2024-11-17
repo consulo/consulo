38340ad158ba4e54515d5915fe472d971534ae73
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-11-17 14:08:53 +0400

    allow set lower value

--- modules/base/builtin-web-server-impl/src/main/java/consulo/builtinWebServer/impl/BuiltInServerConfigurable.java
+++ modules/base/builtin-web-server-impl/src/main/java/consulo/builtinWebServer/impl/BuiltInServerConfigurable.java
@@ -45,7 +45,7 @@ public class BuiltInServerConfigurable extends SimpleConfigurableByProperties im
     BuiltInServerOptions options = BuiltInServerOptions.getInstance();
 
     VerticalLayout root = VerticalLayout.create();
-    IntBox portBox = IntBox.create().withRange(1024, Short.MAX_VALUE & 0xFFFF);
+    IntBox portBox = IntBox.create().withRange(0, Short.MAX_VALUE & 0xFFFF);
     propertyBuilder.add(portBox, () -> options.builtInServerPort, it -> options.builtInServerPort = it);
     root.add(LabeledBuilder.simple(LocalizeValue.localizeTODO("Port"), portBox));
 
