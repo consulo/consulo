9cffe04842844f77607dc121fd83678cb818e096
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-12-03 21:06:41 +0400

    reformat

--- modules/base/ui-ex-awt-api/src/main/java/consulo/ui/ex/awt/speedSearch/TreeSpeedSearch.java
+++ modules/base/ui-ex-awt-api/src/main/java/consulo/ui/ex/awt/speedSearch/TreeSpeedSearch.java
@@ -26,140 +26,150 @@ import java.util.function.Function;
 import static javax.swing.tree.TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION;
 
 public class TreeSpeedSearch extends SpeedSearchBase<JTree> {
-  protected boolean myCanExpand;
+    protected boolean myCanExpand;
 
-  private static final Function<TreePath, String> TO_STRING = path -> path.getLastPathComponent().toString();
-  private final Function<? super TreePath, String> myToStringConvertor;
-  public static final Function<TreePath, String> NODE_DESCRIPTOR_TOSTRING = path -> {
-    NodeDescriptor descriptor = TreeUtil.getLastUserObject(NodeDescriptor.class, path);
-    if (descriptor != null) return descriptor.toString();
-    return TO_STRING.apply(path);
-  };
+    private static final Function<TreePath, String> TO_STRING = path -> path.getLastPathComponent().toString();
+    private final Function<? super TreePath, String> myToStringConvertor;
+    public static final Function<TreePath, String> NODE_DESCRIPTOR_TOSTRING = path -> {
+        NodeDescriptor descriptor = TreeUtil.getLastUserObject(NodeDescriptor.class, path);
+        if (descriptor != null) {
+            return descriptor.toString();
+        }
+        return TO_STRING.apply(path);
+    };
 
-  public TreeSpeedSearch(JTree tree, Function<? super TreePath, String> toStringConvertor) {
-    this(tree, toStringConvertor, false);
-  }
-
-  public TreeSpeedSearch(JTree tree) {
-    this(tree, TO_STRING);
-  }
-
-  public TreeSpeedSearch(Tree tree, Function<? super TreePath, String> toString) {
-    this(tree, toString, false);
-  }
-
-  public TreeSpeedSearch(Tree tree, Function<? super TreePath, String> toString, boolean canExpand) {
-    this((JTree)tree, toString, canExpand);
-  }
-
-  public TreeSpeedSearch(JTree tree, Function<? super TreePath, String> toString, boolean canExpand) {
-    super(tree);
-    setComparator(new SpeedSearchComparator(false, true));
-    myToStringConvertor = toString;
-    myCanExpand = canExpand;
-
-    new MySelectAllAction(tree, this).registerCustomShortcutSet(tree, null);
-  }
-
-  @Override
-  protected void selectElement(Object element, String selectedText) {
-    TreeUtil.selectPath(myComponent, (TreePath)element);
-  }
-
-  @Override
-  protected int getSelectedIndex() {
-    if (myCanExpand) {
-      return ArrayUtil.find(getAllElements(), myComponent.getSelectionPath());
+    public TreeSpeedSearch(JTree tree, Function<? super TreePath, String> toStringConvertor) {
+        this(tree, toStringConvertor, false);
     }
-    int[] selectionRows = myComponent.getSelectionRows();
-    return selectionRows == null || selectionRows.length == 0 ? -1 : selectionRows[0];
-  }
 
-  @Nonnull
-  @Override
-  protected Object[] getAllElements() {
-    JBIterable<TreePath> paths;
-    if (myCanExpand) {
-      paths = TreeUtil.treePathTraverser(myComponent).traverse();
+    public TreeSpeedSearch(JTree tree) {
+        this(tree, TO_STRING);
     }
-    else {
-      TreePath[] arr = new TreePath[myComponent.getRowCount()];
-      for (int i = 0; i < arr.length; i++) {
-        arr[i] = myComponent.getPathForRow(i);
-      }
-      paths = JBIterable.of(arr);
+
+    public TreeSpeedSearch(Tree tree, Function<? super TreePath, String> toString) {
+        this(tree, toString, false);
     }
-    List<TreePath> result = paths.filter(o -> !(o.getLastPathComponent() instanceof LoadingNode)).toList();
-    return result.toArray(new TreePath[0]);
-  }
 
-  @Override
-  protected String getElementText(Object element) {
-    TreePath path = (TreePath)element;
-    String string = myToStringConvertor.apply(path);
-    if (string == null) return TO_STRING.apply(path);
-    return string;
-  }
-
-  @Nonnull
-  private List<TreePath> findAllFilteredElements(String s) {
-    List<TreePath> paths = new ArrayList<>();
-    String _s = s.trim();
-
-    ListIterator<Object> it = getElementIterator(0);
-    while (it.hasNext()) {
-      Object element = it.next();
-      if (isMatchingElement(element, _s)) paths.add((TreePath)element);
+    public TreeSpeedSearch(Tree tree, Function<? super TreePath, String> toString, boolean canExpand) {
+        this((JTree) tree, toString, canExpand);
     }
-    return paths;
-  }
 
-  private static class MySelectAllAction extends DumbAwareAction {
-    @Nonnull
-    private final JTree myTree;
-    @Nonnull
-    private final TreeSpeedSearch mySearch;
+    public TreeSpeedSearch(JTree tree, Function<? super TreePath, String> toString, boolean canExpand) {
+        super(tree);
+        setComparator(new SpeedSearchComparator(false, true));
+        myToStringConvertor = toString;
+        myCanExpand = canExpand;
 
-    MySelectAllAction(@Nonnull JTree tree, @Nonnull TreeSpeedSearch search) {
-      myTree = tree;
-      mySearch = search;
-      copyShortcutFrom(ActionManager.getInstance().getAction(IdeActions.ACTION_SELECT_ALL));
-      setEnabledInModalContext(true);
+        new MySelectAllAction(tree, this).registerCustomShortcutSet(tree, null);
     }
 
     @Override
-    public void update(@Nonnull AnActionEvent e) {
-      e.getPresentation()
-       .setEnabled(mySearch.isPopupActive() && myTree.getSelectionModel().getSelectionMode() == DISCONTIGUOUS_TREE_SELECTION);
+    protected void selectElement(Object element, String selectedText) {
+        TreeUtil.selectPath(myComponent, (TreePath) element);
     }
 
     @Override
-    public void actionPerformed(@Nonnull AnActionEvent e) {
-      TreeSelectionModel sm = myTree.getSelectionModel();
-
-      String query = mySearch.getEnteredPrefix();
-      if (query == null) return;
-
-      List<TreePath> filtered = mySearch.findAllFilteredElements(query);
-      if (filtered.isEmpty()) return;
-
-      boolean alreadySelected = sm.getSelectionCount() == filtered.size() && ContainerUtil.and(filtered, (path) -> sm.isPathSelected(path));
-
-      if (alreadySelected) {
-        TreePath anchor = myTree.getAnchorSelectionPath();
-
-        sm.setSelectionPath(anchor);
-        myTree.setAnchorSelectionPath(anchor);
-
-        mySearch.findAndSelectElement(query);
-      }
-      else {
-        TreePath currentElement = (TreePath)mySearch.findElement(query);
-        TreePath anchor = ObjectUtil.chooseNotNull(currentElement, filtered.get(0));
-
-        sm.setSelectionPaths(filtered.toArray(new TreePath[0]));
-        myTree.setAnchorSelectionPath(anchor);
-      }
+    protected int getSelectedIndex() {
+        if (myCanExpand) {
+            return ArrayUtil.find(getAllElements(), myComponent.getSelectionPath());
+        }
+        int[] selectionRows = myComponent.getSelectionRows();
+        return selectionRows == null || selectionRows.length == 0 ? -1 : selectionRows[0];
     }
-  }
+
+    @Nonnull
+    @Override
+    protected Object[] getAllElements() {
+        JBIterable<TreePath> paths;
+        if (myCanExpand) {
+            paths = TreeUtil.treePathTraverser(myComponent).traverse();
+        }
+        else {
+            TreePath[] arr = new TreePath[myComponent.getRowCount()];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = myComponent.getPathForRow(i);
+            }
+            paths = JBIterable.of(arr);
+        }
+        List<TreePath> result = paths.filter(o -> !(o.getLastPathComponent() instanceof LoadingNode)).toList();
+        return result.toArray(new TreePath[0]);
+    }
+
+    @Override
+    protected String getElementText(Object element) {
+        TreePath path = (TreePath) element;
+        String string = myToStringConvertor.apply(path);
+        if (string == null) {
+            return TO_STRING.apply(path);
+        }
+        return string;
+    }
+
+    @Nonnull
+    private List<TreePath> findAllFilteredElements(String s) {
+        List<TreePath> paths = new ArrayList<>();
+        String _s = s.trim();
+
+        ListIterator<Object> it = getElementIterator(0);
+        while (it.hasNext()) {
+            Object element = it.next();
+            if (isMatchingElement(element, _s)) {
+                paths.add((TreePath) element);
+            }
+        }
+        return paths;
+    }
+
+    private static class MySelectAllAction extends DumbAwareAction {
+        @Nonnull
+        private final JTree myTree;
+        @Nonnull
+        private final TreeSpeedSearch mySearch;
+
+        MySelectAllAction(@Nonnull JTree tree, @Nonnull TreeSpeedSearch search) {
+            myTree = tree;
+            mySearch = search;
+            copyShortcutFrom(ActionManager.getInstance().getAction(IdeActions.ACTION_SELECT_ALL));
+            setEnabledInModalContext(true);
+        }
+
+        @Override
+        public void update(@Nonnull AnActionEvent e) {
+            e.getPresentation()
+                .setEnabled(mySearch.isPopupActive() && myTree.getSelectionModel().getSelectionMode() == DISCONTIGUOUS_TREE_SELECTION);
+        }
+
+        @Override
+        public void actionPerformed(@Nonnull AnActionEvent e) {
+            TreeSelectionModel sm = myTree.getSelectionModel();
+
+            String query = mySearch.getEnteredPrefix();
+            if (query == null) {
+                return;
+            }
+
+            List<TreePath> filtered = mySearch.findAllFilteredElements(query);
+            if (filtered.isEmpty()) {
+                return;
+            }
+
+            boolean alreadySelected = sm.getSelectionCount() == filtered.size() && ContainerUtil.and(filtered, (path) -> sm.isPathSelected(path));
+
+            if (alreadySelected) {
+                TreePath anchor = myTree.getAnchorSelectionPath();
+
+                sm.setSelectionPath(anchor);
+                myTree.setAnchorSelectionPath(anchor);
+
+                mySearch.findAndSelectElement(query);
+            }
+            else {
+                TreePath currentElement = (TreePath) mySearch.findElement(query);
+                TreePath anchor = ObjectUtil.chooseNotNull(currentElement, filtered.get(0));
+
+                sm.setSelectionPaths(filtered.toArray(new TreePath[0]));
+                myTree.setAnchorSelectionPath(anchor);
+            }
+        }
+    }
 }
