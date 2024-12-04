cf2fb625176f15f0ff5b28817ed1111d6a1326bb
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-12-04 13:07:32 +0400

    add caching

--- modules/base/ide-impl/src/main/java/consulo/ide/impl/idea/xdebugger/impl/frame/XDebuggerFramesList.java
+++ modules/base/ide-impl/src/main/java/consulo/ide/impl/idea/xdebugger/impl/frame/XDebuggerFramesList.java
@@ -37,7 +37,6 @@ import consulo.ui.ex.ColoredStringBuilder;
 import consulo.ui.ex.ColoredTextContainer;
 import consulo.ui.ex.SimpleTextAttributes;
 import consulo.ui.ex.awt.ColoredListCellRenderer;
-import consulo.ui.ex.awt.JBCurrentTheme;
 import consulo.ui.ex.awt.popup.GroupedItemsListRenderer;
 import consulo.ui.ex.awt.popup.ListItemDescriptorAdapter;
 import consulo.ui.image.Image;
@@ -59,235 +58,240 @@ import java.util.function.Consumer;
  * @author nik
  */
 public class XDebuggerFramesList extends DebuggerFramesList {
-  private final Project myProject;
-  private final Map<VirtualFile, Color> myFileColors = new HashMap<>();
+    private final Project myProject;
+    private final Map<VirtualFile, Color> myFileColors = new HashMap<>();
+    private final Map<VirtualFile, Image> myFileIcons = new HashMap<>();
 
-  private static final TransferHandler DEFAULT_TRANSFER_HANDLER = new TransferHandler() {
-    @Override
-    protected Transferable createTransferable(JComponent c) {
-      if (!(c instanceof XDebuggerFramesList)) {
-        return null;
-      }
-      XDebuggerFramesList list = (XDebuggerFramesList)c;
-      //noinspection deprecation
-      Object[] values = list.getSelectedValues();
-      if (values == null || values.length == 0) {
-        return null;
-      }
-
-      StringBuilder plainBuf = new StringBuilder();
-      StringBuilder htmlBuf = new StringBuilder();
-      ColoredStringBuilder coloredTextContainer = new ColoredStringBuilder();
-      htmlBuf.append("<html>\n<body>\n<ul>\n");
-      for (Object value : values) {
-        htmlBuf.append("  <li>");
-        if (value != null) {
-          if (value instanceof XStackFrame) {
-            ((XStackFrame)value).customizePresentation(coloredTextContainer);
-            coloredTextContainer.appendTo(plainBuf, htmlBuf);
-          }
-          else {
-            String text = value.toString();
-            plainBuf.append(text);
-            htmlBuf.append(text);
-          }
-        }
-        plainBuf.append('\n');
-        htmlBuf.append("</li>\n");
-      }
-
-      // remove the last newline
-      plainBuf.setLength(plainBuf.length() - 1);
-      htmlBuf.append("</ul>\n</body>\n</html>");
-      return new TextTransferable(htmlBuf.toString(), plainBuf.toString());
-    }
-
-    @Override
-    public int getSourceActions(@Nonnull JComponent c) {
-      return COPY;
-    }
-  };
-
-  private XStackFrame mySelectedFrame;
-
-  public XDebuggerFramesList(@Nonnull Project project) {
-    myProject = project;
-
-    doInit();
-    setTransferHandler(DEFAULT_TRANSFER_HANDLER);
-    setDataProvider(new DataProvider() {
-      @Nullable
-      @Override
-      @RequiredReadAction
-      public Object getData(@Nonnull @NonNls Key dataId) {
-        if (mySelectedFrame != null) {
-          if (VirtualFile.KEY == dataId) {
-            return getFile(mySelectedFrame);
-          }
-          else if (PsiFile.KEY == dataId) {
-            VirtualFile file = getFile(mySelectedFrame);
-            if (file != null && file.isValid()) {
-              return PsiManager.getInstance(myProject).findFile(file);
+    private static final TransferHandler DEFAULT_TRANSFER_HANDLER = new TransferHandler() {
+        @Override
+        protected Transferable createTransferable(JComponent c) {
+            if (!(c instanceof XDebuggerFramesList)) {
+                return null;
             }
-          }
-        }
-        return null;
-      }
-    });
-  }
+            XDebuggerFramesList list = (XDebuggerFramesList) c;
+            //noinspection deprecation
+            Object[] values = list.getSelectedValues();
+            if (values == null || values.length == 0) {
+                return null;
+            }
 
-  @Override
-  public void clear() {
-    super.clear();
-    myFileColors.clear();
-  }
+            StringBuilder plainBuf = new StringBuilder();
+            StringBuilder htmlBuf = new StringBuilder();
+            ColoredStringBuilder coloredTextContainer = new ColoredStringBuilder();
+            htmlBuf.append("<html>\n<body>\n<ul>\n");
+            for (Object value : values) {
+                htmlBuf.append("  <li>");
+                if (value != null) {
+                    if (value instanceof XStackFrame) {
+                        ((XStackFrame) value).customizePresentation(coloredTextContainer);
+                        coloredTextContainer.appendTo(plainBuf, htmlBuf);
+                    }
+                    else {
+                        String text = value.toString();
+                        plainBuf.append(text);
+                        htmlBuf.append(text);
+                    }
+                }
+                plainBuf.append('\n');
+                htmlBuf.append("</li>\n");
+            }
 
-  @Nullable
-  private static VirtualFile getFile(XStackFrame frame) {
-    XSourcePosition position = frame.getSourcePosition();
-    return position != null ? position.getFile() : null;
-  }
-
-  @Override
-  protected ListCellRenderer createListRenderer() {
-    return new XDebuggerGroupedFrameListRenderer();
-  }
-
-  @Override
-  protected void onFrameChanged(final Object selectedValue) {
-    if (mySelectedFrame != selectedValue) {
-      SwingUtilities.invokeLater(() -> repaint());
-      if (selectedValue instanceof XStackFrame) {
-        mySelectedFrame = (XStackFrame)selectedValue;
-      }
-      else {
-        mySelectedFrame = null;
-      }
-    }
-  }
-
-  private class XDebuggerGroupedFrameListRenderer extends GroupedItemsListRenderer<Object> {
-    private final XDebuggerFrameListRenderer myOriginalRenderer = new XDebuggerFrameListRenderer(myProject);
-
-    public XDebuggerGroupedFrameListRenderer() {
-      super(new ListItemDescriptorAdapter<>() {
-        @Nullable
-        @Override
-        public String getTextFor(Object value) {
-          return null;
+            // remove the last newline
+            plainBuf.setLength(plainBuf.length() - 1);
+            htmlBuf.append("</ul>\n</body>\n</html>");
+            return new TextTransferable(htmlBuf.toString(), plainBuf.toString());
         }
 
         @Override
-        public boolean hasSeparatorAboveOf(Object value) {
-          return value instanceof XStackFrameWithSeparatorAbove frameWithSeparatorAbove && frameWithSeparatorAbove.hasSeparatorAbove();
+        public int getSourceActions(@Nonnull JComponent c) {
+            return COPY;
         }
-      });
+    };
+
+    private XStackFrame mySelectedFrame;
+
+    public XDebuggerFramesList(@Nonnull Project project) {
+        myProject = project;
+
+        doInit();
+        setTransferHandler(DEFAULT_TRANSFER_HANDLER);
+        setDataProvider(new DataProvider() {
+            @Nullable
+            @Override
+            @RequiredReadAction
+            public Object getData(@Nonnull @NonNls Key dataId) {
+                if (mySelectedFrame != null) {
+                    if (VirtualFile.KEY == dataId) {
+                        return getFile(mySelectedFrame);
+                    }
+                    else if (PsiFile.KEY == dataId) {
+                        VirtualFile file = getFile(mySelectedFrame);
+                        if (file != null && file.isValid()) {
+                            return PsiManager.getInstance(myProject).findFile(file);
+                        }
+                    }
+                }
+                return null;
+            }
+        });
     }
 
     @Override
-    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-      if (myDescriptor.hasSeparatorAboveOf(value)) {
-        Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-        ((XDebuggerFrameListRenderer)myComponent).getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-        return component;
-      }
-      else {
-        return myOriginalRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-      }
+    public void clear() {
+        super.clear();
+        myFileColors.clear();
+    }
+
+    @Nullable
+    private static VirtualFile getFile(XStackFrame frame) {
+        XSourcePosition position = frame.getSourcePosition();
+        return position != null ? position.getFile() : null;
     }
 
     @Override
-    protected JComponent createItemComponent() {
-      createLabel();
-      return new XDebuggerFrameListRenderer(myProject);
-    }
-  }
-
-  private class XDebuggerFrameListRenderer extends ColoredListCellRenderer {
-    private final FileColorManager myColorsManager;
-
-    public XDebuggerFrameListRenderer(@Nonnull Project project) {
-      myColorsManager = FileColorManager.getInstance(project);
+    protected ListCellRenderer createListRenderer() {
+        return new XDebuggerGroupedFrameListRenderer();
     }
 
     @Override
-    protected void customizeCellRenderer(@Nonnull final JList list,
-                                         final Object value,
-                                         final int index,
-                                         final boolean selected,
-                                         final boolean hasFocus) {
-      if (value == null) {
-        append(XDebuggerBundle.message("stack.frame.loading.text"), SimpleTextAttributes.GRAY_ATTRIBUTES);
-        return;
-      }
-
-      if (value instanceof String) {
-        append((String)value, SimpleTextAttributes.ERROR_ATTRIBUTES);
-        return;
-      }
-
-      XStackFrame stackFrame = (XStackFrame)value;
-      if (!selected) {
-        Color c = getFrameBgColor(stackFrame);
-        if (c != null) {
-          setBackground(c);
+    protected void onFrameChanged(final Object selectedValue) {
+        if (mySelectedFrame != selectedValue) {
+            SwingUtilities.invokeLater(() -> repaint());
+            
+            if (selectedValue instanceof XStackFrame) {
+                mySelectedFrame = (XStackFrame) selectedValue;
+            }
+            else {
+                mySelectedFrame = null;
+            }
         }
-      }
-      stackFrame.customizePresentation(this);
-      
-      // override icon - fully ignored, from provider
-      setIcon(getSourceIcon(stackFrame));
     }
 
-    @SuppressWarnings("unchecked")
-    Image getSourceIcon(XStackFrame stackFrame) {
-      VirtualFile virtualFile = getFile(stackFrame);
-      if (virtualFile == null) {
-        return PlatformIconGroup.actionsHelp();
-      }
+    private class XDebuggerGroupedFrameListRenderer extends GroupedItemsListRenderer<Object> {
+        private final XDebuggerFrameListRenderer myOriginalRenderer = new XDebuggerFrameListRenderer(myProject);
 
-      ProjectFileIndex index = ProjectFileIndex.getInstance(myProject);
-      if (index.isInLibrary(virtualFile)) {
-        List<OrderEntry> entries = index.getOrderEntriesForFile(virtualFile);
-        for (OrderEntry entry : entries) {
-          OrderEntryTypeEditor<OrderEntry> editor = OrderEntryTypeEditor.getEditor(entry.getType().getId());
+        public XDebuggerGroupedFrameListRenderer() {
+            super(new ListItemDescriptorAdapter<>() {
+                @Nullable
+                @Override
+                public String getTextFor(Object value) {
+                    return null;
+                }
 
-          Consumer<ColoredTextContainer> render = editor.getRender(entry);
-          ColoredStringBuilder builder = new ColoredStringBuilder();
-          render.accept(builder);
-          Image icon = builder.getIcon();
-          if (icon != null) {
-            return icon;
-          }
+                @Override
+                public boolean hasSeparatorAboveOf(Object value) {
+                    return value instanceof XStackFrameWithSeparatorAbove frameWithSeparatorAbove && frameWithSeparatorAbove.hasSeparatorAbove();
+                }
+            });
         }
-      }
-      else {
-        ContentFolder contentFolder = index.getContentFolder(virtualFile);
-        if (contentFolder != null) {
-          return ContentFoldersSupportUtil.getContentFolderIcon(contentFolder.getType(), contentFolder.getProperties());
-        }
-      }
 
-      return PlatformIconGroup.nodesFolder();
+        @Override
+        @SuppressWarnings("unchecked")
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+            if (myDescriptor.hasSeparatorAboveOf(value)) {
+                Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                ((XDebuggerFrameListRenderer) myComponent).getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                return component;
+            }
+            else {
+                return myOriginalRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+            }
+        }
+
+        @Override
+        protected JComponent createItemComponent() {
+            createLabel();
+            return new XDebuggerFrameListRenderer(myProject);
+        }
     }
 
-    Color getFrameBgColor(XStackFrame stackFrame) {
-      VirtualFile virtualFile = getFile(stackFrame);
-      if (virtualFile != null) {
-        // handle null value
-        if (myFileColors.containsKey(virtualFile)) {
-          return myFileColors.get(virtualFile);
+    private class XDebuggerFrameListRenderer extends ColoredListCellRenderer {
+        private final FileColorManager myColorsManager;
+
+        public XDebuggerFrameListRenderer(@Nonnull Project project) {
+            myColorsManager = FileColorManager.getInstance(project);
         }
-        else if (virtualFile.isValid()) {
-          Color color = myColorsManager.getFileColor(virtualFile);
-          myFileColors.put(virtualFile, color);
-          return color;
+
+        @Override
+        protected void customizeCellRenderer(@Nonnull final JList list,
+                                             final Object value,
+                                             final int index,
+                                             final boolean selected,
+                                             final boolean hasFocus) {
+            if (value == null) {
+                append(XDebuggerBundle.message("stack.frame.loading.text"), SimpleTextAttributes.GRAY_ATTRIBUTES);
+                return;
+            }
+
+            if (value instanceof String) {
+                append((String) value, SimpleTextAttributes.ERROR_ATTRIBUTES);
+                return;
+            }
+
+            XStackFrame stackFrame = (XStackFrame) value;
+            if (!selected) {
+                Color c = getFrameBgColor(stackFrame);
+                if (c != null) {
+                    setBackground(c);
+                }
+            }
+            stackFrame.customizePresentation(this);
+
+            // override icon - fully ignored, from provider
+            setIcon(getSourceIcon(stackFrame));
         }
-      }
-      else {
-        return myColorsManager.getScopeColor(NonProjectFilesScope.NAME);
-      }
-      return null;
+
+        @SuppressWarnings("unchecked")
+        Image getSourceIcon(XStackFrame stackFrame) {
+            VirtualFile virtualFile = getFile(stackFrame);
+            if (virtualFile == null || !virtualFile.isValid()) {
+                return PlatformIconGroup.actionsHelp();
+            }
+
+            return myFileIcons.computeIfAbsent(virtualFile, vf -> {
+                ProjectFileIndex index = ProjectFileIndex.getInstance(myProject);
+                if (index.isInLibrary(vf)) {
+                    List<OrderEntry> entries = index.getOrderEntriesForFile(vf);
+                    for (OrderEntry entry : entries) {
+                        OrderEntryTypeEditor<OrderEntry> editor = OrderEntryTypeEditor.getEditor(entry.getType().getId());
+
+                        Consumer<ColoredTextContainer> render = editor.getRender(entry);
+                        ColoredStringBuilder builder = new ColoredStringBuilder();
+                        render.accept(builder);
+                        Image icon = builder.getIcon();
+                        if (icon != null) {
+                            return icon;
+                        }
+                    }
+                }
+                else {
+                    ContentFolder contentFolder = index.getContentFolder(vf);
+                    if (contentFolder != null) {
+                        return ContentFoldersSupportUtil.getContentFolderIcon(contentFolder.getType(), contentFolder.getProperties());
+                    }
+                }
+
+                return PlatformIconGroup.nodesFolder();
+            });
+        }
+
+        Color getFrameBgColor(XStackFrame stackFrame) {
+            VirtualFile virtualFile = getFile(stackFrame);
+            if (virtualFile != null) {
+                // handle null value
+                if (myFileColors.containsKey(virtualFile)) {
+                    return myFileColors.get(virtualFile);
+                }
+                else if (virtualFile.isValid()) {
+                    Color color = myColorsManager.getFileColor(virtualFile);
+                    myFileColors.put(virtualFile, color);
+                    return color;
+                }
+            }
+            else {
+                return myColorsManager.getScopeColor(NonProjectFilesScope.NAME);
+            }
+            return null;
+        }
     }
-  }
 }
