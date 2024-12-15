9ac05eae375d42f0f6e3a833675d537a9dc963e2
UNV <nikolay@yurchenko.su>
2024-12-15 16:37:37 +0300

    Adding CommonQuickFixLocalize.

--- modules/base/language-editor-api/src/main/java/consulo/language/editor/intention/CommonQuickFixBundle.java
+++ modules/base/language-editor-api/src/main/java/consulo/language/editor/intention/CommonQuickFixBundle.java
@@ -1,12 +1,18 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package consulo.language.editor.intention;
 
+import consulo.annotation.DeprecationInfo;
+import consulo.annotation.internal.MigratedExtensionsTo;
 import consulo.component.util.localize.AbstractBundle;
+import consulo.language.editor.localize.CommonQuickFixLocalize;
 import org.jetbrains.annotations.PropertyKey;
 
 import jakarta.annotation.Nonnull;
 import java.util.function.Supplier;
 
+@Deprecated
+@DeprecationInfo("Use CommonQuickFixLocalize")
+@MigratedExtensionsTo(CommonQuickFixLocalize.class)
 public final class CommonQuickFixBundle extends AbstractBundle {
   public static final String BUNDLE = "messages.CommonQuickFixBundle";
   private static final CommonQuickFixBundle INSTANCE = new CommonQuickFixBundle();
