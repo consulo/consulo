bc2e4aecaf6c3c4802d13971ddff870f4465f692
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-01-01 02:07:47 +0400

    drop header color paint

--- modules/desktop-awt/desktop-awt-ide-impl/src/main/java/consulo/desktop/awt/ui/plaf2/flat/FlatCaptionPanelUI.java
+++ modules/desktop-awt/desktop-awt-ide-impl/src/main/java/consulo/desktop/awt/ui/plaf2/flat/FlatCaptionPanelUI.java
@@ -16,13 +16,9 @@
 package consulo.desktop.awt.ui.plaf2.flat;
 
 import consulo.desktop.awt.ui.plaf.BasicCaptionPanelUI;
-import consulo.ide.impl.idea.ui.CaptionPanel;
-import consulo.ide.impl.ui.ToolwindowPaintUtil;
-import consulo.ui.ex.awt.UIUtil;
 
 import javax.swing.*;
 import javax.swing.plaf.ComponentUI;
-import java.awt.*;
 
 /**
  * @author VISTALL
@@ -31,16 +27,5 @@ import java.awt.*;
 public class FlatCaptionPanelUI extends BasicCaptionPanelUI {
     public static ComponentUI createUI(JComponent c) {
         return new FlatCaptionPanelUI();
-    }
-
-    @Override
-    public void paint(Graphics g, JComponent c) {
-        super.paint(g, c);
-
-        final Graphics2D g2d = (Graphics2D) g;
-        CaptionPanel panel = (CaptionPanel) c;
-
-        g2d.setPaint(panel.isActive() ? ToolwindowPaintUtil.getActiveToolWindowHeaderColor() : UIUtil.getPanelBackground());
-        g2d.fillRect(0, 0, c.getWidth(), c.getHeight());
     }
 }
