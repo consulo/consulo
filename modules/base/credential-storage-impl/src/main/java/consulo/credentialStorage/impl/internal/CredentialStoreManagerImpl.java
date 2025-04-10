e825556987d348ab716d82687e1a27db42c0c67a
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-10 11:57:03 +0400

    disable keepass for now

--- modules/base/credential-storage-impl/src/main/java/consulo/credentialStorage/impl/internal/CredentialStoreManagerImpl.java
+++ modules/base/credential-storage-impl/src/main/java/consulo/credentialStorage/impl/internal/CredentialStoreManagerImpl.java
@@ -5,25 +5,25 @@ import consulo.credentialStorage.internal.CredentialStoreManager;
 import consulo.credentialStorage.internal.ProviderType;
 import jakarta.inject.Singleton;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Singleton
 @ServiceImpl
 public class CredentialStoreManagerImpl implements CredentialStoreManager {
+    private List<ProviderType> mySupported = List.of(ProviderType.KEYCHAIN, ProviderType.MEMORY_ONLY);
 
     @Override
     public boolean isSupported(ProviderType provider) {
-        return availableProviders().contains(provider);
+        return mySupported.contains(provider);
     }
 
     @Override
     public List<ProviderType> availableProviders() {
-        return Arrays.asList(ProviderType.values());
+        return mySupported;
     }
 
     @Override
     public ProviderType defaultProvider() {
-        return ProviderType.KEYCHAIN;
+        return mySupported.get(0);
     }
 }
