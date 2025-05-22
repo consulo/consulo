848d5a52c0dfd92d171cddd625657ff8c090a0b8
UNV <nikolay@yurchenko.su>
2025-05-22 19:42:21 +0300

    Reformatting PsiElement and InlineHandler.

--- modules/base/language-editor-refactoring-api/src/main/java/consulo/language/editor/refactoring/inline/InlineHandler.java
+++ modules/base/language-editor-refactoring-api/src/main/java/consulo/language/editor/refactoring/inline/InlineHandler.java
@@ -32,79 +32,82 @@ import consulo.util.collection.MultiMap;
 
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import java.util.List;
 
 /**
  * Interface that should be implemented by the language in order to provide inline functionality and possibly
  * participate in inline of elements in other languages this language may reference.
+ *
  * @author ven
  * @see #forLanguage(Language)
  */
 @ExtensionAPI(ComponentScope.APPLICATION)
 public interface InlineHandler extends LanguageExtension {
-  ExtensionPointCacheKey<InlineHandler, ByLanguageValue<List<InlineHandler>>> KEY = ExtensionPointCacheKey.create("InlineHandler", LanguageOneToMany.build(false));
+    ExtensionPointCacheKey<InlineHandler, ByLanguageValue<List<InlineHandler>>> KEY =
+        ExtensionPointCacheKey.create("InlineHandler", LanguageOneToMany.build(false));
 
-  @Nonnull
-  static List<InlineHandler> forLanguage(@Nonnull Language language) {
-    return Application.get().getExtensionPoint(InlineHandler.class).getOrBuildCache(KEY).requiredGet(language);
-  }
+    @Nonnull
+    static List<InlineHandler> forLanguage(@Nonnull Language language) {
+        return Application.get().getExtensionPoint(InlineHandler.class).getOrBuildCache(KEY).requiredGet(language);
+    }
 
-  interface Settings {
+    interface Settings {
+        /**
+         * @return true if as a result of refactoring setup only the reference where refactoring
+         * was triggered should be inlined.
+         */
+        boolean isOnlyOneReferenceToInline();
+
+        /**
+         * Special settings for the case when inline cannot be performed due to already reported (by error hint) problem
+         */
+        Settings CANNOT_INLINE_SETTINGS = new Settings() {
+            @Override
+            public boolean isOnlyOneReferenceToInline() {
+                return false;
+            }
+        };
+    }
+
     /**
-     * @return true if as a result of refactoring setup only the reference where refactoring
-     * was triggered should be inlined.
-     */
-    boolean isOnlyOneReferenceToInline();
-
-    /**
-     * Special settings for the case when inline cannot be performed due to already reported (by error hint) problem
-     */
-    Settings CANNOT_INLINE_SETTINGS = new Settings() {
-      @Override
-      public boolean isOnlyOneReferenceToInline() {
-        return false;
-      }
-    };
-  }
-
-  /**
-   * @param element element to be inlined
-   * @param invokedOnReference true if the user invoked the refactoring on an element reference
-   * @param editor in case refactoring has been called in the editor
-   * @return <code>Settings</code> object in case refactoring should be performed or null otherwise
-
-   */
-  @Nullable
-  Settings prepareInlineElement(@Nonnull PsiElement element, @Nullable Editor editor, boolean invokedOnReference);
-
-  /**
-   * @param element inlined element
-   */
-  void removeDefinition(@Nonnull PsiElement element, @Nonnull Settings settings);
-
-  /**
-   * @param element inlined element
-   * @param settings
-   * @return Inliner instance to be used for inlining references in this language
-   */
-  @Nullable
-  Inliner createInliner(@Nonnull PsiElement element, @Nonnull Settings settings);
-
-  interface Inliner {
-    /**
-     * @param reference reference to inlined element
-     * @param referenced inlined element
-     * @return set of conflicts inline of this element to the place denoted by reference would incur
-     * or null if no conflicts detected.
+     * @param element            element to be inlined
+     * @param invokedOnReference true if the user invoked the refactoring on an element reference
+     * @param editor             in case refactoring has been called in the editor
+     * @return <code>Settings</code> object in case refactoring should be performed or null otherwise
      */
     @Nullable
-    MultiMap<PsiElement, String> getConflicts(@Nonnull PsiReference reference, @Nonnull PsiElement referenced);
+    Settings prepareInlineElement(@Nonnull PsiElement element, @Nullable Editor editor, boolean invokedOnReference);
 
     /**
-     * Perform actual inline of element to the point where it is referenced
-     * @param usage usage of inlined element
-     * @param referenced inlined element
+     * @param element inlined element
      */
-    void inlineUsage(@Nonnull UsageInfo usage, @Nonnull PsiElement referenced);
-  }
+    void removeDefinition(@Nonnull PsiElement element, @Nonnull Settings settings);
+
+    /**
+     * @param element  inlined element
+     * @param settings
+     * @return Inliner instance to be used for inlining references in this language
+     */
+    @Nullable
+    Inliner createInliner(@Nonnull PsiElement element, @Nonnull Settings settings);
+
+    interface Inliner {
+        /**
+         * @param reference  reference to inlined element
+         * @param referenced inlined element
+         * @return set of conflicts inline of this element to the place denoted by reference would incur
+         * or null if no conflicts detected.
+         */
+        @Nullable
+        MultiMap<PsiElement, String> getConflicts(@Nonnull PsiReference reference, @Nonnull PsiElement referenced);
+
+        /**
+         * Perform actual inline of element to the point where it is referenced
+         *
+         * @param usage      usage of inlined element
+         * @param referenced inlined element
+         */
+        void inlineUsage(@Nonnull UsageInfo usage, @Nonnull PsiElement referenced);
+    }
 }
