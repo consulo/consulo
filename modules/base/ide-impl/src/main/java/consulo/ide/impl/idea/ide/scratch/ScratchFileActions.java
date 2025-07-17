914bdafb831f76aa4ee2c12fe1684b9107defd48
UNV <nikolay@yurchenko.su>
2025-07-17 17:18:22 +0300

    Adding @ActionImpl annotations to new actions in the main menu. Localizing.

--- modules/base/ide-impl/src/main/java/consulo/ide/impl/idea/ide/scratch/ScratchFileActions.java
+++ modules/base/ide-impl/src/main/java/consulo/ide/impl/idea/ide/scratch/ScratchFileActions.java
@@ -2,14 +2,13 @@
 package consulo.ide.impl.idea.ide.scratch;
 
 import consulo.annotation.access.RequiredReadAction;
-import consulo.application.util.NotNullLazyValue;
+import consulo.annotation.component.ActionImpl;
 import consulo.application.util.registry.Registry;
 import consulo.codeEditor.Caret;
 import consulo.codeEditor.Editor;
 import consulo.dataContext.DataContext;
 import consulo.externalService.statistic.FeatureUsageTracker;
 import consulo.ide.IdeView;
-import consulo.ide.impl.idea.ide.actions.NewActionGroup;
 import consulo.language.Language;
 import consulo.language.editor.PlatformDataKeys;
 import consulo.language.file.LanguageFileType;
@@ -20,16 +19,10 @@ import consulo.language.scratch.RootType;
 import consulo.language.scratch.ScratchFileService;
 import consulo.language.util.LanguageUtil;
 import consulo.localize.LocalizeValue;
-import consulo.platform.base.icon.PlatformIconGroup;
-import consulo.platform.base.localize.ActionLocalize;
 import consulo.project.Project;
 import consulo.ui.annotation.RequiredUIAccess;
-import consulo.ui.ex.action.ActionPlaces;
 import consulo.ui.ex.action.AnActionEvent;
 import consulo.ui.ex.action.DumbAwareAction;
-import consulo.ui.ex.action.Presentation;
-import consulo.ui.image.Image;
-import consulo.ui.image.ImageEffects;
 import consulo.util.collection.ArrayUtil;
 import consulo.util.collection.JBIterable;
 import consulo.util.io.FileUtil;
@@ -44,7 +37,6 @@ import jakarta.annotation.Nullable;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -60,66 +52,6 @@ public class ScratchFileActions {
     private static int nextBufferIndex() {
         ourCurrentBuffer = (ourCurrentBuffer % Registry.intValue("ide.scratch.buffers")) + 1;
         return ourCurrentBuffer;
-    }
-
-    public static class NewFileAction extends DumbAwareAction {
-        private static final Image ICON = ImageEffects.layered(PlatformIconGroup.filetypesText(), PlatformIconGroup.actionsScratch());
-
-        private static final String ACTION_ID = "NewScratchFile";
-
-        private final NotNullLazyValue<LocalizeValue> myActionText = NotNullLazyValue.createValue(
-            () -> NewActionGroup.isActionInNewPopupMenu(this)
-                ? ActionLocalize.actionNewscratchfileText()
-                : ActionLocalize.actionNewscratchfileTextWithNew()
-        );
-
-        public NewFileAction() {
-            getTemplatePresentation().setIcon(ICON);
-        }
-
-        @Override
-        public void update(@Nonnull AnActionEvent e) {
-            getTemplatePresentation().setTextValue(myActionText.getValue());
-
-            Project project = e.getData(Project.KEY);
-            String place = e.getPlace();
-            boolean enabled = project != null && (e.isFromActionToolbar() || ActionPlaces.isMainMenuOrActionSearch(place)
-                || ActionPlaces.isPopupPlace(place) && e.getData(IdeView.KEY) != null);
-
-            e.getPresentation().setEnabledAndVisible(enabled);
-            updatePresentationTextAndIcon(e, e.getPresentation());
-        }
-
-        @Override
-        @RequiredReadAction
-        public void actionPerformed(@Nonnull AnActionEvent e) {
-            Project project = e.getData(Project.KEY);
-            if (project == null) {
-                return;
-            }
-
-            ScratchFileCreationHelper.Context context = createContext(e, project);
-            Consumer<Language> consumer = l -> {
-                context.language = l;
-                ScratchFileCreationHelper.forLanguage(context.language).prepareText(project, context, DataContext.EMPTY_CONTEXT);
-                doCreateNewScratch(project, context);
-            };
-            if (context.language != null) {
-                consumer.accept(context.language);
-            }
-            else {
-                LRUPopupBuilder.forFileLanguages(project, ActionLocalize.actionNewscratchfileTextWithNew().get(), null, consumer)
-                    .showCenteredInCurrentWindow(project);
-            }
-        }
-
-        private void updatePresentationTextAndIcon(@Nonnull AnActionEvent e, @Nonnull Presentation presentation) {
-            presentation.setTextValue(myActionText.getValue());
-            presentation.setIcon(ICON);
-            if (ActionPlaces.MAIN_MENU.equals(e.getPlace()) && !NewActionGroup.isActionInNewPopupMenu(this)) {
-                presentation.setIcon(null);
-            }
-        }
     }
 
     public static class NewBufferAction extends DumbAwareAction {
