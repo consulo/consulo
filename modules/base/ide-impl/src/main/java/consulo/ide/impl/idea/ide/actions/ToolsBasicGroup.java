cd693141e47ff34c2a9cc26b6e2a894b498e782f
Nikolay Yurchenko <nikolay@yurchenko.su>
2025-08-03 19:25:58 +0300

    Undeleting ToolsBasicGroup. (#821)

--- modules/base/ide-impl/src/main/java/consulo/ide/impl/idea/ide/actions/ToolsBasicGroup.java
+++ modules/base/ide-impl/src/main/java/consulo/ide/impl/idea/ide/actions/ToolsBasicGroup.java
@@ -19,34 +19,24 @@ import consulo.annotation.component.ActionImpl;
 import consulo.annotation.component.ActionRef;
 import consulo.application.dumb.DumbAware;
 import consulo.ide.impl.idea.codeInsight.template.actions.SaveAsTemplateAction;
-import consulo.ide.impl.idea.internal.psiView.PsiViewerAction;
-import consulo.ide.impl.idea.internal.psiView.PsiViewerForContextAction;
-import consulo.ide.impl.idea.tools.ExternalToolsGroup;
 import consulo.platform.base.localize.ActionLocalize;
 import consulo.ui.ex.action.AnSeparator;
 import consulo.ui.ex.action.DefaultActionGroup;
 
 /**
  * @author UNV
- * @since 2025-08-01
+ * @since 2025-08-03
  */
 @ActionImpl(
-    id = "ToolsMenu",
+    id = "ToolsBasicGroup",
     children = {
-        @ActionRef(type = TasksAndContextsGroup.class),
-        @ActionRef(type = AnSeparator.class),
-        @ActionRef(type = ToolsBasicGroup.class),
-        @ActionRef(type = PsiViewerAction.class),
-        @ActionRef(type = PsiViewerForContextAction.class),
-        @ActionRef(type = AnSeparator.class),
-        @ActionRef(type = CreateLauncherScriptAction.class),
-        @ActionRef(type = CreateDesktopEntryAction.class),
-        @ActionRef(type = AnSeparator.class),
-        @ActionRef(type = ExternalToolsGroup.class)
+        @ActionRef(type = SaveAsTemplateAction.class),
+        @ActionRef(type = SaveFileAsTemplateAction.class),
+        @ActionRef(type = AnSeparator.class)
     }
 )
-public class ToolsMenuGroup extends DefaultActionGroup implements DumbAware {
-    public ToolsMenuGroup() {
-        super(ActionLocalize.groupToolsmenuText(), true);
+public class ToolsBasicGroup extends DefaultActionGroup implements DumbAware {
+    public ToolsBasicGroup() {
+        super(ActionLocalize.groupToolsbasicgroupText(), ActionLocalize.groupToolsbasicgroupDescription(), false);
     }
 }
