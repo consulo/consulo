173af95bf1290afe3e34d02ddea7ca92bced8290
Nikolay Yurchenko <nikolay@yurchenko.su>
2025-08-18 22:37:17 +0300

    Refactoring VCS actions (#832)
    * Refactoring VCS actions.
    
    * DirDiffOperation fix: use color supplier.
    
    * VcsToolbarLabelAction refactoring.

--- modules/base/ide-impl/src/main/java/consulo/ide/impl/idea/openapi/vcs/actions/VcsToolbarLabelAction.java
+++ modules/base/ide-impl/src/main/java/consulo/ide/impl/idea/openapi/vcs/actions/VcsToolbarLabelAction.java
@@ -1,11 +1,11 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package consulo.ide.impl.idea.openapi.vcs.actions;
 
+//import consulo.annotation.component.ActionImpl;
 import consulo.ide.impl.idea.openapi.actionSystem.ex.ToolbarLabelAction;
 import consulo.versionControlSystem.ProjectLevelVcsManager;
 import consulo.localize.LocalizeValue;
 import consulo.project.Project;
-import consulo.ui.annotation.RequiredUIAccess;
 import consulo.ui.ex.action.AnActionEvent;
 import consulo.ui.ex.action.Presentation;
 import consulo.ui.ex.awt.action.CustomComponentAction;
@@ -14,23 +14,29 @@ import consulo.versionControlSystem.localize.VcsLocalize;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 
+//@ActionImpl(id = "VcsToolbarLabelAction")
 public class VcsToolbarLabelAction extends ToolbarLabelAction implements CustomComponentAction {
-  @RequiredUIAccess
-  @Override
-  public void update(@Nonnull AnActionEvent e) {
-    super.update(e);
-
-    Project project = e.getData(Project.KEY);
-    e.getPresentation().setVisible(project != null && ProjectLevelVcsManager.getInstance(project).hasActiveVcss());
-    e.getPresentation().setTextValue(getConsolidatedVcsName(project));
-  }
-
-  @Nonnull
-  private static LocalizeValue getConsolidatedVcsName(@Nullable Project project) {
-    LocalizeValue name = VcsLocalize.vcsCommonLabelsVcs();
-    if (project != null) {
-      name = ProjectLevelVcsManager.getInstance(project).getConsolidatedVcsName().map(Presentation.NO_MNEMONIC).map((l, s) -> s + ":");
+    public VcsToolbarLabelAction() {
+        super(LocalizeValue.localizeTODO("VCS Label"));
     }
-    return name;
-  }
+
+    @Override
+    public void update(@Nonnull AnActionEvent e) {
+        super.update(e);
+
+        Project project = e.getData(Project.KEY);
+        e.getPresentation().setVisible(project != null && ProjectLevelVcsManager.getInstance(project).hasActiveVcss());
+        e.getPresentation().setTextValue(getConsolidatedVcsName(project));
+    }
+
+    @Nonnull
+    private static LocalizeValue getConsolidatedVcsName(@Nullable Project project) {
+        if (project != null) {
+            return ProjectLevelVcsManager.getInstance(project)
+                .getConsolidatedVcsName()
+                .map(Presentation.NO_MNEMONIC)
+                .map((l, s) -> s + ":");
+        }
+        return VcsLocalize.vcsCommonLabelsVcs();
+    }
 }
