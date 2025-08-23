9928e40945578da36d531e6e82699fdfcd03e8e6
UNV <nikolay@yurchenko.su>
2025-08-23 20:13:52 +0300

    Fixing references to ChangesView.DeleteUnversioned by id. Refactoring.

--- modules/base/version-control-system-impl/src/main/java/consulo/versionControlSystem/impl/internal/change/ui/awt/IgnoredViewDialog.java
+++ modules/base/version-control-system-impl/src/main/java/consulo/versionControlSystem/impl/internal/change/ui/awt/IgnoredViewDialog.java
@@ -17,6 +17,7 @@ package consulo.versionControlSystem.impl.internal.change.ui.awt;
 
 import consulo.dataContext.DataContext;
 import consulo.project.Project;
+import consulo.ui.annotation.RequiredUIAccess;
 import consulo.ui.ex.action.*;
 import consulo.ui.ex.action.event.AnActionListener;
 import consulo.versionControlSystem.internal.ChangeListManagerEx;
@@ -26,34 +27,40 @@ import jakarta.annotation.Nonnull;
 import java.util.List;
 
 public class IgnoredViewDialog extends SpecificFilesViewDialog {
-  public IgnoredViewDialog(@Nonnull Project project) {
-    super(project,
-          "Ignored Files",
-          ChangesListView.IGNORED_FILES_DATA_KEY,
-          ChangeListManagerEx.getInstanceEx(project).getIgnoredFiles());
-  }
+    public IgnoredViewDialog(@Nonnull Project project) {
+        super(
+            project,
+            "Ignored Files",
+            ChangesListView.IGNORED_FILES_DATA_KEY,
+            ChangeListManagerEx.getInstanceEx(project).getIgnoredFiles()
+        );
+    }
 
-  @Override
-  protected void addCustomActions(@Nonnull DefaultActionGroup group, @Nonnull ActionToolbar actionToolbar) {
-    ActionManager actionManager = ActionManager.getInstance();
-    AnAction deleteAction =
-      EmptyAction.registerWithShortcutSet("ChangesView.DeleteUnversioned.From.Dialog", CommonShortcuts.getDelete(), myView);
-    actionManager.addAnActionListener(new AnActionListener() {
-      @Override
-      public void afterActionPerformed(AnAction action, DataContext dataContext, AnActionEvent event) {
-        if (action.equals(deleteAction)) {
-          refreshView();
-          refreshChanges(myProject, getBrowserBase(myView));
-        }
-      }
-    }, myDisposable);
-    group.add(deleteAction);
-    myView.setMenuActions(new DefaultActionGroup(deleteAction));
-  }
+    @Override
+    protected void addCustomActions(@Nonnull DefaultActionGroup group, @Nonnull ActionToolbar actionToolbar) {
+        ActionManager actionManager = ActionManager.getInstance();
+        AnAction deleteAction =
+            EmptyAction.registerWithShortcutSet(IdeActions.DELETE_UNVERSIONED_FILES, CommonShortcuts.getDelete(), myView);
+        actionManager.addAnActionListener(
+            new AnActionListener() {
+                @Override
+                @RequiredUIAccess
+                public void afterActionPerformed(AnAction action, DataContext dataContext, AnActionEvent event) {
+                    if (action.equals(deleteAction)) {
+                        refreshView();
+                        refreshChanges(myProject, getBrowserBase(myView));
+                    }
+                }
+            },
+            myDisposable
+        );
+        group.add(deleteAction);
+        myView.setMenuActions(new DefaultActionGroup(deleteAction));
+    }
 
-  @Nonnull
-  @Override
-  protected List<VirtualFile> getFiles() {
-    return ChangeListManagerEx.getInstanceEx(myProject).getIgnoredFiles();
-  }
+    @Nonnull
+    @Override
+    protected List<VirtualFile> getFiles() {
+        return ChangeListManagerEx.getInstanceEx(myProject).getIgnoredFiles();
+    }
 }
