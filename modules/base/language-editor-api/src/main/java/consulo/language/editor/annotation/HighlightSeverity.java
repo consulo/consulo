2b7d9482d19b78b1a0bf805dc8104c85d06eab66
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-01-17 12:13:52 +0200

    add consulo.language.editor.annotation.HighlightSeverity.TEXT_ATTRIBUTES for using inside annotator instead INFORMATION

--- modules/base/language-editor-api/src/main/java/consulo/language/editor/annotation/HighlightSeverity.java
+++ modules/base/language-editor-api/src/main/java/consulo/language/editor/annotation/HighlightSeverity.java
@@ -15,6 +15,7 @@
  */
 package consulo.language.editor.annotation;
 
+import consulo.annotation.DeprecationInfo;
 import consulo.util.xml.serializer.DefaultJDOMExternalizer;
 import consulo.util.xml.serializer.JDOMExternalizerUtil;
 import consulo.util.xml.serializer.WriteExternalException;
@@ -28,95 +29,111 @@ import org.jdom.Element;
  * @see Annotation
  */
 public final class HighlightSeverity implements Comparable<HighlightSeverity> {
-  public final String myName;
-  public final int myVal;
+    public final String myName;
+    public final int myVal;
 
-  /**
-   * The standard severity level for information annotations.
-   */
-  public static final HighlightSeverity INFORMATION = new HighlightSeverity("INFORMATION", 10);
+    /**
+     * The standard severity level for information annotations.
+     */
+    public static final HighlightSeverity INFORMATION = new HighlightSeverity("INFORMATION", 10);
 
-  /**
-   * The severity level for errors or warnings obtained from server.
-   */
-  public static final HighlightSeverity GENERIC_SERVER_ERROR_OR_WARNING = new HighlightSeverity("SERVER PROBLEM", 100);
+    public static final HighlightSeverity TEXT_ATTRIBUTES = new HighlightSeverity("TEXT_ATTRIBUTES", 11);
 
-  /**
-   * The standard severity level for 'weak' :) warning annotations.
-   */
-  @Deprecated
-  public static final HighlightSeverity INFO = new HighlightSeverity("INFO", 200);
+    /**
+     * The severity level for errors or warnings obtained from server.
+     */
+    public static final HighlightSeverity GENERIC_SERVER_ERROR_OR_WARNING = new HighlightSeverity("SERVER PROBLEM", 100);
 
-
-  public static final HighlightSeverity WEAK_WARNING = new HighlightSeverity("WEAK WARNING", 200);
-
-  /**
-   * The standard severity level for warning annotations.
-   */
-  public static final HighlightSeverity WARNING = new HighlightSeverity("WARNING", 300);
-
-  /**
-   * The standard severity level for error annotations.
-   */
-  public static final HighlightSeverity ERROR = new HighlightSeverity("ERROR", 400);
-
-  /**
-   * Standard severities levels
-   */
-  public static final HighlightSeverity[] DEFAULT_SEVERITIES = {INFORMATION, GENERIC_SERVER_ERROR_OR_WARNING, INFO, WEAK_WARNING, WARNING, ERROR};
-
-  /**
-   * Creates a new highlighting severity level with the specified name and value.
-   *
-   * @param name the name of the highlighting level.
-   * @param val  the value of the highlighting level. Used for comparing the annotations -
-   *             if two annotations with different severity levels cover the same text range, only
-   *             the annotation with a higher severity level is displayed.
-   */
-  public HighlightSeverity(@Nonnull String name, int val) {
-    myName = name;
-    myVal = val;
-  }
+    /**
+     * The standard severity level for 'weak' :) warning annotations.
+     */
+    @Deprecated
+    @DeprecationInfo("Use #WEAK_WARNING")
+    public static final HighlightSeverity INFO = new HighlightSeverity("INFO", 200);
 
 
-  //read external only
-  public HighlightSeverity(@Nonnull Element element) {
-    this(JDOMExternalizerUtil.readField(element, "myName"), Integer.valueOf(JDOMExternalizerUtil.readField(element, "myVal")));
-  }
+    public static final HighlightSeverity WEAK_WARNING = new HighlightSeverity("WEAK WARNING", 200);
 
-  @Override
-  public String toString() {
-    return myName;
-  }
+    /**
+     * The standard severity level for warning annotations.
+     */
+    public static final HighlightSeverity WARNING = new HighlightSeverity("WARNING", 300);
 
-  @Override
-  public int compareTo(@Nonnull HighlightSeverity highlightSeverity) {
-    return myVal - highlightSeverity.myVal;
-  }
+    /**
+     * The standard severity level for error annotations.
+     */
+    public static final HighlightSeverity ERROR = new HighlightSeverity("ERROR", 400);
 
-  public void writeExternal(Element element) throws WriteExternalException {
-    DefaultJDOMExternalizer.writeExternal(this, element);
-  }
+    /**
+     * Standard severities levels
+     */
+    public static final HighlightSeverity[] DEFAULT_SEVERITIES = {
+        INFORMATION,
+        TEXT_ATTRIBUTES,
+        GENERIC_SERVER_ERROR_OR_WARNING,
+        INFO,
+        WEAK_WARNING,
+        WARNING,
+        ERROR
+    };
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    /**
+     * Creates a new highlighting severity level with the specified name and value.
+     *
+     * @param name the name of the highlighting level.
+     * @param val  the value of the highlighting level. Used for comparing the annotations -
+     *             if two annotations with different severity levels cover the same text range, only
+     *             the annotation with a higher severity level is displayed.
+     */
+    public HighlightSeverity(@Nonnull String name, int val) {
+        myName = name;
+        myVal = val;
+    }
 
-    HighlightSeverity that = (HighlightSeverity)o;
+    //read external only
+    public HighlightSeverity(@Nonnull Element element) {
+        this(JDOMExternalizerUtil.readField(element, "myName"), Integer.valueOf(JDOMExternalizerUtil.readField(element, "myVal")));
+    }
 
-    if (myVal != that.myVal) return false;
-    return myName.equals(that.myName);
-  }
+    @Override
+    public String toString() {
+        return myName;
+    }
 
-  @Override
-  public int hashCode() {
-    int result = myName != null ? myName.hashCode() : 0;
-    return 31 * result + myVal;
-  }
+    @Override
+    public int compareTo(@Nonnull HighlightSeverity highlightSeverity) {
+        return myVal - highlightSeverity.myVal;
+    }
 
-  @Nonnull
-  public String getName() {
-    return myName;
-  }
+    public void writeExternal(Element element) throws WriteExternalException {
+        DefaultJDOMExternalizer.writeExternal(this, element);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        HighlightSeverity that = (HighlightSeverity) o;
+
+        if (myVal != that.myVal) {
+            return false;
+        }
+        return myName.equals(that.myName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = myName != null ? myName.hashCode() : 0;
+        return 31 * result + myVal;
+    }
+
+    @Nonnull
+    public String getName() {
+        return myName;
+    }
 }
