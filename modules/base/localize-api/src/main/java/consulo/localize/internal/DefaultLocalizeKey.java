1ccdc68aa09cbe11a9621e3672abb085b4cf4435
Nikolay Yurchenko <nikolay@yurchenko.su>
2026-01-17 15:49:37 +0300

    Syncing localize-api with localization-api. (#930)

--- modules/base/localize-api/src/main/java/consulo/localize/internal/DefaultLocalizeKey.java
+++ modules/base/localize-api/src/main/java/consulo/localize/internal/DefaultLocalizeKey.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013-2026 consulo.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package consulo.localize.internal;
 
 import consulo.localize.LocalizeKey;
@@ -42,36 +57,6 @@ public final class DefaultLocalizeKey implements LocalizeKey {
         defaultValue = new DefaultLocalizeValue(this);
         myDefaultValue = defaultValue;
         return defaultValue;
-    }
-
-    @Nonnull
-    @Override
-    public LocalizeValue getValue(Object arg) {
-        return new DefaultLocalizeValue(this, arg);
-    }
-
-    @Nonnull
-    @Override
-    public LocalizeValue getValue(Object arg0, Object arg1) {
-        return new DefaultLocalizeValue(this, arg0, arg1);
-    }
-
-    @Nonnull
-    @Override
-    public LocalizeValue getValue(Object arg0, Object arg1, Object arg2) {
-        return new DefaultLocalizeValue(this, arg0, arg1, arg2);
-    }
-
-    @Nonnull
-    @Override
-    public LocalizeValue getValue(Object arg0, Object arg1, Object arg2, Object arg3) {
-        return new DefaultLocalizeValue(this, arg0, arg1, arg2, arg3);
-    }
-
-    @Nonnull
-    @Override
-    public LocalizeValue getValue(Object arg0, Object arg1, Object arg2, Object arg3, Object arg4) {
-        return new DefaultLocalizeValue(this, arg0, arg1, arg2, arg3, arg4);
     }
 
     @Nonnull
--- modules/base/localize-api/src/main/java/consulo/localize/internal/SeparatorJoinedLocalizeValue2.java
+++ modules/base/localize-api/src/main/java/consulo/localize/internal/SeparatorJoinedLocalizeValue2.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2021 consulo.io
+ * Copyright 2013-2026 consulo.io
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,11 +24,14 @@ import java.util.Map;
 
 /**
  * @author VISTALL
- * @since 24/09/2021
+ * @since 2025-09-18
  */
-public final class JoinLocalizeValue extends BaseLocalizeValue {
-    public JoinLocalizeValue(LocalizeValue[] values) {
+public class SeparatorJoinedLocalizeValue2 extends BaseLocalizeValue {
+    private final LocalizeValue mySeparator;
+
+    public SeparatorJoinedLocalizeValue2(LocalizeValue separator, LocalizeValue[] values) {
         super(values);
+        mySeparator = separator;
     }
 
     @Nonnull
@@ -42,11 +45,25 @@ public final class JoinLocalizeValue extends BaseLocalizeValue {
     protected String calcValue(LocalizeManager manager) {
         StringBuilder builder = new StringBuilder();
 
-        for (Object arg : myArgs) {
-            String value = arg instanceof LocalizeValue lv ? lv.getValue() : String.valueOf(arg);
+        for (int i = 0; i < myArgs.length; i++) {
+            if (i != 0) {
+                builder.append(mySeparator.getValue());
+            }
+
+            Object ar = myArgs[i];
+
+            String value = ar instanceof LocalizeValue lv ? lv.getValue() : String.valueOf(ar);
 
             builder.append(value);
         }
         return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o == this
+            || super.equals(o)
+            && o instanceof SeparatorJoinedLocalizeValue2 that
+            && mySeparator.equals(that.mySeparator);
     }
 }
