f42b1718e0da48f9fdfac9adac40167beb56590a
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-02-13 16:45:29 +0200

    hide http requests impl to impl module

--- modules/base/http-impl/src/main/java/consulo/http/impl/internal/ProgressMonitorInputStream.java
+++ modules/base/http-impl/src/main/java/consulo/http/impl/internal/ProgressMonitorInputStream.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package consulo.http.internal;
+package consulo.http.impl.internal;
 
 import consulo.application.progress.ProgressIndicator;
 import jakarta.annotation.Nonnull;
