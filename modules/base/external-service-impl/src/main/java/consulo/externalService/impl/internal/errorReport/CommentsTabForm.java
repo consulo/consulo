6ee1e11d7130c4e9962e8ccee215ded4f807b8d7
Nikolay Yurchenko <nikolay@yurchenko.su>
2026-02-15 17:06:11 +0300

    Eliminating error-reporting forms (#949)
    * Eliminating error-reporting forms.
    
    * setComponentClass → setComponent.

--- modules/base/external-service-impl/src/main/java/consulo/externalService/impl/internal/errorReport/CommentsTabForm.java
+++ modules/base/external-service-impl/src/main/java/consulo/externalService/impl/internal/errorReport/CommentsTabForm.java
@@ -1,7 +1,11 @@
 package consulo.externalService.impl.internal.errorReport;
 
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
 import consulo.externalService.localize.ExternalServiceLocalize;
 import consulo.ui.ex.awt.IdeBorderFactory;
+import consulo.ui.ex.awt.JBUI;
 import consulo.ui.ex.awt.LabeledComponent;
 import consulo.ui.ex.awt.UIUtil;
 
@@ -17,6 +21,7 @@ public class CommentsTabForm {
     private LabeledComponent<JTextField> myErrorComponent;
 
     public CommentsTabForm() {
+        createUIComponents();
         myCommentsArea.setTitle(ExternalServiceLocalize.errorDialogCommentPrompt().get());
         myErrorComponent.getComponent().setPreferredSize(new Dimension(IdeErrorsDialog.COMPONENTS_WIDTH, -1));
         myErrorComponent.setLabelLocation(BorderLayout.NORTH);
@@ -48,5 +53,69 @@ public class CommentsTabForm {
 
     public void addCommentsListener(LabeledTextComponent.TextListener l) {
         myCommentsArea.addCommentsListener(l);
+    }
+
+    private void createUIComponents() {
+        myContentPane = new JPanel();
+        myContentPane.setLayout(new GridLayoutManager(2, 2, JBUI.emptyInsets(), -1, 15));
+        myContentPane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5), null));
+        myErrorComponent = new LabeledComponent<>();
+        myErrorComponent.setComponent(new JTextField());
+        myErrorComponent.setText("&Error message");
+        myContentPane.add(
+            myErrorComponent,
+            new GridConstraints(
+                0,
+                0,
+                1,
+                1,
+                GridConstraints.ANCHOR_NORTH,
+                GridConstraints.FILL_HORIZONTAL,
+                GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                null,
+                null,
+                null,
+                0,
+                false
+            )
+        );
+        myContentPane.add(
+            new Spacer(),
+            new GridConstraints(
+                1,
+                1,
+                1,
+                1,
+                GridConstraints.ANCHOR_CENTER,
+                GridConstraints.FILL_VERTICAL,
+                1,
+                GridConstraints.SIZEPOLICY_WANT_GROW,
+                null,
+                null,
+                null,
+                0,
+                false
+            )
+        );
+        myCommentsArea = new LabeledTextComponent();
+        myContentPane.add(
+            myCommentsArea.getContentPane(),
+            new GridConstraints(
+                1,
+                0,
+                1,
+                1,
+                GridConstraints.ANCHOR_CENTER,
+                GridConstraints.FILL_BOTH,
+                GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                null,
+                null,
+                null,
+                0,
+                false
+            )
+        );
     }
 }
