9077a476bfe2d9d88dea17c22217745280c448cc
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-03-05 18:18:00 +0200

    fixed wrong exports

--- modules/base/language-editor-impl/src/main/java/module-info.java
+++ modules/base/language-editor-impl/src/main/java/module-info.java
@@ -20,7 +20,7 @@ module consulo.language.editor.impl {
 
   exports consulo.language.editor.impl.action;
   exports consulo.language.editor.impl.intention;
-  exports consulo.language.editor.impl.highlight to consulo.ide.impl, consulo.file.editor.impl;
+  exports consulo.language.editor.impl.highlight;
   exports consulo.language.editor.impl.inspection;
   exports consulo.language.editor.impl.inspection.reference;
 
