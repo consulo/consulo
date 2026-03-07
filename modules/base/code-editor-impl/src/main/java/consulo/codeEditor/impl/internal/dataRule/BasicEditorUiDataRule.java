ea4b266dafd220d66e3ea828fd1df1db8f822726
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-03-07 13:28:15 +0200

    missed rules

---
+++ modules/base/code-editor-impl/src/main/java/consulo/codeEditor/impl/internal/dataRule/BasicEditorUiDataRule.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2000-2009 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package consulo.codeEditor.impl.internal.dataRule;
+
+import consulo.annotation.component.ExtensionImpl;
+import consulo.codeEditor.Editor;
+import consulo.codeEditor.EditorEx;
+import consulo.codeEditor.EditorKeys;
+import consulo.dataContext.DataSink;
+import consulo.dataContext.DataSnapshot;
+import consulo.dataContext.UiDataRule;
+import consulo.ui.ex.CopyProvider;
+import consulo.ui.ex.CutProvider;
+import consulo.ui.ex.PasteProvider;
+import jakarta.annotation.Nonnull;
+
+@ExtensionImpl
+public class BasicEditorUiDataRule implements UiDataRule {
+    @Override
+    public void uiDataSnapshot(@Nonnull DataSink sink, @Nonnull DataSnapshot snapshot) {
+        Editor editor = snapshot.get(Editor.KEY);
+        if (editor instanceof EditorEx editorEx) {
+            sink.set(CopyProvider.KEY, editorEx.getCopyProvider());
+            if (!editorEx.isViewer()) {
+                sink.set(CutProvider.KEY, editorEx.getCutProvider());
+                sink.set(PasteProvider.KEY, editorEx.getPasteProvider());
+            }
+        }
+        if (editor != null) {
+            sink.set(EditorKeys.EDITOR_EVEN_IF_INACTIVE, editor);
+        }
+    }
+}
