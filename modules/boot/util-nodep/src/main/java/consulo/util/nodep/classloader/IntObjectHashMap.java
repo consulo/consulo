f2306a7673536f0b3287325b0d6c3162ad0a2ba5
UNV <nikolay@yurchenko.su>
2026-03-14 14:02:12 +0300

    Removing excess new-lines at the end of IntObjectHashMap.

--- modules/boot/util-nodep/src/main/java/consulo/util/nodep/classloader/IntObjectHashMap.java
+++ modules/boot/util-nodep/src/main/java/consulo/util/nodep/classloader/IntObjectHashMap.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+`// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package consulo.util.nodep.classloader;
 
 import org.jspecify.annotations.Nullable;
@@ -78,23 +78,3 @@ final class IntObjectHashMap {
     return key == 0 ? specialZeroValue : values[hashIndex(keys, key)];
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
