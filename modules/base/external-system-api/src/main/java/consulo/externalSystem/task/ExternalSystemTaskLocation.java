da0b7aaec50eacb494dd817ef71dfeed22646bd5
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-03-23 22:50:15 +0200

    fixes after merge

--- modules/base/external-system-api/src/main/java/consulo/externalSystem/task/ExternalSystemTaskLocation.java
+++ modules/base/external-system-api/src/main/java/consulo/externalSystem/task/ExternalSystemTaskLocation.java
@@ -15,27 +15,58 @@
  */
 package consulo.externalSystem.task;
 
+import consulo.annotation.access.RequiredReadAction;
 import consulo.execution.action.PsiLocation;
+import consulo.externalSystem.model.ProjectSystemId;
 import consulo.externalSystem.model.execution.ExternalTaskExecutionInfo;
+import consulo.language.plain.PlainTextFileType;
+import consulo.language.psi.*;
 import consulo.project.Project;
-import consulo.language.psi.PsiFile;
+import consulo.util.lang.StringUtil;
+import consulo.virtualFileSystem.VirtualFile;
+import consulo.virtualFileSystem.util.VirtualFileUtil;
+import org.jspecify.annotations.Nullable;
+
+import java.io.File;
 
 /**
  * @author Denis Zhdanov
  * @since 6/5/13 8:11 PM
  */
-public class ExternalSystemTaskLocation extends PsiLocation<PsiFile> {
+public class ExternalSystemTaskLocation extends PsiLocation<PsiElement> {
+    private final ExternalTaskExecutionInfo myTaskInfo;
 
-  
-  private final ExternalTaskExecutionInfo myTaskInfo;
+    @RequiredReadAction
+    public ExternalSystemTaskLocation(Project project, PsiElement psiElement, ExternalTaskExecutionInfo taskInfo) {
+        super(project, psiElement);
+        myTaskInfo = taskInfo;
+    }
 
-  public ExternalSystemTaskLocation(Project project, PsiFile psiElement, ExternalTaskExecutionInfo taskInfo) {
-    super(project, psiElement);
-    myTaskInfo = taskInfo;
-  }
+    public ExternalTaskExecutionInfo getTaskInfo() {
+        return myTaskInfo;
+    }
 
-  
-  public ExternalTaskExecutionInfo getTaskInfo() {
-    return myTaskInfo;
-  }
+    @RequiredReadAction
+    public static ExternalSystemTaskLocation create(Project project,
+                                                    ProjectSystemId systemId,
+                                                    @Nullable String projectPath,
+                                                    ExternalTaskExecutionInfo taskInfo) {
+        if (projectPath != null) {
+            VirtualFile file = VirtualFileUtil.findFileByIoFile(new File(projectPath), false);
+            if (file != null) {
+                PsiDirectory psiFile = PsiManager.getInstance(project).findDirectory(file);
+                if (psiFile != null) {
+                    return new ExternalSystemTaskLocation(project, psiFile, taskInfo);
+                }
+            }
+        }
+
+        String name = systemId.getReadableName() + projectPath + StringUtil.join(taskInfo.getSettings().getTaskNames(), " ");
+        // We create a dummy text file instead of re-using external system file in order to avoid clashing with other configuration producers.
+        // For example gradle files are enhanced groovy scripts but we don't want to run them via regular IJ groovy script runners.
+        // Gradle tooling api should be used for running gradle tasks instead. IJ execution sub-system operates on Location objects
+        // which encapsulate PsiElement and groovy runners are automatically applied if that PsiElement IS-A GroovyFile.
+        PsiFile psiFile = PsiFileFactory.getInstance(project).createFileFromText(name, PlainTextFileType.INSTANCE, "");
+        return new ExternalSystemTaskLocation(project, psiFile, taskInfo);
+    }
 }
