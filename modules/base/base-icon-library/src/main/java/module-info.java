fa4bf28d075e9326693864e272a921497e8d2201
UNV <nikolay@yurchenko.su>
2026-04-02 14:36:25 +0300

    Marking base-icon-library and base-localize-library as @NullMarked.

--- modules/base/base-icon-library/src/main/java/module-info.java
+++ modules/base/base-icon-library/src/main/java/module-info.java
@@ -1,8 +1,12 @@
+import org.jspecify.annotations.NullMarked;
+
 /**
  * @author VISTALL
- * @since 13/01/2022
+ * @since 2022-01-13
  */
+@NullMarked
 module consulo.base.icon.library {
+  requires consulo.annotation;
   requires consulo.ui.api;
 
   exports consulo.platform.base.icon;
