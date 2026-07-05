73a9b28049c9111a1aafd37434d051e4bad41437
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-07-06 00:04:49 +0300

    sync from jb

--- modules/base/language-editor-impl/src/main/java/consulo/language/editor/impl/internal/daemon/PsiChangeHandler.java
+++ modules/base/language-editor-impl/src/main/java/consulo/language/editor/impl/internal/daemon/PsiChangeHandler.java
@@ -1,4 +1,20 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+/*
+ * Copyright 2013-2026 consulo.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package consulo.language.editor.impl.internal.daemon;
 
 import consulo.annotation.access.RequiredReadAction;
@@ -28,30 +44,43 @@ import consulo.language.psi.event.PsiTreeChangeEvent;
 import consulo.module.content.ProjectRootManager;
 import consulo.project.Project;
 import consulo.project.ProjectCoreUtil;
-import consulo.ui.UIAccess;
 import consulo.ui.annotation.RequiredUIAccess;
+import consulo.ui.ex.awt.util.Alarm;
 import consulo.util.collection.ContainerUtil;
 import consulo.util.collection.SmartList;
 import consulo.util.dataholder.Key;
-import consulo.util.lang.Pair;
 import consulo.virtualFileSystem.VirtualFile;
 import org.jspecify.annotations.Nullable;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-final class PsiChangeHandler extends PsiTreeChangeAdapter implements Disposable {
+/**
+ * Records PSI/document changes and defers the daemon-dirty-scope computation off the write/commit path.
+ * <p>
+ * Matching the JetBrains design, PSI tree change events and the on-commit callback only <b>record</b>
+ * changed elements (guarded by {@link #changedElements}) and <b>queue</b> a background pooled-thread alarm.
+ * The alarm's {@link #run()} then processes the recorded changes under a read action on a background thread,
+ * so no write lock is required on the commit finalization path (which runs outside the write action).
+ */
+final class PsiChangeHandler extends PsiTreeChangeAdapter implements Disposable, Runnable {
     private static final ExtensionPointName<ChangeLocalityDetector> EP_NAME = ExtensionPointName.create(ChangeLocalityDetector.class);
     private /*NOT STATIC!!!*/ final Key<Boolean> UPDATE_ON_COMMIT_ENGAGED = Key.create("UPDATE_ON_COMMIT_ENGAGED");
 
     private final Project myProject;
-    private final Map<Document, List<Pair<PsiElement, Boolean>>> changedElements = ContainerUtil.createWeakMap();
+    private final Map<Document, List<Change>> changedElements = ContainerUtil.createWeakMap(); // guarded by changedElements
     private final FileStatusMap myFileStatusMap;
+    private final Alarm myUpdateFileStatusAlarm;
+
+    private record Change(PsiElement psiElement, boolean whiteSpaceOptimizationAllowed) {
+    }
 
     PsiChangeHandler(Project project, MessageBusConnection connection) {
         myProject = project;
         myFileStatusMap = DaemonCodeAnalyzerInternal.getInstanceEx(myProject).getFileStatusMap();
+        myUpdateFileStatusAlarm = new Alarm(Alarm.ThreadToUse.POOLED_THREAD, this);
+
         EditorFactory.getInstance().getEventMulticaster().addDocumentListener(
             new DocumentListener() {
                 @Override
@@ -71,7 +100,7 @@ final class PsiChangeHandler extends PsiTreeChangeAdapter implements Disposable 
                             document,
                             () -> {
                                 if (document.getUserData(UPDATE_ON_COMMIT_ENGAGED) != null) {
-                                    updateChangesForDocument(document);
+                                    updateChangesForDocumentOnCommit(document);
                                     document.putUserData(UPDATE_ON_COMMIT_ENGAGED, null);
                                 }
                             }
@@ -90,11 +119,9 @@ final class PsiChangeHandler extends PsiTreeChangeAdapter implements Disposable 
             @Override
             @RequiredUIAccess
             public void transactionCompleted(Document document, PsiFile file) {
-                updateChangesForDocument(document);
-                document.putUserData(
-                    UPDATE_ON_COMMIT_ENGAGED,
-                    null
-                ); // ensure we don't call updateChangesForDocument() twice which can lead to whole file re-highlight
+                updateChangesForDocumentOnCommit(document);
+                // ensure we don't call updateChangesForDocumentOnCommit() twice which can lead to whole file re-highlight
+                document.putUserData(UPDATE_ON_COMMIT_ENGAGED, null);
             }
         });
     }
@@ -104,49 +131,40 @@ final class PsiChangeHandler extends PsiTreeChangeAdapter implements Disposable 
     }
 
     @RequiredUIAccess
-    private void updateChangesForDocument(Document document) {
-        Application application = myProject.getApplication();
-        application.assertIsWriteThread();
+    private void updateChangesForDocumentOnCommit(Document document) {
         if (myProject.isDisposed()) {
             return;
         }
-        List<Pair<PsiElement, Boolean>> toUpdate = changedElements.get(document);
-        if (toUpdate == null) {
-            // The document has been changed, but psi hasn't
-            // We may still need to rehighlight the file if there were changes inside highlighted ranges.
-            if (UpdateHighlightersUtilImpl.isWhitespaceOptimizationAllowed(document)) {
-                return;
-            }
+        Application application = myProject.getApplication();
+        PsiDocumentManager psiDocumentManager = PsiDocumentManager.getInstance(myProject);
 
-            // don't create PSI for files in other projects
-            PsiElement file = PsiDocumentManager.getInstance(myProject).getCachedPsiFile(document);
-            if (file == null) {
-                return;
+        // don't create PSI for files in other projects
+        PsiFile psiFile = psiDocumentManager.getCachedPsiFile(document);
+        if (psiFile != null) {
+            synchronized (changedElements) {
+                // The document has been changed, but psi hasn't.
+                // We may still need to rehighlight the file if there were changes inside highlighted ranges.
+                if (!UpdateHighlightersUtilImpl.isWhitespaceOptimizationAllowed(document) && !changedElements.containsKey(document)) {
+                    storeChangedElement(psiFile, document, true);
+                }
             }
-
-            toUpdate = Collections.singletonList(Pair.create(file, true));
         }
-        Editor editor = FileEditorManager.getInstance(myProject).getSelectedTextEditor();
-        if (editor != null && !application.isUnitTestMode()) {
+
+        Editor selectedEditor = FileEditorManager.getInstance(myProject).getSelectedTextEditor();
+        PsiFile selectedPsiFile = selectedEditor == null ? null : psiDocumentManager.getCachedPsiFile(selectedEditor.getDocument());
+        if (selectedPsiFile != null && !application.isUnitTestMode()) {
             application.invokeLater(
                 () -> {
-                    if (!editor.isDisposed()) {
-                        EditorMarkupModel markupModel = (EditorMarkupModel)editor.getMarkupModel();
-                        PsiFile file = PsiDocumentManager.getInstance(myProject).getPsiFile(editor.getDocument());
-                        ErrorStripeUpdateManager.getInstance(myProject).setOrRefreshErrorStripeRenderer(markupModel, file);
+                    if (!selectedEditor.isDisposed() && selectedEditor.getMarkupModel() instanceof EditorMarkupModel markupModel) {
+                        ErrorStripeUpdateManager.getInstance(myProject).setOrRefreshErrorStripeRenderer(markupModel, selectedPsiFile);
                     }
                 },
-                application.getModalityStateForComponent(editor.getComponent()),
+                application.getModalityStateForComponent(selectedEditor.getComponent()),
                 myProject.getDisposed()
             );
         }
 
-        for (Pair<PsiElement, Boolean> changedElement : toUpdate) {
-            PsiElement element = changedElement.getFirst();
-            Boolean whiteSpaceOptimizationAllowed = changedElement.getSecond();
-            updateByChange(element, document, whiteSpaceOptimizationAllowed);
-        }
-        changedElements.remove(document);
+        queueUpdateFileStatusQueue();
     }
 
     @Override
@@ -206,7 +224,6 @@ final class PsiChangeHandler extends PsiTreeChangeAdapter implements Disposable 
     }
 
     private void queueElement(PsiElement child, boolean whitespaceOptimizationAllowed, PsiTreeChangeEvent event) {
-        myProject.getApplication().assertIsWriteThread();
         PsiFile file = event.getFile();
         if (file == null) {
             file = child.getContainingFile();
@@ -229,18 +246,65 @@ final class PsiChangeHandler extends PsiTreeChangeAdapter implements Disposable 
                 return;
             }
 
-            List<Pair<PsiElement, Boolean>> toUpdate = changedElements.get(document);
-            if (toUpdate == null) {
-                toUpdate = new SmartList<>();
-                changedElements.put(document, toUpdate);
-            }
-            toUpdate.add(Pair.create(child, whitespaceOptimizationAllowed));
+            storeChangedElement(child, document, whitespaceOptimizationAllowed);
         }
     }
 
-    @RequiredUIAccess
-    private void updateByChange(PsiElement child, Document document, boolean whitespaceOptimizationAllowed) {
-        UIAccess.assertIsUIThread();
+    private void storeChangedElement(PsiElement child, Document document, boolean whitespaceOptimizationAllowed) {
+        synchronized (changedElements) {
+            List<Change> toUpdate = changedElements.computeIfAbsent(document, __ -> new SmartList<>());
+            toUpdate.add(new Change(child, whitespaceOptimizationAllowed));
+        }
+    }
+
+    private void queueUpdateFileStatusQueue() {
+        synchronized (myUpdateFileStatusAlarm) {
+            myUpdateFileStatusAlarm.cancelRequest(this);
+            myUpdateFileStatusAlarm.addRequest(this, 0);
+        }
+    }
+
+    // handle queued elements on a background thread under a read action
+    @Override
+    public void run() {
+        if (myProject.isDisposed()) {
+            return;
+        }
+        myProject.getApplication().runReadAction((Runnable)() -> {
+            // assume changedElements won't change under read action
+            boolean needRestart = handleChangedElements();
+            boolean hasUncommittedDocuments = PsiDocumentManager.getInstance(myProject).hasUncommitedDocuments();
+            // when hasUncommittedDocuments=true, daemon will restart again on commit anyway
+            if (needRestart && !hasUncommittedDocuments) {
+                DaemonCodeAnalyzerInternal.getInstanceEx(myProject).stopProcess(true, "flushUpdateFileStatusQueue");
+            }
+        });
+    }
+
+    // return true if myFileStatusMap has changed and we need restart
+    @RequiredReadAction
+    private boolean handleChangedElements() {
+        List<Map.Entry<Document, List<Change>>> entries;
+        synchronized (changedElements) {
+            entries = new ArrayList<>(changedElements.entrySet());
+            changedElements.clear();
+        }
+        boolean changed = false;
+        for (Map.Entry<Document, List<Change>> entry : entries) {
+            Document document = entry.getKey();
+            for (Change change : entry.getValue()) {
+                updateByChange(document, change.psiElement(), change.whiteSpaceOptimizationAllowed());
+                changed = true;
+            }
+        }
+        return changed;
+    }
+
+    @RequiredReadAction
+    private void updateByChange(Document document, PsiElement child, boolean whitespaceOptimizationAllowed) {
+        if (myProject.isDisposed()) {
+            return;
+        }
         PsiFile file;
         try {
             file = child.getContainingFile();
@@ -289,6 +353,7 @@ final class PsiChangeHandler extends PsiTreeChangeAdapter implements Disposable 
             .isExcluded(virtualFile);
     }
 
+    @RequiredReadAction
     private static @Nullable PsiElement getChangeHighlightingScope(PsiElement element) {
         for (ChangeLocalityDetector detector : element.getProject()
             .getApplication()
