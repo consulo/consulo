adad6e82d92cde1ee4ed4696a7cec7f4dc2436bd
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-08-09 19:18:55 +0300

    web distro

---
+++ distribution/boot/web/consulo.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ROOT_DIR="$(dirname "$0")"
+
+while true ; do
+  BUILD_DIR=`ls -1v $ROOT_DIR/platform | tail -n 1`
+
+  CONSULO_HOME="$ROOT_DIR/platform/$BUILD_DIR"
+
+  export CONSULO_HOME
+
+  export ROOT_DIR
+
+  . "$CONSULO_HOME/bin/launcher.sh"
+
+  eval "$JDK/bin/java" $ALL_JVM_ARGS -Dconsulo.restart.code=88 $MAIN_CLASS_NAME "$@"
+
+  test $? -ne 88 && break
+done
