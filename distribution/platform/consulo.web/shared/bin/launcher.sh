83a32cf62f63104c90cfd7f9365c2c87097d0331
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-08-09 22:17:37 +0300

    missed launcher.sh

---
+++ distribution/platform/consulo.web/shared/bin/launcher.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+#
+# ---------------------------------------------------------------------
+# Consulo Web startup script.
+# ---------------------------------------------------------------------
+#
+
+message()
+{
+  echo "ERROR: Cannot start Consulo Web" >&2
+  echo "$1" >&2
+}
+
+UNAME=`which uname`
+GREP=`which egrep`
+GREP_OPTIONS=""
+CUT=`which cut`
+READLINK=`which readlink`
+CAT=`which cat`
+TR=`which tr`
+
+if [ -z "$UNAME" -o -z "$GREP" -o -z "$CUT" -o -z "$CAT" -o -z "$TR" ]; then
+  message "Required tools are missing - check beginning of \"$0\" file for details."
+  exit 1
+fi
+
+OS_TYPE=`"$UNAME" -s`
+
+IDE_HOME="$CONSULO_HOME"
+IDE_BIN_HOME="$CONSULO_HOME/bin"
+
+# ---------------------------------------------------------------------
+# Locate a JDK installation directory which will be used to run the IDE.
+# Try (in order): CONSULO_JRE, bundled jdk, JDK_HOME, JAVA_HOME, "java" in PATH.
+# ---------------------------------------------------------------------
+if [ -n "$CONSULO_JRE" -a -x "$CONSULO_JRE/bin/java" ]; then
+  JDK="$CONSULO_JRE"
+elif [ -x "$IDE_HOME/jre/bin/java" ] && "$IDE_HOME/jre/bin/java" -version > /dev/null 2>&1 ; then
+  JDK="$IDE_HOME/jre"
+elif [ -n "$JDK_HOME" -a -x "$JDK_HOME/bin/java" ]; then
+  JDK="$JDK_HOME"
+elif [ -n "$JAVA_HOME" -a -x "$JAVA_HOME/bin/java" ]; then
+  JDK="$JAVA_HOME"
+else
+  JAVA_BIN_PATH=`which java`
+  if [ -n "$JAVA_BIN_PATH" ]; then
+    if [ "$OS_TYPE" = "FreeBSD" -o "$OS_TYPE" = "MidnightBSD" ]; then
+      JAVA_LOCATION=`JAVAVM_DRYRUN=yes java | "$GREP" '^JAVA_HOME' | "$CUT" -c11-`
+      if [ -x "$JAVA_LOCATION/bin/java" ]; then
+        JDK="$JAVA_LOCATION"
+      fi
+    elif [ "$OS_TYPE" = "SunOS" ]; then
+      JAVA_LOCATION="/usr/jdk/latest"
+      if [ -x "$JAVA_LOCATION/bin/java" ]; then
+        JDK="$JAVA_LOCATION"
+      fi
+    elif [ "$OS_TYPE" = "Darwin" ]; then
+      JAVA_LOCATION=`/usr/libexec/java_home`
+      if [ -x "$JAVA_LOCATION/bin/java" ]; then
+        JDK="$JAVA_LOCATION"
+      fi
+    fi
+
+    if [ -z "$JDK" -a -x "$READLINK" ]; then
+      JAVA_LOCATION=`"$READLINK" -f "$JAVA_BIN_PATH"`
+      case "$JAVA_LOCATION" in
+        */jre/bin/java)
+          JAVA_LOCATION=`echo "$JAVA_LOCATION" | xargs dirname | xargs dirname | xargs dirname` ;;
+        *)
+          JAVA_LOCATION=`echo "$JAVA_LOCATION" | xargs dirname | xargs dirname` ;;
+      esac
+      if [ -x "$JAVA_LOCATION/bin/java" ]; then
+        JDK="$JAVA_LOCATION"
+      fi
+    fi
+  fi
+fi
+
+if [ -z "$JDK" ]; then
+  message "No JDK found. Please validate either CONSULO_JRE, JDK_HOME or JAVA_HOME environment variable points to valid JDK installation."
+  exit 1
+fi
+
+# ---------------------------------------------------------------------
+# Collect JVM options and properties.
+# ---------------------------------------------------------------------
+
+MAIN_CLASS_NAME="consulo.web.boot.main.Main"
+
+VM_OPTIONS_FILE="$ROOT_DIR/consulo64.vmoptions"
+
+if [ -r "$VM_OPTIONS_FILE" ]; then
+  VM_OPTIONS=`"$CAT" "$VM_OPTIONS_FILE" | "$GREP" -v "^#.*" | "$TR" '\n' ' '`
+  VM_OPTIONS="$VM_OPTIONS -Djb.vmOptionsFile=\"$VM_OPTIONS_FILE\" -Dconsulo.vm.options.file=\"$VM_OPTIONS_FILE\""
+fi
+
+APP_OPTIONS_FILE="$IDE_HOME/bin/app.vmoptions"
+APP_OPTIONS=""
+
+if [ -r "$APP_OPTIONS_FILE" ]; then
+  APP_OPTIONS=`"$CAT" "$APP_OPTIONS_FILE" | "$GREP" -v "^#.*" | "$TR" '\n' ' '`
+fi
+
+COMMON_JVM_ARGS="-classpath \"$IDE_HOME/boot/*\" -Dconsulo.home.path=\"$IDE_HOME\" -Didea.home.path=\"$IDE_HOME\" -Didea.properties.file=\"$ROOT_DIR/consulo.properties\" -Dconsulo.properties.file=\"$ROOT_DIR/consulo.properties\""
+IDE_JVM_ARGS=""
+ALL_JVM_ARGS="$APP_OPTIONS $VM_OPTIONS $COMMON_JVM_ARGS $IDE_JVM_ARGS $AGENT $REQUIRED_JVM_ARGS"
+
+export ALL_JVM_ARGS
+export MAIN_CLASS_NAME
+export JDK
