ed9112faa71e1d89b380f4768dc24f8f98049e8e
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-08-15 13:11:56 +0300

    return back empty creation

---
+++ modules/base/module-creation-api/src/main/java/consulo/module/creation/internal/EmptyNewModuleBuilder.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2013-2016 consulo.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package consulo.module.creation.internal;
+
+import consulo.annotation.component.ExtensionImpl;
+import consulo.application.AllIcons;
+import consulo.localize.LocalizeValue;
+import consulo.module.creation.NewModuleWizardContextBase;
+import consulo.module.creation.scratch.NewModuleBuilder;
+import consulo.module.creation.scratch.NewModuleContext;
+
+/**
+ * @author VISTALL
+ * @since 05.06.14
+ */
+@ExtensionImpl(id = "empty", order = "last")
+public class EmptyNewModuleBuilder implements NewModuleBuilder {
+    @Override
+    public void setupContext(NewModuleContext context) {
+        context.add(LocalizeValue.localizeTODO("Empty"), AllIcons.FileTypes.Any_type, NewModuleWizardContextBase::new);
+    }
+}
