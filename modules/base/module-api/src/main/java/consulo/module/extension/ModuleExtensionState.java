97ee1ab631683c4028110dc81cb8d060e1222baf
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-09-06 17:14:06 +0300

    preparing for pojo module extension state

---
+++ modules/base/module-api/src/main/java/consulo/module/extension/ModuleExtensionState.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2013-2026 consulo.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package consulo.module.extension;
+
+import consulo.util.xml.serializer.annotation.Attribute;
+
+/**
+ * @author VISTALL
+ * @since 2026-09-06
+ */
+public abstract class ModuleExtensionState {
+    @Attribute
+    public String id;
+
+    @Attribute
+    public boolean enabled;
+}
